ab44906307a68f3da2b99961a51b3da3bf46b9d7
Halgari <tbaldridge@gmail.com>
2022-11-15 08:07:35 -0700

    Add GamePath and associated tests

---
+++ src/NexusMods.Paths/GamePath.cs
@@ -0,0 +1,61 @@
+﻿namespace NexusMods.Paths;
+
+/// <summary>
+///     The base folder for the GamePath, more values can easily be added here as needed
+/// </summary>
+public enum GameFolderType : byte
+{
+    Game = 0,
+    Saves,
+    Profiles
+}
+
+public struct GamePath : IPath
+{
+    public GamePath(GameFolderType folder, RelativePath path)
+    {
+        Folder = folder;
+        Path = path;
+    }
+    
+    public GamePath(GameFolderType folder, string path) : this(folder, path.ToRelativePath())
+    {
+    }
+    public bool Equals(GamePath other)
+    {
+        return Folder == other.Folder && Path == other.Path;
+    }
+
+    public static bool operator ==(GamePath a, GamePath b)
+    {
+        return a.Folder == b.Folder && a.Path == b.Path;
+    }
+
+    public static bool operator !=(GamePath a, GamePath b)
+    {
+        return a.Folder != b.Folder || a.Path != b.Path;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is GamePath other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return Path.GetHashCode() ^ (int)Folder;
+    }
+
+    public RelativePath Path { get; }
+    public GameFolderType Folder { get; }
+
+
+
+    public override string ToString()
+    {
+        return "{" + Folder + "}\\" + Path;
+    }
+
+    public Extension Extension => Path.Extension;
+    public RelativePath FileName => Path.FileName;
+}
---
+++ tests/NexusMods.Paths.Tests/GamePathTests.cs
@@ -0,0 +1,52 @@
+﻿namespace NexusMods.Paths.Tests;
+
+public class GamePathTests
+{
+
+    [Fact]
+    public void CanComparePaths()
+    {
+        var pathA = new GamePath(GameFolderType.Game, @"\foo\bar.zip");
+        var pathB = new GamePath(GameFolderType.Game, @"\Foo\bar.zip");
+        var pathC = new GamePath(GameFolderType.Profiles, @"\foo\bar.zip");
+        var pathD = new GamePath(GameFolderType.Game, @"\foo\bar.pex");
+        
+        Assert.Equal(pathA, pathB);
+        Assert.NotEqual(pathA, pathC);
+        Assert.NotEqual(pathA, pathD);
+        
+        Assert.True(pathA == pathB);
+        Assert.False(pathA == pathC);
+        Assert.True(pathA != pathC);
+    }
+
+    [Fact]
+    public void CanTreatLikeIPath()
+    {
+        var pathA = new GamePath(GameFolderType.Game, @"foo\bar.zip");
+        var ipath = (IPath)pathA;
+        
+        Assert.Equal(Ext.Zip, ipath.Extension);
+        Assert.Equal(@"bar.zip".ToRelativePath(), ipath.FileName);
+    }
+
+    [Fact]
+    public void CanGetHashCode()
+    {
+        var pathA = new GamePath(GameFolderType.Game, @"foo\bar.zip");
+        var pathB = new GamePath(GameFolderType.Game, @"foo\ba.zip");
+        
+        Assert.Equal(pathA.GetHashCode(), pathA.GetHashCode());
+        Assert.NotEqual(pathA.GetHashCode(), pathB.GetHashCode());
+    }
+
+    [Fact]
+    public void CanConvertToString()
+    {
+        var pathA = new GamePath(GameFolderType.Game, @"foo\bar.zip");
+        var pathB = new GamePath(GameFolderType.Saves, @"foo\ba.zip");
+        Assert.Equal(@"{Game}\foo\bar.zip", pathA.ToString());
+        Assert.Equal(@"{Saves}\foo\ba.zip", pathB.ToString());
+    }
+
+}
