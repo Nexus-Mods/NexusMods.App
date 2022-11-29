86d635cd61a06ca10a5c92394f506db236960ecc
Halgari <tbaldridge@gmail.com>
2022-11-29 09:11:49 -0700

    Add Root, and another test

---
+++ src/NexusMods.DataModel/Abstractions/Root.cs
@@ -0,0 +1,94 @@
+﻿using System.Reactive.Subjects;
+
+namespace NexusMods.DataModel.Abstractions;
+
+public class Root<TRoot> where TRoot : Entity
+{
+    private readonly Subject<(TRoot Old, TRoot New)> _changes = new();
+    private readonly object _lockObject = new();
+
+    /// <summary>
+    /// Current value of the root instance
+    /// </summary>
+    public TRoot Value { get; private set; }
+    
+    
+    /// <summary>
+    /// Datastore used by this root when creating new entities
+    /// </summary>
+    public IDataStore Store { get; private set; }
+    
+    /// <summary>
+    /// A list of all changes to this root. Values here are published outside of the locking
+    /// semantics of this system, so changes may be received out-of-order of a large number of
+    /// updates are happening and once from multiple threads. This can be avoided by performing
+    /// multi-threaded code inside of the `Alter` function instead of having multiple threads
+    /// call `Alter` at once.
+    /// </summary>
+    public IObservable<(TRoot Old, TRoot New)> Changes => _changes;
+
+    public Root(TRoot registry)
+    {
+        Value = registry;
+        Store = registry.Store;
+    }
+
+    /// <summary>
+    /// Transactionally modify the contents of this root. The body of `f` may be executed
+    /// several times if the contents of this `Root` instance has during the execution of `f`
+    /// </summary>
+    /// <param name="f"></param>
+    public void Alter(Func<TRoot, TRoot> f)
+    {
+        using var _ = IDataStore.WithCurrent(Store);
+        restart:
+        var oldRoot = Value;
+        var newRoot = f(oldRoot);
+        if (newRoot.Id == oldRoot.Id)
+            return;
+
+        lock (_lockObject)
+        {
+            if (oldRoot.Id != Value.Id) goto restart;
+            Value = newRoot;
+        }
+        _changes.OnNext((oldRoot, newRoot));
+    }
+    
+    /// <summary>
+    /// Transactionally modify the contents of this root. The body of `f` may be executed
+    /// several times if the contents of this `Root` instance has during the execution of `f`
+    /// </summary>
+    /// <param name="f"></param>
+    public async Task AlterAsync(Func<TRoot, ValueTask<TRoot>> f)
+    {
+        using var _ = IDataStore.WithCurrent(Store);
+        restart:
+        var oldRoot = Value;
+        var newRoot = await f(oldRoot);
+        if (newRoot.Id == oldRoot.Id)
+            return;
+        
+        lock (_lockObject)
+        {
+            if (oldRoot.Id != Value.Id) goto restart;
+            Value = newRoot;
+        }
+        _changes.OnNext((oldRoot, newRoot));
+    }
+
+    /// <summary>
+    /// Hard re-sets the value of this root to a new value.
+    /// </summary>
+    /// <param name="newRoot"></param>
+    public void Rebase(TRoot newRoot)
+    {
+        TRoot oldRoot;
+        lock (_lockObject)
+        {
+            oldRoot = Value;
+            Value = newRoot;
+        }
+        _changes.OnNext((oldRoot, newRoot));
+    }
+}
--- tests/NexusMods.DataModel.Tests/ModelTests.cs
+++ tests/NexusMods.DataModel.Tests/ModelTests.cs
@@ -1,4 +1,5 @@
-﻿using FluentAssertions;
+﻿using System.Reactive.Linq;
+using FluentAssertions;
 using NexusMods.DataModel.Abstractions;
 using NexusMods.Paths;
 
@@ -17,10 +18,26 @@ public class ModelTests
     public async Task CanCreateModFile()
     {
         using var _ = IDataStore.WithCurrent(_datastore);
-        var file = new ModFile(new GamePath(GameFolderType.Game, "foo/bar.pez"));
+        var file = new ModFile(To:new GamePath(GameFolderType.Game, "foo/bar.pez"));
         file.Store.Should().NotBeNull();
         file.Id.Should().NotBeNull();
 
         _datastore.Get<ModFile>(file.Id).To.Should().BeEquivalentTo(file.To);
+    }
+
+    [Fact]
+    public async Task CanSeeChangesViaObservable()
+    {
+        using var _2 = IDataStore.WithCurrent(_datastore);
+        var list = new HashSet<Id>();
+        
+        var root = new Root<ModFile>(new ModFile(To: new GamePath(GameFolderType.Game, "foo/bar.pez")));
+        using var _ = root.Changes.Select(x => x.New)
+            .Subscribe(x => list.Add(x.Id));
+
+        await root.AlterAsync(async x => x with { To = new GamePath(GameFolderType.Game, "foo/baz.pex") });
+
+        root.Value.To.Should().NotBeEquivalentTo(new GamePath(GameFolderType.Game, "foo/bar.pez"));
+        list.Should().Contain(root.Value.Id);
     }
 }
