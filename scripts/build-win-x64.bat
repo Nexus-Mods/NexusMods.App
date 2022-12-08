83ba0017c871dc0ee7df495589af2ce57b063089
Halgari <tbaldridge@gmail.com>
2022-12-08 06:36:28 -0700

    Add windows build script

---
+++ scripts/build-win-x64.bat
@@ -0,0 +1,3 @@
+﻿dotnet clean
+dotnet restore
+dotnet publish src\NexusMods.App\NexusMods.App.csproj -c Release -r win-x64 --output bin/win-x64 --self-contained -p:PublishReadyToRun=true -p:PublishSingleFile=true
