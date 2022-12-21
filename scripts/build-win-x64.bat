b44b900794a0031a4309feec3952c9c9cdcd9e3a
Halgari <tbaldridge@gmail.com>
2022-12-20 17:20:25 -0700

    rename some files missed in the modlist->loadout rename, add tiered compilation

--- scripts/build-win-x64.bat
+++ scripts/build-win-x64.bat
@@ -1,3 +1,3 @@
 dotnet clean
 dotnet restore
-dotnet publish src\NexusMods.App\NexusMods.App.csproj -c Release -r win-x64 --output bin/win-x64 --self-contained -p:PublishReadyToRun=true -p:PublishSingleFile=true
+dotnet publish src\NexusMods.App\NexusMods.App.csproj -c Release -r win-x64 --output bin/win-x64 --self-contained -p:TieredCompilation=true -p:PublishReadyToRun=true -p:PublishSingleFile=true
