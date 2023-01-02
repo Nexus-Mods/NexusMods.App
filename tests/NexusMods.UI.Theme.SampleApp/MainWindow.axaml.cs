c014828406fb8f237631df86d0cd3568aa717d35
Halgari <tbaldridge@gmail.com>
2023-01-02 13:38:01 -0700

    WIP

---
+++ tests/NexusMods.UI.Theme.SampleApp/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace NexusMods.UI.Theme.SampleApp;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
