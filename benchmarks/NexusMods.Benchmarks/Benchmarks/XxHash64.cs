90bba55b9710a961c8e70fdb16e52c95d000cc49
Sewer 56 <seweratwork@gmail.com>
2023-02-06 22:27:02 +0000

    Cleanup: Benchmark Projects (Fixes #88)

---
+++ benchmarks/NexusMods.Benchmarks/Benchmarks/XxHash64.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Attributes;
+using NexusMods.Benchmarks.Interfaces;
+using NexusMods.Hashing.xxHash64;
+
+namespace NexusMods.Benchmarks.Benchmarks;
+
+[BenchmarkInfo("Hashing", "Tests the speed of our xxHash64 implementation.")]
+[MemoryDiagnoser]
+public class XxHash64 : IBenchmark
+{
+    private readonly byte[] _oneGB;
+
+    public XxHash64()
+    {
+        _oneGB = new byte[1024 * 1024 * 1024];
+        Random.Shared.NextBytes(_oneGB);
+    }
+    
+    [Params(1024, 1024 * 1024, 1024 * 1024 * 1024)]
+    public int Size { get; set; }
+    
+    [Benchmark]
+    public void NonAsyncHash()
+    {
+        var algo = new xxHashAlgorithm(0);
+        algo.HashBytes(_oneGB.AsSpan()[..Size]);
+    }
+}
