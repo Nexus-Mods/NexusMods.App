85e39917e8414f23eb7cf0233bbb2903468b79d1
Sewer 56 <seweratwork@gmail.com>
2023-02-08 17:58:40 +0000

    Fixed: ModSortingRulesArePreserved Regression (fixes #101)

--- src/NexusMods.DataModel/Loadouts/LoadoutManager.cs
+++ src/NexusMods.DataModel/Loadouts/LoadoutManager.cs
@@ -66,9 +66,9 @@ public class LoadoutManager
             Id = ModId.New(),
             Name = "Game Files",
             Files = new EntityDictionary<ModFileId, AModFile>(_store, gameFiles.Select(g => new KeyValuePair<ModFileId, Id>(g.Id, g.DataStoreId))),
-            Store = _store
+            Store = _store,
+            SortRules = ImmutableList<ISortRule<Mod, ModId>>.Empty.Add(new First<Mod, ModId>())
         };
-        mod.SortRules.Add(new First<Mod, ModId>());
         
         var n = Loadout.Empty(_store) with
         {
