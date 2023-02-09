dfaee52eb55abde0f6f140fd4ab60171181b9960
Sewer 56 <seweratwork@gmail.com>
2023-02-09 15:38:05 +0000

    Added: Sample benchmark for Signature Scanning

---
+++ benchmarks/NexusMods.Benchmarks/Assets.cs
@@ -0,0 +1,11 @@
+using NexusMods.Paths;
+
+namespace NexusMods.Benchmarks;
+
+public class Assets
+{
+    public static string ProgramFolder => KnownFolders.EntryFolder.ToString();
+    public static string AssetsFolder => Path.Combine(ProgramFolder, "Assets");
+    public static string Sample7zFile => Path.Combine(AssetsFolder, "data_7zip_lzma2.7z");
+    public static string SampleZipFile => Path.Combine(AssetsFolder, "data_zip_lzma.zip");
+}
