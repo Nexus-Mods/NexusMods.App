cd9be387a2fed41174e5d8e3fa3c6115a657e24f
Sewer 56 <seweratwork@gmail.com>
2023-02-09 18:08:24 +0000

    Added: Missing Documentation on Services

--- src/ArchiveManagement/NexusMods.FileExtractor/FileExtractionException.cs
+++ src/ArchiveManagement/NexusMods.FileExtractor/FileExtractionException.cs
@@ -1,7 +1,8 @@
 ﻿namespace NexusMods.FileExtractor;
 
+/// <inheritdoc />
 public class FileExtractionException : Exception
 {
+    /// <inheritdoc />
     public FileExtractionException(string message) : base(message) {}
-
 }
--- src/ArchiveManagement/NexusMods.FileExtractor/Services.cs
+++ src/ArchiveManagement/NexusMods.FileExtractor/Services.cs
@@ -3,13 +3,20 @@ using NexusMods.FileExtractor.Extractors;
 
 namespace NexusMods.FileExtractor;
 
+/// <summary>
+/// Functionality related to Dependency Injection.
+/// </summary>
 public static class Services
 {
+    /// <summary>
+    /// Adds file extraction related services to the provided DI container.
+    /// </summary>
+    /// <param name="coll">Service collection to register.</param>
+    /// <returns>Service collection passed as parameter.</returns>
     public static IServiceCollection AddFileExtractors(this IServiceCollection coll)
     {
         coll.AddSingleton<FileExtractor>();
         coll.AddSingleton<IExtractor, SevenZipExtractor>();
         return coll;
     }
-    
 }
