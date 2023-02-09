b69d0dc8b23755c154119443832187939dbca240
Sewer 56 <seweratwork@gmail.com>
2023-02-09 11:08:18 +0000

    Refactored: ForEachEntryAsync + ExtractAllAsync w/ Deduplicated Code

--- src/NexusMods.Paths/AbsolutePath.cs
+++ src/NexusMods.Paths/AbsolutePath.cs
@@ -398,8 +398,7 @@ public struct AbsolutePath : IPath, IComparable<AbsolutePath>, IEquatable<Absolu
         new(Parts[..1], PathFormat) : 
         new AbsolutePath(Array.Empty<string>(), PathFormat);
 
-    public readonly IEnumerable<AbsolutePath> EnumerateFiles(string pattern = "*",
-        bool recursive = true)
+    public readonly IEnumerable<AbsolutePath> EnumerateFiles(string pattern = "*", bool recursive = true)
     {
         return Directory.EnumerateFiles(ToNativePath(), pattern,
                 recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly)
