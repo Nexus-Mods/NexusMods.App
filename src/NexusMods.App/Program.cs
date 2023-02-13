d3a044b4cb8171304edd0b809f5e1ef81b9efbb6
Timothy Baldridge <tbaldridge@gmail.com>
2023-02-13 11:45:10 -0700

    Fix SimpleHttpDownloader.cs for StressTests (#113)

--- src/NexusMods.App/Program.cs
+++ src/NexusMods.App/Program.cs
@@ -37,7 +37,7 @@ var host = Host.CreateDefaultBuilder(Environment.GetCommandLineArgs())
             .AddStandardGameLocators()
             .AddRenderers()
             .AddNexusWebApi()
-            .AddAdvancedHttpDownloader()
+            .AddHttpDownloader()
             .AddRenderers()
             .AddTestHarness();
 
