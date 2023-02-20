fcac864d18513100c4156172f32bdf907f93b4e8
Halgari <tbaldridge@gmail.com>
2023-02-20 15:24:59 -0700

    Exposed properties outside of the UserControl on the buttons

--- src/NexusMods.UI.Theme/Controls/SpineGame.axaml.cs
+++ src/NexusMods.UI.Theme/Controls/SpineGame.axaml.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
 using Avalonia.Data;
 using Avalonia.Markup.Xaml;
 using Avalonia.Media;
@@ -10,24 +11,27 @@ public partial class SpineGame : UserControl
 {
     public static readonly StyledProperty<IImage> SourceProperty = AvaloniaProperty.Register<SpineGame, IImage>(nameof(Source));
     
-    public static readonly DirectProperty<SpineGame, bool> IsCheckedProperty = AvaloniaProperty.RegisterDirect<SpineGame, bool>(nameof(IsChecked),
-        x => x.IsChecked, (x, v) => x.IsChecked = v, unsetValue: false, defaultBindingMode: BindingMode.TwoWay);
-    
+    public static readonly DirectProperty<SpineGame, bool?> IsCheckedProperty = AvaloniaProperty.RegisterDirect<SpineGame, bool?>(nameof(IsChecked),
+        x => x._toggle?.IsChecked, (x, v) => x._toggle!.IsChecked = v, unsetValue: false, defaultBindingMode: BindingMode.TwoWay);
+
+    private readonly ToggleButton? _toggle;
+
     public IImage Source
     {
         get => GetValue(SourceProperty);
         set => SetValue(SourceProperty, value);
     }
-    
-    public bool IsChecked
+
+    public bool? IsChecked
     {
         get => GetValue(IsCheckedProperty);
         set => SetValue(IsCheckedProperty, value);
     }
-
+    
     public SpineGame()
     {
         InitializeComponent();
+        _toggle = this.Find<ToggleButton>("ToggleButton");
     }
 
 
