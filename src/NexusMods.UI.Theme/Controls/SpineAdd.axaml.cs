41b12ac938d6f951e134114775f4009632e9620a
Halgari <tbaldridge@gmail.com>
2023-02-21 09:20:18 -0700

    Add "add" buttons with proper styling

--- src/NexusMods.UI.Theme/Controls/SpineAdd.axaml.cs
+++ src/NexusMods.UI.Theme/Controls/SpineAdd.axaml.cs
@@ -1,15 +1,30 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
 
 namespace NexusMods.UI.Theme.Controls;
 
 public partial class SpineAdd : UserControl
 {
-    public SpineAdd()
+    public static readonly DirectProperty<SpineAdd, bool?> IsCheckedProperty = AvaloniaProperty.RegisterDirect<SpineAdd, bool?>(nameof(IsChecked),
+        x => x._toggle?.IsChecked, (x, v) => x._toggle!.IsChecked = v, unsetValue: false, defaultBindingMode: BindingMode.TwoWay);
+
+    private readonly ToggleButton? _toggle;
+
+    public bool? IsChecked
     {
-        InitializeComponent();
+        get => GetValue(IsCheckedProperty);
+        set => SetValue(IsCheckedProperty, value);
     }
+    
+    public SpineAdd()    {
+        AffectsRender<SpineAdd>(IsCheckedProperty);
+        InitializeComponent();
+        _toggle = this.Find<ToggleButton>("ToggleButton");
+    }
+
 
     private void InitializeComponent()
     {
