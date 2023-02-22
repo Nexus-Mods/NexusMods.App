9e65b18e9bbf8c460c6445be2b8bf9c7b4378836
TanninOne <seppleviathan@gmx.de>
2023-02-22 11:02:33 +0100

    platform abstraction for opening urls in default application
    includes testable abstraction for process creation

---
+++ src/NexusMods.Common/OSInteropOSX.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+
+namespace NexusMods.Common;
+
+/// <summary>
+/// OS interoperation for MacOS
+/// </summary>
+public class OSInteropOSX : IOSInterop
+{
+    private readonly IProcessFactory _processFactory;
+    /// <summary>
+    /// constructor
+    /// </summary>
+    /// <param name="processFactory"></param>
+    public OSInteropOSX(IProcessFactory processFactory)
+    {
+        _processFactory = processFactory;
+    }
+
+    /// <inheritdoc/>
+    public void OpenURL(string url)
+    {
+        _processFactory.Start("open", url);
+    }
+}
