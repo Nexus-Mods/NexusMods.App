a964a976a9a6ea2b9ecb1278a20f85cffb0b6163
Halgari <tbaldridge@gmail.com>
2023-02-28 12:57:02 -0700

    Add TopBarView, Logo, and Login button

---
+++ src/NexusMods.App.UI/Controls/TopBar/TopBarView.axaml.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NexusMods.App.UI.Controls.TopBar;
+
+public partial class TopBarView : UserControl
+{
+    public TopBarView()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
