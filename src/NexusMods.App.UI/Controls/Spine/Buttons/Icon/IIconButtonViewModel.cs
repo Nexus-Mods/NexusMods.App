0d851cde0d8eade659ed28d9855e126e1e49fa16
Halgari <tbaldridge@gmail.com>
2023-03-02 13:30:09 -0700

    Add icon button

---
+++ src/NexusMods.App.UI/Controls/Spine/Buttons/Icon/IIconButtonViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Input;
+
+namespace NexusMods.App.UI.Controls.Spine.Buttons.Icon;
+
+public interface IIconButtonViewModel : IViewModelInterface
+{
+    /// <summary>
+    /// Is the button active?
+    /// </summary>
+    public bool IsActive { get; }
+
+    /// <summary>
+    /// Command to execute when the button is clicked.
+    /// </summary>
+    public ICommand Click { get; }
+
+}
---
+++ src/NexusMods.App.UI/Controls/Spine/Buttons/Icon/IconButtonViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+using NexusMods.App.UI.ViewModels;
+using ReactiveUI.Fody.Helpers;
+
+namespace NexusMods.App.UI.Controls.Spine.Buttons.Icon;
+
+public class IconButtonViewModel : AViewModel<IIconButtonViewModel>, IIconButtonViewModel
+{
+    [Reactive]
+    public bool IsActive { get; set; }
+
+    [Reactive]
+    public ICommand Click { get; set; }
+}
