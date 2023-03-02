d89a0e987b9bf1ca72239ad58da10e15e2d133e6
Sewer 56 <seweratwork@gmail.com>
2023-03-02 16:52:35 +0000

    Suppress Inconsistent Naming in R# for 7zip

--- benchmarks/NexusMods.Benchmarks/Assets.cs
+++ benchmarks/NexusMods.Benchmarks/Assets.cs
@@ -6,6 +6,7 @@ public class Assets
 {
     public static string ProgramFolder => KnownFolders.EntryFolder.ToString();
     public static string AssetsFolder => Path.Combine(ProgramFolder, "Assets");
+    // ReSharper disable once InconsistentNaming
     public static string Sample7zFile => Path.Combine(AssetsFolder, "data_7zip_lzma2.7z");
     public static string SampleZipFile => Path.Combine(AssetsFolder, "data_zip_lzma.zip");
 }
