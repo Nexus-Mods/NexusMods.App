be739525fbce81af6477489c384a9889d0783f01
Sewer 56 <seweratwork@gmail.com>
2023-03-03 17:50:42 +0000

    Added: Documentation in EntityLink Type

--- src/NexusMods.DataModel/Abstractions/EntityLink.cs
+++ src/NexusMods.DataModel/Abstractions/EntityLink.cs
@@ -3,26 +3,36 @@ using System.Text.Json.Serialization;
 
 namespace NexusMods.DataModel.Abstractions;
 
+/// <summary>
+/// This record holds a reference to another entity in the data store.
+/// This is essentially a wrapper around an ID.
+/// </summary>
+/// <typeparam name="T">Type of entity this entity links to </typeparam>
 [JsonConverter(typeof(EntityLinkConverterFactory))]
 public record EntityLink<T> : IEmptyWithDataStore<EntityLink<T>>,
     IWalkable<Entity>
     where T : Entity
 {
-
-
     [JsonIgnore]
-    private T? _value = null;
+    private T? _value;
+
+    /// <summary>
+    /// ID of the element in the data store.
+    /// </summary>
     public Id Id { get; }
 
+    /// <summary>
+    /// Retrieves the item from the data store.
+    /// </summary>
     [JsonIgnore]
     public T Value => Get();
-
-    public static implicit operator T(EntityLink<T> t) => t.Value;
-    public static implicit operator EntityLink<T>(T t) => new(t.DataStoreId, t.Store);
 
     [JsonIgnore]
     private readonly IDataStore _store;
 
+    /// <summary/>
+    /// <param name="id">The ID to link to within the database.</param>
+    /// <param name="store">The store in which the link will be held.</param>
     public EntityLink(Id id, IDataStore store)
     {
         Id = id;
@@ -35,46 +45,65 @@ public record EntityLink<T> : IEmptyWithDataStore<EntityLink<T>>,
         return _value!;
     }
 
+    /// <inheritdoc />
     public static EntityLink<T> Empty(IDataStore store) => new(IdEmpty.Empty, store);
+
+    /// <inheritdoc />
     public TState Walk<TState>(Func<TState, Entity, TState> visitor, TState initial)
     {
         return Id is IdEmpty ? initial : visitor(initial, Value);
     }
+
+    /// <summary/>
+    public static implicit operator T(EntityLink<T> t) => t.Value;
+
+    /// <summary/>
+    public static implicit operator EntityLink<T>(T t) => new(t.DataStoreId, t.Store);
 }
 
+/// <inheritdoc />
 public class EntityLinkConverterFactory : JsonConverterFactory
 {
     private readonly IServiceProvider _provider;
 
+    /// <inheritdoc />
     public EntityLinkConverterFactory(IServiceProvider provider)
     {
         _provider = provider;
     }
+
+    /// <inheritdoc />
     public override bool CanConvert(Type typeToConvert) =>
         typeToConvert.GenericTypeArguments.Length == 1 &&
         typeToConvert.GetGenericTypeDefinition() == typeof(EntityLink<>);
 
-    public override JsonConverter? CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+    /// <inheritdoc />
+    public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
     {
         return (JsonConverter)_provider.GetService(typeof(EntityLinkConverter<>).MakeGenericType(typeToConvert.GenericTypeArguments))!;
     }
 }
 
+/// <inheritdoc />
 public class EntityLinkConverter<T> : JsonConverter<EntityLink<T>>
     where T : Entity
 {
     private readonly IDataStore _store;
 
+    /// <inheritdoc />
     public EntityLinkConverter(IDataStore store)
     {
         _store = store;
     }
-    public override EntityLink<T>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+
+    /// <inheritdoc />
+    public override EntityLink<T> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
         var id = JsonSerializer.Deserialize<Id>(ref reader, options)!;
         return new EntityLink<T>(id, _store);
     }
 
+    /// <inheritdoc />
     public override void Write(Utf8JsonWriter writer, EntityLink<T> value, JsonSerializerOptions options)
     {
         JsonSerializer.Serialize(writer, value.Id, options);
