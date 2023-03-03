2f32b3431bf7724076f0cf2e3ddb0fd4c11650f4
Sewer 56 <seweratwork@gmail.com>
2023-03-03 10:23:02 +0000

    Update: IWalkable Description for better IDE Display

--- src/NexusMods.DataModel/Abstractions/IWalkable.cs
+++ src/NexusMods.DataModel/Abstractions/IWalkable.cs
@@ -7,14 +7,13 @@ namespace NexusMods.DataModel.Abstractions;
 public interface IWalkable<out TItem>
 {
     /// <summary>
-    /// Goes over each item in the collection, firing your callback function.
-    /// A 'state' parameter can be specified, which can hold shared state between individual callback invocations.
+    /// Goes over each item in the collection, firing your callback function.<br/>
+    /// A 'state' parameter can be specified, which can hold shared state between individual callback invocations.<br/><br/>
     ///
     /// This method effectively is equivalent to LINQ's <see cref="Enumerable.Aggregate{TSource}"/>.
     /// </summary>
     /// <param name="visitor">The function to call for each item in the collection.</param>
     /// <param name="initial">The item that holds the state.</param>
     /// <typeparam name="TState">The initial state.</typeparam>
-    /// <returns></returns>
     public TState Walk<TState>(Func<TState, TItem, TState> visitor, TState initial);
 }
