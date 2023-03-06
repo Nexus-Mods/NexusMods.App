7821f74baaecfc54411b45e53606a782ccc0e536
Sewer 56 <seweratwork@gmail.com>
2023-03-06 18:00:17 +0000

    Added: Docs for File Analysis.

--- src/NexusMods.DataModel/Abstractions/IFileAnalyzer.cs
+++ src/NexusMods.DataModel/Abstractions/IFileAnalyzer.cs
@@ -2,8 +2,24 @@ using NexusMods.FileExtractor.FileSignatures;
 
 namespace NexusMods.DataModel.Abstractions;
 
+/// <summary>
+/// Provides an abstraction over a component used to analyze files.<br/><br/>
+///
+/// A file analyzer reads data from a given stream, and extracts metadata necessary
+/// for. Such as dependency information.
+/// </summary>
 public interface IFileAnalyzer
 {
+    /// <summary>
+    /// Defines the file types supported by this file analyzer.
+    /// </summary>
     public IEnumerable<FileType> FileTypes { get; }
+
+    /// <summary>
+    /// Asynchronously analyzes a file exposed by a given stream.
+    /// </summary>
+    /// <param name="stream">The stream to be analyzed; be it a file or an archive.</param>
+    /// <param name="ct">Allows you to cancel the operation.</param>
+    /// <returns>Analysis data for the processed files.</returns>
     public IAsyncEnumerable<IFileAnalysisData> AnalyzeAsync(Stream stream, CancellationToken ct = default);
 }
