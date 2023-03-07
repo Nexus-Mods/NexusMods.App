204db6f3686c46c93c7e30e79731f859f29d107b
Sewer 56 <seweratwork@gmail.com>
2023-03-07 18:55:22 +0000

    Added: Remaining Documentation for NexusMods.DataModel.Games

--- src/NexusMods.DataModel/Games/GameDomain.cs
+++ src/NexusMods.DataModel/Games/GameDomain.cs
@@ -2,8 +2,16 @@ using Vogen;
 
 namespace NexusMods.DataModel.Games;
 
+/// <summary>
+/// Machine friendly name for the game, should be devoid of special characters
+/// that may conflict with URLs or file paths.
+///
+/// Sometimes used as a Game ID.
+/// </summary>
+/// <remarks>
+///    Usually we match these with NexusMods' URLs.
+/// </remarks>
 [ValueObject<string>]
 [Instance("Cyberpunk2077", "cyberpunk2077")]
-public partial class GameDomain
-{
-}
+// ReSharper disable once PartialTypeWithSinglePart
+public partial class GameDomain { }
