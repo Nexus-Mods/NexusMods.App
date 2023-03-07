c0c42daf053f815389c3b77e5cccfda09f85acbe
Sewer 56 <seweratwork@gmail.com>
2023-03-07 16:13:36 +0000

    Added: Documentation for AnalyzedArchive, AnalyzedFile & FileContainedIn
    Also fixed *.cs file name. for AnalyzedArchive

--- src/NexusMods.DataModel/ArchiveContents/FileContainedIn.cs
+++ src/NexusMods.DataModel/ArchiveContents/FileContainedIn.cs
@@ -6,13 +6,28 @@ using NexusMods.Paths;
 
 namespace NexusMods.DataModel.ArchiveContents;
 
+/// <summary>
+///
+/// </summary>
 [JsonName("FileContainedIn")]
 public record FileContainedIn : Entity
 {
+    /// <summary>
+    /// Hash of the individual file, corresponding to an <see cref="AnalyzedFile"/>.
+    /// </summary>
     public required Hash File { get; init; }
+
+    /// <summary>
+    /// Hash of the parent element [usually an <see cref="AnalyzedArchive"/>].
+    /// </summary>
     public required Hash Parent { get; init; }
+
+    /// <summary>
+    /// Relative path of the file inside the parent archive.
+    /// </summary>
     public required RelativePath Path { get; init; }
 
+    /// <inheritdoc />
     protected override IId Persist()
     {
         var id = new TwoId64(Category, (ulong)File, (ulong)Parent);
@@ -20,5 +35,6 @@ public record FileContainedIn : Entity
         return id;
     }
 
+    /// <inheritdoc />
     public override EntityCategory Category => EntityCategory.FileContainedIn;
 }
