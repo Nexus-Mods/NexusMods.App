4f0fb22c755ab4c5316ecc2dae0efaafec444fee
Sewer 56 <seweratwork@gmail.com>
2023-03-10 21:42:52 +0000

    Added: Dummy docs for sorting rules

--- src/NexusMods.DataModel/Sorting/Rules/After.cs
+++ src/NexusMods.DataModel/Sorting/Rules/After.cs
@@ -2,5 +2,6 @@ using NexusMods.DataModel.JsonConverters;
 
 namespace NexusMods.DataModel.Sorting.Rules;
 
+/// <summary />
 [JsonName("After")]
 public record After<TType, TId>(TId Other) : ISortRule<TType, TId>;
--- src/NexusMods.DataModel/Sorting/Rules/Before.cs
+++ src/NexusMods.DataModel/Sorting/Rules/Before.cs
@@ -2,5 +2,6 @@ using NexusMods.DataModel.JsonConverters;
 
 namespace NexusMods.DataModel.Sorting.Rules;
 
+/// <summary />
 [JsonName("Before")]
 public record Before<TType, TId>(TId Other) : ISortRule<TType, TId>;
