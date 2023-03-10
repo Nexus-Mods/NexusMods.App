71756f46b7f6e4e732de9d552b797d483c4f4e35
Timothy Baldridge <tbaldridge@gmail.com>
2023-03-10 06:35:19 -0700

    Rework the IPC system (#182)
    * Rework the IPC system to not use memory mapped files (they're so brittle). It's all based on SQLite now, and has a self-cleaning task that runs periodically.
    
    * Add a long poll interval as well

--- src/NexusMods.DataModel/Interprocess/InterprocessProducer.cs
+++ src/NexusMods.DataModel/Interprocess/InterprocessProducer.cs
@@ -1,4 +1,3 @@
-using Cloudtoid.Interprocess;
 using Microsoft.Extensions.Logging;
 
 namespace NexusMods.DataModel.Interprocess;
@@ -7,27 +6,20 @@ namespace NexusMods.DataModel.Interprocess;
 /// Implementation of <see cref="IMessageProducer{T}"/> that uses an interprocess queue.
 /// </summary>
 /// <typeparam name="T"></typeparam>
-public class InterprocessProducer<T> : IMessageProducer<T>, IDisposable where T : IMessage
+public class InterprocessProducer<T> : IMessageProducer<T> where T : IMessage
 {
-    private readonly ILogger<InterprocessProducer<T>> _logger;
-    private readonly IQueueFactory _queueFactory;
-    private readonly IPublisher _queue;
     private readonly string _queueName;
+    private readonly SqliteIPC _sqliteIpc;
 
     /// <summary>
     /// DI Constructor
     /// </summary>
     /// <param name="logger">Logger</param>
     /// <param name="queueFactory">Queue Factory</param>
-    public InterprocessProducer(ILogger<InterprocessProducer<T>> logger, IQueueFactory queueFactory)
+    public InterprocessProducer(ILogger<InterprocessProducer<T>> logger, SqliteIPC sqliteIpc)
     {
-        _queueFactory = queueFactory;
-        _logger = logger;
-        _queueName = "NexusMods.DataModel.Interprocess." + typeof(T).Name;
-
-        _queue = queueFactory.CreatePublisher(new QueueOptions(
-            _queueName,
-            T.MaxSize * 16));
+        _queueName = typeof(T).Name;
+        _sqliteIpc = sqliteIpc;
     }
 
     /// <summary>
@@ -36,22 +28,10 @@ public class InterprocessProducer<T> : IMessageProducer<T>, IDisposable where T 
     /// <param name="message"></param>
     /// <param name="token"></param>
     /// <exception cref="TaskCanceledException"></exception>
-    public async ValueTask Write(T message, CancellationToken token)
+    public ValueTask Write(T message, CancellationToken token)
     {
-        _logger.LogTrace("Writing message {Message} to queue {Queue}", message, _queueName);
-
-        while (!token.IsCancellationRequested)
-        {
-            if (WriteInner(message))
-            {
-                return;
-            }
-
-            _logger.LogDebug("Failed to write {Message} to queue {Queue}, retrying", message, _queueName);
-            await Task.Delay(100, token);
-        }
-
-        throw new TaskCanceledException();
+        WriteInner(message);
+        return ValueTask.CompletedTask;
     }
 
     /// <summary>
@@ -59,17 +39,12 @@ public class InterprocessProducer<T> : IMessageProducer<T>, IDisposable where T 
     /// </summary>
     /// <param name="message"></param>
     /// <returns></returns>
-    private bool WriteInner(T message)
+    private void WriteInner(T message)
     {
         // This means we re-encode the message on every attempt, but it is assumed that failed messages
         // are rare and the cost of re-encoding is negligible.
         Span<byte> buffer = stackalloc byte[T.MaxSize];
         var used = message.Write(buffer);
-        return _queue.TryEnqueue(buffer[..used]);
-    }
-
-    public void Dispose()
-    {
-        _queue.Dispose();
+        _sqliteIpc.Send(_queueName, buffer[..used]);
     }
 }
