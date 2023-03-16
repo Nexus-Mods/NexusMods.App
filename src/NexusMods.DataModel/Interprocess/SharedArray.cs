31a76200009a39e6945e657684e88eb4788dc2a7
Halgari <tbaldridge@gmail.com>
2023-03-16 16:40:28 -0600

    Implement a shared IPC array

---
+++ src/NexusMods.DataModel/Interprocess/SharedArray.cs
@@ -0,0 +1,116 @@
+﻿using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using NexusMods.Hashing.xxHash64;
+using NexusMods.Paths;
+
+namespace NexusMods.DataModel.Interprocess;
+
+/// <summary>
+/// A shared array of 64-bit unsigned integers, supports atomic operations via
+/// a CAS operation on a mmaped file.
+/// </summary>
+public unsafe class SharedArray : IDisposable
+{
+    private readonly int _totalSize;
+    private Stream _stream;
+    private MemoryMappedFile _mmapFile;
+    private MemoryMappedViewAccessor _view;
+
+    public SharedArray(AbsolutePath path, int itemCount)
+    {
+        _totalSize = itemCount * sizeof(ulong);
+
+        while (true)
+        {
+            try
+            {
+                // Make sure the a previous process didn't exit without setting the file size
+                if (path.FileExists)
+                {
+                    var tmpStream =
+                        path.Open(FileMode.Open, FileAccess.ReadWrite);
+                    tmpStream.Position = _totalSize;
+                    tmpStream.Close();
+                }
+                else
+                {
+                    var tmpStream = path.Open(FileMode.CreateNew,
+                        FileAccess.ReadWrite);
+                    tmpStream.Position = _totalSize;
+                    tmpStream.Close();
+                }
+                break;
+            }
+            catch (IOException)
+            {
+                continue;
+            }
+        }
+
+        _stream = path.Open(FileMode.Open, FileAccess.ReadWrite);
+        _mmapFile = MemoryMappedFile.CreateFromFile((FileStream)_stream,
+            path.GetFullPath().ToLower().XxHash64AsUtf8().ToHex(),
+            _totalSize,
+            MemoryMappedFileAccess.ReadWrite,
+            HandleInheritability.Inheritable,
+            false);
+
+        _view = _mmapFile.CreateViewAccessor(0, _totalSize, MemoryMappedFileAccess.ReadWrite);
+    }
+
+    /// <summary>
+    /// Get the value at the given index
+    /// </summary>
+    /// <param name="idx"></param>
+    /// <returns></returns>
+    public ulong Get(int idx)
+    {
+#if DEBUG
+        if (idx < 0 || idx >= _totalSize / sizeof(ulong))
+        {
+            throw new ArgumentOutOfRangeException(nameof(idx));
+        }
+#endif
+        _view.Read(idx * sizeof(ulong), out ulong value);
+        return value;
+    }
+
+    /// <summary>
+    /// Set the value at the given index to the given value if the current value is the expected value.
+    /// Returns true if the value was set, false otherwise.
+    /// </summary>
+    /// <param name="idx"></param>
+    /// <param name="expected"></param>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    public bool CompareAndSwap(int idx, ulong expected, ulong value)
+    {
+#if DEBUG
+        if (idx < 0 || idx >= _totalSize / sizeof(ulong))
+        {
+            throw new ArgumentOutOfRangeException(nameof(idx));
+        }
+#endif
+
+        try
+        {
+            byte* ptr = null;
+            _view.SafeMemoryMappedViewHandle.AcquirePointer(ref ptr);
+            var result = Interlocked.CompareExchange(ref *(ulong*)(ptr + idx * sizeof(ulong)), value, expected);
+            return result == expected;
+        }
+        finally
+        {
+            _view.SafeMemoryMappedViewHandle.ReleasePointer();
+        }
+    }
+
+    /// <inheritdoc />
+    public void Dispose()
+    {
+        _view.Dispose();
+        _mmapFile.Dispose();
+        _stream.Dispose();
+    }
+}
---
+++ tests/NexusMods.DataModel.Tests/SharedArrayTests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using NexusMods.DataModel.Interprocess;
+using NexusMods.Paths;
+
+namespace NexusMods.DataModel.Tests;
+
+public class SharedArrayTests
+{
+    private readonly TemporaryFileManager _temporaryFileManager;
+    private readonly TemporaryPath _file;
+
+    private const int ThreadCount = 16;
+
+    public SharedArrayTests(TemporaryFileManager temporaryFileManager)
+    {
+        _temporaryFileManager = temporaryFileManager;
+
+        _file = _temporaryFileManager.CreateFile();
+    }
+
+    [Fact]
+    public void CanPerformSimpleCasOperationsFromManyThreads()
+    {
+        var arraySize = 16;
+        ulong iterations = 1024 * 16;
+        using var array = new SharedArray(_file, arraySize);
+        var threads = new Thread[ThreadCount];
+
+        // Brute force CAS operations and make sure the final value is correct
+        for (var i = 0; i < ThreadCount; i++)
+        {
+            threads[i] = new Thread(() =>
+            {
+                for (ulong loop = 0; loop < iterations; loop++)
+                {
+                    for (var idx = 0; idx < arraySize; idx++)
+                    {
+                        var oldValue = array.Get(idx);
+                        var newValue = oldValue + 1;
+                        while (!array.CompareAndSwap(idx, oldValue, newValue))
+                        {
+                            oldValue = array.Get(idx);
+                            newValue = oldValue + 1;
+                        }
+                    }
+                }
+            });
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Start();
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        for (var i = 0; i < arraySize; i++)
+        {
+            array.Get(1).Should().Be(ThreadCount * iterations);
+        }
+    }
+}
