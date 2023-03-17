26060dd228aa11373b4aff3c601bd7e8d1dce503
erri120 <erri120@protonmail.com>
2023-03-17 10:28:40 +0100

    Update FS enumerators

--- src/NexusMods.Paths/FileSystemAbstraction/RealFileSystem/FilesEnumeratorEx.cs
+++ src/NexusMods.Paths/FileSystemAbstraction/RealFileSystem/FilesEnumeratorEx.cs
@@ -5,24 +5,29 @@ namespace NexusMods.Paths;
 
 internal sealed class FilesEnumeratorEx : FileSystemEnumerator<FilesEnumeratorExEntry>
 {
-    public string CurrentDirectory { get; private set; }
+    private string? _currentDirectory;
+    public string CurrentDirectory => _currentDirectory ?? _startDirectory;
 
+    private readonly string _startDirectory;
     private readonly string _pattern;
     private readonly EnumerationOptions _options;
 
     public FilesEnumeratorEx(string directory, string pattern, EnumerationOptions options) : base(directory, options)
     {
-        CurrentDirectory = directory;
+        _startDirectory = directory;
         _pattern = pattern;
         _options = options;
     }
+
+    protected override void OnDirectoryFinished(ReadOnlySpan<char> directory)
+        => _currentDirectory = null;
 
     protected override bool ShouldIncludeEntry(ref FileSystemEntry entry)
         => EnumeratorHelpers.MatchesPattern(_pattern, entry.FileName, _options.MatchType);
 
     protected override FilesEnumeratorExEntry TransformEntry(ref FileSystemEntry entry)
     {
-        CurrentDirectory = entry.Directory.ToString();
+        _currentDirectory ??= entry.Directory.ToString();
         return new FilesEnumeratorExEntry(entry.FileName.ToString(), Size.From(entry.Length), entry.LastWriteTimeUtc.DateTime, entry.IsDirectory);
     }
 }
