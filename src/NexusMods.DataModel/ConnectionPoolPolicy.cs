2cae4a34cd5c0a0990f7843e8ffffa336789d581
Halgari <tbaldridge@gmail.com>
2023-03-27 11:55:03 -0600

    doc updates

--- src/NexusMods.DataModel/ConnectionPoolPolicy.cs
+++ src/NexusMods.DataModel/ConnectionPoolPolicy.cs
@@ -4,15 +4,25 @@ using NexusMods.Paths;
 
 namespace NexusMods.DataModel;
 
+/// <summary>
+/// Simple connection pool policy that opens a connection when it is created
+/// since the pool automatically disposes of the connection when it is no longer
+/// needed we don't need to do that here.
+/// </summary>
 public class ConnectionPoolPolicy : IPooledObjectPolicy<SqliteConnection>
 {
     private readonly string _connectionString;
 
+    /// <summary>
+    /// Constructor that takes the connection string used when creating new connections
+    /// </summary>
+    /// <param name="connectionString"></param>
     public ConnectionPoolPolicy(string connectionString)
     {
         _connectionString = connectionString;
     }
 
+    /// <inheritdoc />
     public SqliteConnection Create()
     {
         var conn = new SqliteConnection(_connectionString);
@@ -20,6 +30,7 @@ public class ConnectionPoolPolicy : IPooledObjectPolicy<SqliteConnection>
         return conn;
     }
 
+    /// <inheritdoc />
     public bool Return(SqliteConnection obj)
     {
         return true;
--- src/NexusMods.DataModel/Extensions/SqliteParameterCollectionExtensions.cs
+++ src/NexusMods.DataModel/Extensions/SqliteParameterCollectionExtensions.cs
@@ -3,8 +3,18 @@ using NexusMods.DataModel.Abstractions.Ids;
 
 namespace NexusMods.DataModel.Extensions;
 
+/// <summary>
+/// Extension methods for SqliteParameterCollection
+/// </summary>
 public static class SqliteParameterCollectionExtensions
 {
+    /// <summary>
+    /// Adds a parameter to the collection with the given name and an id value
+    /// stores the id as a blob without a tag
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="name"></param>
+    /// <param name="id"></param>
     public static void AddWithValueUntagged(this SqliteParameterCollection collection, string name, IId id)
     {
         var idBytes = new byte[id.SpanSize];
@@ -12,6 +22,13 @@ public static class SqliteParameterCollectionExtensions
         collection.AddWithValue(name, idBytes);
     }
 
+    /// <summary>
+    /// Adds a parameter to the collection with the given name and an id value
+    /// stores the id as a blob with a tag
+    /// </summary>
+    /// <param name="collection"></param>
+    /// <param name="name"></param>
+    /// <param name="id"></param>
     public static void AddWithValueTagged(this SqliteParameterCollection collection, string name, IId id)
     {
         var idBytes = new byte[id.SpanSize + 1];
