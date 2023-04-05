04691825f5f45a7f3c5ef4d67b34ab21297a6ac4
erri120 <erri120@protonmail.com>
2023-04-05 16:10:33 +0200

    Simplified Startup files

--- tests/Games/NexusMods.Games.RedEngine.Tests/Startup.cs
+++ tests/Games/NexusMods.Games.RedEngine.Tests/Startup.cs
@@ -1,14 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NexusMods.Common;
-using NexusMods.DataModel;
-using NexusMods.DataModel.RateLimiting;
-using NexusMods.FileExtractor;
-using NexusMods.FileExtractor.Extractors;
-using NexusMods.Networking.HttpDownloader;
-using NexusMods.Networking.NexusWebApi;
-using NexusMods.Paths;
-using NexusMods.Paths.Utilities;
+using NexusMods.Games.TestFramework;
 using NexusMods.StandardGameLocators.TestHelpers;
 using Xunit.DependencyInjection;
 using Xunit.DependencyInjection.Logging;
@@ -19,24 +12,11 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection container)
     {
-        var prefix = KnownFolders.EntryFolder.CombineUnchecked("DataModel").CombineUnchecked(Guid.NewGuid().ToString());
-        container.AddUniversalGameLocator<Cyberpunk2077>(new Version("1.61"))
-                 .AddRedEngineGames()
-                 .AddNexusWebApi(true)
-                 .AddHttpDownloader()
-
-                 .AddSingleton<HttpClient>()
-                 .AddLogging(builder => builder.SetMinimumLevel(LogLevel.Debug))
-                 .AddDataModel(new DataModelSettings(prefix))
-                 .AddAllSingleton<IResource, IResource<FileContentsCache, Size>>(_ =>
-            new Resource<FileContentsCache, Size>("File Analysis"))
-                 .AddAllSingleton<IResource, IResource<IExtractor, Size>>(_ =>
-            new Resource<IExtractor, Size>("File Extraction"))
-                 .AddFileExtractors(new FileExtractorSettings())
-
-                 .AddSingleton<TemporaryFileManager>(_ =>
-            new TemporaryFileManager(KnownFolders.EntryFolder.CombineUnchecked("tempFiles")))
-                 .Validate();
+        container
+            .AddDefaultServicesForTesting()
+            .AddUniversalGameLocator<Cyberpunk2077>(new Version("1.61"))
+            .AddRedEngineGames()
+            .Validate();
     }
 
     public void Configure(ILoggerFactory loggerFactory, ITestOutputHelperAccessor accessor) =>
