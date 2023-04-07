96fa90f48e3af3d3bf1fe0ea800b5bbe5c719423
Sewer 56 <seweratwork@gmail.com>
2023-04-07 05:35:23 +0100

    Added: Unit test for Light Master

--- tests/Games/NexusMods.Games.BethesdaGameStudios.Tests/FileAnalysisTests.cs
+++ tests/Games/NexusMods.Games.BethesdaGameStudios.Tests/FileAnalysisTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using NexusMods.DataModel;
+using NexusMods.DataModel.ArchiveContents;
 using NexusMods.Paths;
 using NexusMods.Paths.Extensions;
 using NexusMods.Paths.Utilities;
@@ -9,26 +10,25 @@ namespace NexusMods.Games.BethesdaGameStudios.Tests;
 [Trait("RequiresGameInstalls", "True")] // Technically this doesn't require the game, but the DI system does for the other tests
 public class FileAnalysisTests
 {
-
     private readonly FileContentsCache _cache;
     private readonly AbsolutePath _plugin1;
-
-    // TODO: Write tests using _plugin2.
-    // ReSharper disable once NotAccessedField.Local
     private readonly AbsolutePath _plugin2;
 
-    public FileAnalysisTests(FileContentsCache cache)
+    public FileAnalysisTests(FileContentsCache cache, IFileSystem fileSystem)
     {
         _cache = cache;
-        _plugin1 = KnownFolders.EntryFolder.CombineUnchecked("Resources").CombineUnchecked("testfile1.esp");
-        _plugin2 = KnownFolders.EntryFolder.CombineUnchecked("Resources").CombineUnchecked("testfile2.esl");
+        _plugin1 = fileSystem.GetKnownPath(KnownPath.EntryDirectory).CombineUnchecked("Resources").CombineUnchecked("testfile1.esp");
+        _plugin2 = fileSystem.GetKnownPath(KnownPath.EntryDirectory).CombineUnchecked("Resources").CombineUnchecked("testfile2.esl");
     }
 
     [Fact]
-    public async Task LoadsMetadataForPlugins()
-    {
-        var result = await _cache.AnalyzeFileAsync(_plugin1);
+    public async Task LoadsMetadataForPlugins_Esm() => VerifyDependsOnSkyrimEsm(await _cache.AnalyzeFileAsync(_plugin1));
 
+    [Fact]
+    public async Task LoadsMetadataForPlugins_Esl() => VerifyDependsOnSkyrimEsm(await _cache.AnalyzeFileAsync(_plugin2));
+
+    private static void VerifyDependsOnSkyrimEsm(AnalyzedFile result)
+    {
         result.AnalysisData.Should().ContainEquivalentOf(new PluginAnalysisData
         {
             IsLightMaster = true,
