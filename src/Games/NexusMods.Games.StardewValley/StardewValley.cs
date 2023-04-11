e6f66e85841bfd94658dc66820933680e6682ca9
erri120 <erri120@protonmail.com>
2023-04-11 17:26:06 +0200

    Cleanup

--- src/Games/NexusMods.Games.StardewValley/StardewValley.cs
+++ src/Games/NexusMods.Games.StardewValley/StardewValley.cs
@@ -1,4 +1,5 @@
 using System.Runtime.InteropServices;
+using JetBrains.Annotations;
 using NexusMods.Common;
 using NexusMods.DataModel.Games;
 using NexusMods.FileExtractor.StreamFactories;
@@ -6,17 +7,23 @@ using NexusMods.Paths;
 
 namespace NexusMods.Games.StardewValley;
 
+[UsedImplicitly]
 public class StardewValley : AGame, ISteamGame, IGogGame
 {
+    private readonly IFileSystem _fileSystem;
+
     public IEnumerable<int> SteamIds => new[] { 413150 };
     public IEnumerable<long> GogIds => new long[] { 1453375253 };
 
     public override string Name => "Stardew Valley";
 
-    public static GameDomain GameDomain => DataModel.Games.GameDomain.From("stardewvalley");
+    public static GameDomain GameDomain => GameDomain.From("stardewvalley");
     public override GameDomain Domain => GameDomain;
 
-    public StardewValley(IEnumerable<IGameLocator> gameLocators) : base(gameLocators) { }
+    public StardewValley(IFileSystem fileSystem, IEnumerable<IGameLocator> gameLocators) : base(gameLocators)
+    {
+        _fileSystem = fileSystem;
+    }
 
     public override GamePath PrimaryFile
     {
@@ -36,6 +43,7 @@ public class StardewValley : AGame, ISteamGame, IGogGame
 
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
+                // TODO: SMAPI adds StardewModdingAPI.exe, which should be launched instead
                 return new GamePath(GameFolderType.Game, "Stardew Valley.exe");
             }
 
@@ -46,6 +54,22 @@ public class StardewValley : AGame, ISteamGame, IGogGame
     protected override IEnumerable<KeyValuePair<GameFolderType, AbsolutePath>> GetLocations(IGameLocator locator, GameLocatorResult installation)
     {
         yield return new KeyValuePair<GameFolderType, AbsolutePath>(GameFolderType.Game, installation.Path);
+
+        var stardewValleyAppDataPath = _fileSystem
+            .GetKnownPath(KnownPath.ApplicationDataDirectory)
+            .CombineUnchecked("StardewValley");
+
+        // base game saves
+        yield return new KeyValuePair<GameFolderType, AbsolutePath>(
+            GameFolderType.Saves,
+            stardewValleyAppDataPath.CombineUnchecked("Saves")
+        );
+
+        // TODO: SMAPI saves
+        //yield return new KeyValuePair<GameFolderType, AbsolutePath>(
+        //    GameFolderType.Saves,
+        //    stardewValleyAppDataPath.CombineUnchecked(".smapi")
+        //);
     }
 
     public override IStreamFactory Icon => new EmbededResourceStreamFactory<StardewValley>("NexusMods.Games.StardewValley.Resources.icon.png");
