f8c7363151dd89269cba7043813a834b3fbb97d5
Timothy Baldridge <tbaldridge@gmail.com>
2023-04-12 05:30:59 -0600

    Archive Content Cache Invalidation (#264)
    * Make the analyzer global so archive contents are recalculated

---
+++ src/NexusMods.DataModel/Abstractions/Ids/FileAnalyzerId.cs
@@ -0,0 +1,6 @@
+﻿namespace NexusMods.DataModel.Abstractions.Ids;
+
+public record FileAnalyzerId(Guid Analyzer, uint Revision)
+{
+    public static FileAnalyzerId New(string guid, uint revision) => new(Guid.Parse(guid), revision);
+}
