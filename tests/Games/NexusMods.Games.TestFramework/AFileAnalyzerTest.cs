8b2a7b008f8632e4a18a9d8380ca20e8b14da45b
Timothy Baldridge <tbaldridge@gmail.com>
2023-04-13 15:31:47 -0600

    Expand analyzer tests

--- tests/Games/NexusMods.Games.TestFramework/AFileAnalyzerTest.cs
+++ tests/Games/NexusMods.Games.TestFramework/AFileAnalyzerTest.cs
@@ -1,6 +1,12 @@
+using System.Collections.Immutable;
+using System.Text.Json;
 using JetBrains.Annotations;
+using Microsoft.Extensions.DependencyInjection;
 using NexusMods.DataModel.Abstractions;
+using NexusMods.DataModel.ArchiveContents;
 using NexusMods.DataModel.Games;
+using NexusMods.FileExtractor.FileSignatures;
+using NexusMods.Hashing.xxHash64;
 using NexusMods.Paths;
 
 namespace NexusMods.Games.TestFramework;
@@ -11,6 +17,7 @@ public class AFileAnalyzerTest<TGame, TFileAnalyzer> : AGameTest<TGame>
     where TFileAnalyzer : IFileAnalyzer
 {
     protected readonly TFileAnalyzer FileAnalyzer;
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
 
     /// <summary>
     /// Constructor.
@@ -18,6 +25,7 @@ public class AFileAnalyzerTest<TGame, TFileAnalyzer> : AGameTest<TGame>
     protected AFileAnalyzerTest(IServiceProvider serviceProvider) : base(serviceProvider)
     {
         FileAnalyzer = serviceProvider.FindImplementationInContainer<TFileAnalyzer, IFileAnalyzer>();
+        _jsonSerializerOptions = serviceProvider.GetRequiredService<JsonSerializerOptions>();
     }
 
     /// <summary>
@@ -37,6 +45,26 @@ public class AFileAnalyzerTest<TGame, TFileAnalyzer> : AGameTest<TGame>
         });
 
         var res = await asyncEnumerable.ToArrayAsync();
-        return res;
+
+        var fileEntry = FileSystem.GetFileEntry(path);
+        // Roundtrip the data through the serializer so we know it works
+        // Forgetting to register some analysis types could cause the
+        // Analyzer to look like it works, when it's really broken
+        var analyzedFile = new AnalyzedFile()
+        {
+            Size = fileEntry.Size,
+            Hash = Hash.From(0xDEADBEEF),
+            AnalyzersHash = Hash.From(0xCAFEFAB0),
+            AnalysisData = res.ToImmutableList(),
+            FileTypes = Array.Empty<FileType>()
+        };
+
+        var json =
+            JsonSerializer.Serialize(analyzedFile, _jsonSerializerOptions);
+        var deserialized =
+            (AnalyzedFile)JsonSerializer.Deserialize<Entity>(json, _jsonSerializerOptions)!;
+
+
+        return deserialized.AnalysisData.ToArray();
     }
 }
