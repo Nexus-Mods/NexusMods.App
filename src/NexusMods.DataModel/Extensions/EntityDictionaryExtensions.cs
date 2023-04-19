2fb828a2ac9b85975bb907556368c6634508a9ef
erri120 <erri120@protonmail.com>
2023-04-19 16:14:51 +0200

    Update ToEntityDictionary

--- src/NexusMods.DataModel/Extensions/EntityDictionaryExtensions.cs
+++ src/NexusMods.DataModel/Extensions/EntityDictionaryExtensions.cs
@@ -17,15 +17,20 @@ public static class EntityDictionaryExtensions
     /// </summary>
     /// <param name="modFiles"></param>
     /// <param name="dataStore"></param>
+    /// <param name="persist">Whether to use persist the collection of entities in the data store.</param>
     /// <returns></returns>
     public static EntityDictionary<ModFileId, AModFile> ToEntityDictionary(
         [InstantHandle(RequireAwait = false)] this IEnumerable<AModFile> modFiles,
-        IDataStore dataStore)
+        IDataStore dataStore,
+        bool persist = true)
     {
+        var enumerable = persist
+            ? modFiles.WithPersist(dataStore)
+            : modFiles;
+
         return new EntityDictionary<ModFileId, AModFile>(
             dataStore,
-            modFiles
-                .WithPersist(dataStore)
+            enumerable
                 .Select(modFile => new KeyValuePair<ModFileId, IId>(
                     modFile.Id,
                     modFile.DataStoreId)));
