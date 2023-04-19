2ef486f72ec147a08f03b373a4ac14010f26fb18
erri120 <erri120@protonmail.com>
2023-04-19 14:01:18 +0200

    Add AModMetadata and GroupMetadata

---
+++ src/NexusMods.DataModel/Loadouts/AModMetadata.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+
+namespace NexusMods.DataModel.Loadouts;
+
+/// <summary>
+/// Represents metadata for a <see cref="Mod"/>.
+/// </summary>
+[PublicAPI]
+public abstract record AModMetadata { }
