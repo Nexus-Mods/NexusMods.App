02b7d7d162608325191e94865e96e269d4217010
Timothy Baldridge <tbaldridge@gmail.com>
2023-04-19 15:37:30 -0600

    Rework tests to reuse code

--- tests/NexusMods.UI.Tests/LeftMenu/DownloadsViewTests.cs
+++ tests/NexusMods.UI.Tests/LeftMenu/DownloadsViewTests.cs
@@ -7,9 +7,11 @@ using NexusMods.UI.Tests.Framework;
 
 namespace NexusMods.UI.Tests.LeftMenu;
 
-public class DownloadsViewTests
+public class DownloadsViewTests : IAsyncLifetime
 {
     private readonly AvaloniaApp _app;
+    private ControlHost<DownloadsView,DownloadsDesignViewModel,IDownloadsViewModel> _host;
+    private (Options Option, Button Control)[] _options;
 
     public DownloadsViewTests(AvaloniaApp app)
     {
@@ -19,30 +21,15 @@ public class DownloadsViewTests
     [Fact]
     public async Task ActiveIsAppliedToButtonsWhenTheVMActivatesThem()
     {
-        await using var host =
-            await _app.GetControl<DownloadsView, DownloadsDesignViewModel,
-                IDownloadsViewModel>();
-
-        var inProgress = await host.GetViewControl<Button>("InProgressButton");
-        var completed = await host.GetViewControl<Button>("CompletedButton");
-        var history = await host.GetViewControl<Button>("HistoryButton");
-
-        var options = new (Options Option, Button Control)[]
-        {
-            (Options.InProgress, inProgress),
-            (Options.Completed, completed),
-            (Options.History, history)
-        };
-
         /// Select each option in turn
-        foreach (var current in options)
+        foreach (var current in _options)
         {
-            host.ViewModel.Set(current.Option);
-            host.ViewModel.Current.Should().Be(current.Option, "value was set in the View Model");
-            await host.Flush();
+            _host.ViewModel.Set(current.Option);
+            _host.ViewModel.Current.Should().Be(current.Option, "value was set in the View Model");
+            await _host.Flush();
 
             /// Check each of the controls for their correct active state
-            foreach (var checking in options)
+            foreach (var checking in _options)
             {
                 if (checking.Option == current.Option)
                 {
@@ -58,4 +45,38 @@ public class DownloadsViewTests
         }
     }
 
+    [Fact]
+    public async Task ClickingAButtonMakesItActive()
+    {
+        foreach (var option in _options)
+        {
+            option.Control.Command!.Execute(null);
+            await _host.Flush();
+
+        }
+
+    }
+
+    public async Task InitializeAsync()
+    {
+        _host =
+            await _app.GetControl<DownloadsView, DownloadsDesignViewModel,
+                IDownloadsViewModel>();
+
+        var inProgress = await _host.GetViewControl<Button>("InProgressButton");
+        var completed = await _host.GetViewControl<Button>("CompletedButton");
+        var history = await _host.GetViewControl<Button>("HistoryButton");
+
+        _options = new (Options Option, Button Control)[]
+        {
+            (Options.InProgress, inProgress),
+            (Options.Completed, completed),
+            (Options.History, history)
+        };
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _host.DisposeAsync();
+    }
 }
