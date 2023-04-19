6fe5801343793f34c45a7e86ee95e8682b5f2625
erri120 <erri120@protonmail.com>
2023-04-19 11:54:24 +0200

    Update UI tests

--- tests/NexusMods.UI.Tests/SpineTests.cs
+++ tests/NexusMods.UI.Tests/SpineTests.cs
@@ -1,7 +1,9 @@
 using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
 using NexusMods.App.UI.Controls.Spine;
 using NexusMods.DataModel.Games;
 using NexusMods.DataModel.Loadouts;
+using NexusMods.Games.RedEngine;
 using NexusMods.Paths.Utilities;
 using Type = NexusMods.App.UI.Controls.Spine.Type;
 
@@ -12,10 +14,12 @@ namespace NexusMods.UI.Tests;
 public class SpineTests : AUiTest
 {
     private readonly LoadoutManager _loadoutManager;
+    private readonly IGame _game;
 
     public SpineTests(LoadoutManager loadoutManager, IServiceProvider provider) : base(provider)
     {
         _loadoutManager = loadoutManager;
+        _game = provider.GetRequiredService<Cyberpunk2077>();
     }
 
     [Fact]
@@ -61,7 +65,7 @@ public class SpineTests : AUiTest
         }
 
         using var vm = GetActivatedViewModel<ISpineViewModel>();
-        await _loadoutManager.ImportFromAsync(KnownFolders.EntryFolder.CombineUnchecked(@"Resources\cyberpunk2077.1.61.zip"));
+        await _loadoutManager.ManageGameAsync(_game.Installations.First(), "Cyberpunk 2077");
 
         using var _ = vm.VM.Actions.Subscribe(vm.VM.Activations);
 
