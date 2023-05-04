2651c3477d8cb6dcb45dd6f0b2b0d88361b0f60b
Timothy Baldridge <tbaldridge@gmail.com>
2023-05-04 13:30:57 -0600

    End-to-End testing framework

---
+++ tests/NexusMods.UI.Tests/AEndToEndTest.cs
@@ -0,0 +1,29 @@
+﻿using NexusMods.UI.Tests.Framework;
+
+namespace NexusMods.UI.Tests;
+
+/// <summary>
+/// A base test class for a test that needs the entire app, the MainWindow and a full View Model
+/// </summary>
+public class AEndToEndTest : IAsyncLifetime
+{
+    private readonly AvaloniaApp _app;
+    
+    protected WindowHost? Host { get; private set; }
+
+    public AEndToEndTest(AvaloniaApp app)
+    {
+        _app = app;
+
+    }
+
+    public async Task InitializeAsync()
+    {
+        Host = await _app.GetMainWindow();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await Host!.DisposeAsync();
+    }
+}
