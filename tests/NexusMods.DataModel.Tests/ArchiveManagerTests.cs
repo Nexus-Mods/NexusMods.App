801a51a3a2b8750880d4ef8dad025d650ccce4e9
Timothy Baldridge <tbaldridge@gmail.com>
2023-05-22 21:25:03 -0600

    Comment the test a bit

--- tests/NexusMods.DataModel.Tests/ArchiveManagerTests.cs
+++ tests/NexusMods.DataModel.Tests/ArchiveManagerTests.cs
@@ -26,47 +26,56 @@ public class ArchiveManagerTests
     [InlineData(124, 1024)]
     public async Task CanArchiveFiles(int fileCount, int maxSize)
     {
+        // Randomly generate some file sizes
         var sizes = Enumerable.Range(0, fileCount).Select(s => Size.FromLong(Random.Shared.Next(maxSize))).ToArray();
+        
+        // Randomly generate some data
         var datas = sizes.Select(size =>
         {
             var buf = new byte[size.Value];
             Random.Shared.NextBytes(buf);
             return buf;
         }).ToArray();
+        
+        // Calculate the hashes
         var hashes = datas.Select(d => d.AsSpan().XxHash64()).ToArray();
 
+        // Create the tuples for compression
         var records = Enumerable.Range(0, fileCount).Select(idx => (
             (IStreamFactory)new MemoryStreamFactory($"{idx}.txt".ToRelativePath(), new MemoryStream(datas[idx])),
             hashes[idx], Size.FromLong(datas[idx].Length)));
 
+        // Backup the files
         await _manager.BackupFiles(records);
 
+        // Verify the files exist in the archive manager
         foreach (var hash in hashes)
             (await _manager.HaveFile(hash)).Should().BeTrue();
 
+        // Extract some of the files
         var extractionCount = Random.Shared.Next(fileCount);
         var extractionIdxs = Enumerable.Range(0, extractionCount).Select(_ => Random.Shared.Next(fileCount)).ToArray();
-        var extractedDatas = extractionIdxs.Select(idx => new byte[sizes[idx].Value]).ToArray();
         
+        // Extract the files via the in-memory method
         var extracted = await _manager.ExtractFiles(extractionIdxs.Select(idx => hashes[idx]));
         
+        // Verify the extracted files are correct
         foreach (var idx in extractionIdxs)
             extracted[hashes[idx]].Should().BeEquivalentTo(datas[idx]);
 
+        // Extract the files via the file method
         await using var tempFolder = _temporaryFileManager.CreateFolder();
         
         var fullPaths = extractionIdxs.Distinct().ToDictionary(idx => idx, idx => tempFolder.Path.CombineUnchecked($"{idx}.dat"));
         
         await _manager.ExtractFiles(extractionIdxs.Select(idx => (hashes[idx], fullPaths[idx])));
 
+        // Verify the extracted files are correct
         foreach (var idx in extractionIdxs)
         {
             var data = await fullPaths[idx].ReadAllBytesAsync();
 
             data.Should().BeEquivalentTo(datas[idx]);
         }
-
     }
-    
-
 }
