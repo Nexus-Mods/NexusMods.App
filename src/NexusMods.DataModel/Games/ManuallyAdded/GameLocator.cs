926ad021a52e0a92f6b371ee5aced506d6b3bb28
Timothy Baldridge <tbaldridge@gmail.com>
2023-06-12 15:08:30 -0600

    WIP

---
+++ src/NexusMods.DataModel/Games/ManuallyAdded/GameLocator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.ObjectModel;
+using NexusMods.DataModel.Abstractions;
+using NexusMods.DataModel.Abstractions.Ids;
+using NexusMods.Paths;
+
+namespace NexusMods.DataModel.Games.ManuallyAdded;
+
+public class GameStore : IGameLocator
+{
+    private readonly IDataStore _store;
+
+    public GameStore(IDataStore store, IEnumerable<IGame> games)
+    {
+        _store = store;
+        _games = games.ToArray();
+    }
+
+    public void Add(IGame game, Version version, AbsolutePath path)
+    {
+        _store.Put(new ManuallyAddedGame
+        {
+            GameDomain = game.Domain,
+            Version = version,
+            Path = path
+        });
+    }
+~
+    public IEnumerable<GameLocatorResult> Find(IGame game)
+    {
+        var allGames = _store.GetByPrefix<ManuallyAddedGame>(new Id64(EntityCategory.ManuallyAddedGame, 0));
+        var games = allGames.Where(g => g.GameDomain == game.Domain);
+        return games.Select(g => new GameLocatorResult(g.Path, GameStore.ManuallyAdded, g, g.Version));
+    }
+}
---
+++ src/NexusMods.DataModel/Games/ManuallyAdded/ManuallyAddedGame.cs
@@ -0,0 +1,17 @@
+﻿using NexusMods.DataModel.Abstractions;
+using NexusMods.DataModel.JsonConverters;
+using NexusMods.Paths;
+
+namespace NexusMods.DataModel.Games.ManuallyAdded;
+
+[JsonName("ManuallyAddedGame")]
+public record ManuallyAddedGame : Entity, IGameLocatorResultMetadata
+{
+    public override EntityCategory Category => EntityCategory.ManuallyAddedGame;
+
+    public required GameDomain GameDomain { get; init; }
+
+    public required Version Version { get; init; }
+
+    public required AbsolutePath Path { get; init; }
+}
