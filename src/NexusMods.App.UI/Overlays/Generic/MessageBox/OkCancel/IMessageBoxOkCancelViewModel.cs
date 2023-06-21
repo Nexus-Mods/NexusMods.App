39f8c3ee5db669b5021675510e844b88bd70dd1e
Sewer 56 <seweratwork@gmail.com>
2023-06-21 06:13:44 +0100

    Added: Ok/Cancel MessageBox

---
+++ src/NexusMods.App.UI/Overlays/Generic/MessageBox/OkCancel/IMessageBoxOkCancelViewModel.cs
@@ -0,0 +1,17 @@
+namespace NexusMods.App.UI.Overlays.Generic.MessageBox.OkCancel;
+
+/// <summary>
+/// Interface for a message box model with an 'ok' and 'cancel' button.
+/// </summary>
+public interface IMessageBoxOkCancelViewModel
+{
+    /// <summary>
+    /// When the user presses 'ok'
+    /// </summary>
+    Action Ok { get; set; }
+    
+    /// <summary>
+    /// When the user presses 'cancel'
+    /// </summary>
+    Action Cancel { get; set; }
+}
---
+++ src/NexusMods.App.UI/Overlays/Generic/MessageBox/OkCancel/MessageBoxOkCancelView.axaml.cs
@@ -0,0 +1,50 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.ReactiveUI;
+using NexusMods.App.UI.Overlays.Generic.MessageBox.Base;
+using ReactiveUI;
+
+namespace NexusMods.App.UI.Overlays.Generic.MessageBox.OkCancel;
+
+public partial class MessageBoxOkCancelView : ReactiveUserControl<IMessageBoxOkCancelViewModel>
+{
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<MessageBoxOkCancelView, string>(nameof(Title), "Cancel and delete download?");
+    
+    // ReSharper disable once MemberCanBePrivate.Global
+    public static readonly StyledProperty<string> DescriptionProperty =
+        AvaloniaProperty.Register<MessageBoxOkCancelView, string>(nameof(Description), "This is some very long text that spans multiple lines!! This text is super cool!!");
+    
+    public string Title
+    {
+        get => GetValue(TitleProperty);
+        set => SetValue(TitleProperty, value);
+    }
+    
+    public string Description
+    {
+        get => GetValue(DescriptionProperty);
+        set => SetValue(DescriptionProperty, value);
+    }
+    
+    public MessageBoxOkCancelView()
+    {
+        InitializeComponent();
+        this.WhenActivated(d =>
+        {
+            OkButton.Command = ReactiveCommand.CreateFromTask(() =>
+            {
+                ViewModel?.Ok.Invoke();
+                return Task.CompletedTask;
+            });
+            
+            CancelButton.Command = ReactiveCommand.CreateFromTask(() =>
+            {
+                ViewModel?.Cancel.Invoke();
+                return Task.CompletedTask;
+            });
+        });
+    }
+}
+
