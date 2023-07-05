ba376ca546253979dd635b0db9489929bd80768a
AL <26797547+Al12rs@users.noreply.github.com>
2023-07-05 12:42:32 +0200

    Update tests to cover Skyrim Legendary edition mod installations

---
+++ tests/Games/NexusMods.Games.BethesdaGameStudios.Tests/SkyrimLegendaryEditionTests/Installers/SkyrimSpecialEditionInstallerTests.cs
@@ -0,0 +1,14 @@
+﻿using NexusMods.Games.BethesdaGameStudios.Tests.Installers;
+using NexusMods.Games.TestFramework;
+using NexusMods.Paths;
+
+namespace NexusMods.Games.BethesdaGameStudios.Tests.SkyrimLegendaryEditionTests.Installers;
+
+public class SkyrimLegendaryEditionInstallerTests : SkyrimInstallerTests<SkyrimLegendaryEdition>
+{
+    public SkyrimLegendaryEditionInstallerTests(
+        IServiceProvider serviceProvider, 
+        TestModDownloader downloader, 
+        IFileSystem realFs) : 
+        base(serviceProvider, downloader, realFs) { }
+}
