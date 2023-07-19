bc578da6bb103e0abfe970576dec781fc0270f07
AL <26797547+Al12rs@users.noreply.github.com>
2023-07-19 18:01:37 +0200

    Use high performance logger for 7z.exe output

---
+++ src/ArchiveManagement/NexusMods.FileExtractor/HighPerformanceLogging.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace NexusMods.FileExtractor;
+
+internal static partial class HighPerformanceLogging
+{
+    [LoggerMessage(
+        EventId = 0,
+        Level = LogLevel.Debug,
+        Message = "[7z.exe] {line}")]
+    public static partial void ExecutingSevenZip(
+        this ILogger logger,
+        string line);
+}
