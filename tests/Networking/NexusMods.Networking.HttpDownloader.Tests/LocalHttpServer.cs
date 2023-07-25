61b29c7c4e2123f200e73aed1b25d8ffb52159c8
Timothy Baldridge <tbaldridge@gmail.com>
2023-07-25 16:50:28 -0600

    Allow NexusMods.Networking.HttpDownloader to be visible to its Test project.
    Preparing the HttpDownloader project for unit testing by adding InternalsVisibleTo attribute in the project file. Implementing LocalHttpServer for serving resource files during tests to simulate real-life scenarios.."

--- tests/Networking/NexusMods.Networking.HttpDownloader.Tests/LocalHttpServer.cs
+++ tests/Networking/NexusMods.Networking.HttpDownloader.Tests/LocalHttpServer.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Headers;
 using System.Text;
 using Microsoft.Extensions.Logging;
 using NexusMods.Hashing.xxHash64;
+using NexusMods.Paths;
 
 namespace NexusMods.Networking.HttpDownloader.Tests;
 
@@ -51,10 +52,17 @@ public class LocalHttpServer : IDisposable
                 _logger.LogInformation("Got connection");
                 using var resp = context.Response;
 
-                switch (context.Request.Url?.PathAndQuery)
+                if (context.Request.Url?.PathAndQuery.StartsWith("/Resources") ?? false)
                 {
-                    case "/hello":
+                    await ServeResource(resp, context.Request);
+                }
+                else
+                {
+
+                    switch (context.Request.Url?.PathAndQuery)
                     {
+                        case "/hello":
+                        {
                             var responseString = Encoding.UTF8.GetBytes("Hello World!");
                             resp.StatusCode = 200;
                             resp.StatusDescription = "OK";
@@ -65,23 +73,81 @@ public class LocalHttpServer : IDisposable
                                 await using var ros = resp.OutputStream;
                                 await ros.WriteAsync(responseString);
                             }
+
                             break;
-                    }
-                    case "/reliable":
-                        await HandleUnreliable(resp, context.Request, false);
-                        break;
-                    case "/unreliable":
-                        await HandleUnreliable(resp, context.Request, true);
-                        break;
-                    default:
+                        }
+                        case "/reliable":
+                            await HandleUnreliable(resp, context.Request, false);
+                            break;
+                        case "/unreliable":
+                            await HandleUnreliable(resp, context.Request, true);
+                            break;
+                        default:
                         {
                             resp.StatusCode = 404;
                             resp.StatusDescription = "Not Found";
                             break;
                         }
+                    }
                 }
             }
         });
+    }
+
+    private async Task ServeResource(HttpListenerResponse resp, HttpListenerRequest request)
+    {
+        var filePath = Uri.UnescapeDataString(request.Url!.AbsolutePath);
+        var fullPath = Path.GetFullPath("."+filePath);
+
+        await using var stream = FileSystem.Shared.FromUnsanitizedFullPath(fullPath).Read();
+
+        if (request.HttpMethod == "HEAD")
+        {
+            resp.StatusCode = (int)HttpStatusCode.OK;
+            resp.StatusDescription = "OK";
+            resp.ProtocolVersion = HttpVersion.Version11;
+            resp.ContentLength64 = stream.Length;
+            resp.Headers.Add(HttpResponseHeader.ContentType, "application/octet-stream");
+            resp.Headers.Add(HttpResponseHeader.AcceptRanges, "bytes");
+            resp.Headers.Add(HttpResponseHeader.KeepAlive, "true");
+            await using var _ = resp.OutputStream;
+            return;
+        }
+
+        var rangeString = request.Headers.Get("Range");
+
+
+        if (rangeString == null)
+        {
+            resp.StatusCode = (int)HttpStatusCode.OK;
+            resp.StatusDescription = "OK";
+            resp.ProtocolVersion = HttpVersion.Version11;
+            resp.Headers.Add(HttpResponseHeader.ContentType, "application/octet-stream");
+            resp.Headers.Add(HttpResponseHeader.AcceptRanges, "bytes");
+            resp.Headers.Add(HttpResponseHeader.KeepAlive, "true");
+            resp.ContentLength64 = stream.Length;
+            await using var ros = resp.OutputStream;
+            await stream.CopyToAsync(ros);
+
+        }
+        else
+        {
+            var rangeValue = RangeHeaderValue.Parse(rangeString);
+            var range = rangeValue.Ranges.First();
+            resp.StatusCode = (int)HttpStatusCode.PartialContent;
+            resp.StatusDescription = "Partial Content";
+            resp.ProtocolVersion = HttpVersion.Version11;
+            resp.Headers.Add(HttpResponseHeader.ContentType, "application/octet-stream");
+            resp.Headers.Add(HttpResponseHeader.AcceptRanges, "bytes");
+            resp.Headers.Add(HttpResponseHeader.KeepAlive, "true");
+            resp.Headers.Add(HttpResponseHeader.ContentRange, range.ToString());
+
+            stream.Position = (long)range.From!;
+            var buffer = new byte[(int)(range.To! - range.From!)];
+            await stream.ReadExactlyAsync(buffer);
+            await using var ros = resp.OutputStream;
+            await ros.WriteAsync(buffer);
+        }
     }
 
     private const int MB = 1024 * 1024;
