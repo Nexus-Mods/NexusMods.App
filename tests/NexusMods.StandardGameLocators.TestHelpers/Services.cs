b3605c2358e4b2547269814f606b9925f81d6a5f
AL <26797547+Al12rs@users.noreply.github.com>
2023-07-25 18:31:06 +0200

    Fix steam based gamelocator tests

--- tests/NexusMods.StandardGameLocators.TestHelpers/Services.cs
+++ tests/NexusMods.StandardGameLocators.TestHelpers/Services.cs
@@ -4,6 +4,8 @@ using GameFinder.StoreHandlers.EGS;
 using GameFinder.StoreHandlers.GOG;
 using GameFinder.StoreHandlers.Origin;
 using GameFinder.StoreHandlers.Steam;
+using GameFinder.StoreHandlers.Steam.Models;
+using GameFinder.StoreHandlers.Steam.Models.ValueTypes;
 using GameFinder.StoreHandlers.Xbox;
 using GameFinder.Wine;
 using GameFinder.Wine.Bottles;
@@ -44,9 +46,28 @@ public static class Services
                 tfm => new GOGGame(GOGGameId.From(42), "Stubbed Game", tfm.CreateFolder("gog_game").Path),
                 game => game.Id));
 
-        coll.AddSingleton<AHandler<SteamGame, SteamGameId>>(s =>
-            new StubbedGameLocator<SteamGame, SteamGameId>(s.GetRequiredService<TemporaryFileManager>(),
-                tfm => new SteamGame(SteamGameId.From( 42), "Stubbed Game", tfm.CreateFolder("steam_game").Path, CloudSavesDirectory: null),
+        coll.AddSingleton<AHandler<SteamGame, AppId>>(s =>
+            new StubbedGameLocator<SteamGame, AppId>(s.GetRequiredService<TemporaryFileManager>(),
+                tfm =>
+                {
+                    var gamePath = tfm.CreateFolder("steam_folder/SteamApps/common/steam_game").Path;
+                    return new SteamGame
+                    {
+                        SteamPath = gamePath.Parent.Parent.Parent,
+                        AppManifest = new AppManifest
+                        {
+                            AppId = AppId.From(42),
+                            Name = "Stubbed Game",
+                            InstallationDirectoryName = gamePath.FileName,
+                            StateFlags = StateFlags.FullyInstalled,
+                            ManifestPath = gamePath.Parent.Parent.Combine("steam_game.acf"),
+                        },
+                        LibraryFolder = new LibraryFolder
+                        {
+                            Path = gamePath.Parent.Parent,
+                        },
+                    };
+                },
                 game => game.AppId));
 
         coll.AddSingleton<AHandler<XboxGame, XboxGameId>>(s =>
