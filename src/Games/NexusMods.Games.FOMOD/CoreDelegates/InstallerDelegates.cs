3873d0ef415d2f6c4bbd2f0513ca53b367139ec5
erri120 <erri120@protonmail.com>
2023-08-08 14:29:29 +0200

    Add dummy implementations

--- src/Games/NexusMods.Games.FOMOD/CoreDelegates/InstallerDelegates.cs
+++ src/Games/NexusMods.Games.FOMOD/CoreDelegates/InstallerDelegates.cs
@@ -9,13 +9,15 @@ namespace NexusMods.Games.FOMOD.CoreDelegates;
 [PublicAPI]
 public sealed class InstallerDelegates : ICoreDelegates
 {
-    public IContextDelegates context => throw new NotImplementedException();
+    public IContextDelegates context { get; }
     public IIniDelegates ini => throw new NotImplementedException();
-    public IPluginDelegates plugin => throw new NotImplementedException();
+    public IPluginDelegates plugin { get; }
     public IUIDelegates ui { get; }
 
     public InstallerDelegates(ILoggerFactory loggerFactory, IOptionSelector optionSelector)
     {
+        context = new ContextDelegates(loggerFactory.CreateLogger<ContextDelegates>());
+        plugin = new PluginDelegates(loggerFactory.CreateLogger<PluginDelegates>());
         ui = new UiDelegates(loggerFactory.CreateLogger<UiDelegates>(), optionSelector);
     }
 }
