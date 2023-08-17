78ca6fcd5569ddd3503b4e67d628a53b81343325
sewer56lol <sewer56lol@googlemail.com>
2023-08-17 11:21:10 +0100

    Added: Basic Localization Plumbing

---
+++ src/NexusMods.App.UI/Localization/Localized.cs
@@ -0,0 +1,39 @@
+using Avalonia.Data;
+using Avalonia.Markup.Xaml;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using Avalonia.Markup.Xaml.MarkupExtensions.CompiledBindings;
+
+namespace NexusMods.App.UI.Localization;
+
+/// <summary>
+/// A XAML Markup Extension that provides localized strings.
+/// It can be used in XAML like this:
+///     `<TextBlock Text="{Localized HelloWorld}"/>`
+///
+/// Where `HelloWorld` is the key of the string to be localized inside the `.resx` file.
+/// </summary>
+public class LocalizedExtension : MarkupExtension
+{
+    /// <summary>
+    /// The key in the .resx file to be localized.
+    /// </summary>
+    public string Key { get; set; }
+
+    public LocalizedExtension(string key) => Key = key;
+
+    /// <inheritdoc/>
+    public override object ProvideValue(IServiceProvider serviceProvider)
+    {
+        // Tip: This binds the [] method of the Localizer class.
+
+        // Note: CompiledBindingExtension would be nicer to use, for performance reasons; but the things
+        // needed to manually create one, are internal :(
+        var binding = new ReflectionBindingExtension($"[{Key}]")
+        {
+            Mode = BindingMode.OneWay,
+            Source = Localizer.Instance,
+        };
+
+        return binding.ProvideValue(serviceProvider);
+    }
+}
---
+++ src/NexusMods.App.UI/Localization/Localizer.cs
@@ -0,0 +1,54 @@
+using System.ComponentModel;
+using System.Globalization;
+using System.Text;
+using Avalonia;
+using Avalonia.Markup.Xaml.MarkupExtensions;
+using NexusMods.App.UI.Resources;
+
+namespace NexusMods.App.UI.Localization;
+
+/// <summary>
+/// Utility class that provides access
+/// </summary>
+public class Localizer : INotifyPropertyChanged // <= INotifyPropertyChanged is required, so we can't be static
+{
+    /// <summary>
+    /// Singleton instance of the
+    /// </summary>
+    public static Localizer Instance { get; set; } = new();
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    // These are 'conventional' names used by the UI framework to indicate that an indexer
+    // property has been changed. When we create a binding with $"[{Key}]" syntax, the UI
+    // framework binds against the this[string key] property.
+
+    // Historically in XAML frameworks, this can be internally represented as 'Item' or 'Item[]';
+    // when I had a look through the Avalonia source for creating bindings; it seems to be 'Item' for
+    // array members; which I believe this would fall under. But just in case, I included both.
+
+    private const string IndexerName = "Item";
+    private const string IndexerArrayName = "Item[]";
+
+    /// <summary>
+    /// Loads a language with the specified locale, resetting the
+    /// </summary>
+    /// <param name="locale">The new locale to apply.</param>
+    public void LoadLanguage(string locale)
+    {
+        var item = new CultureInfo(locale);
+        Language.Culture = item;
+        Invalidate();
+    }
+
+    /// <summary>
+    /// Retrieves a string associated with current language, by key.
+    /// </summary>
+    public string this[string key] => Language.ResourceManager.GetString(key)!;
+
+    private void Invalidate()
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(IndexerName));
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(IndexerArrayName));
+    }
+}
