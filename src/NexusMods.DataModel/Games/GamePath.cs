ca733ea192181b0d34e3486f4b1e725bb879a5de
AL <26797547+Al12rs@users.noreply.github.com>
2023-08-23 14:58:29 +0200

    Added IPath<TPath> interface to GamePath - Implemented interface methods - This gives us FileTreeNode<GamePath>

--- src/NexusMods.DataModel/Games/GamePath.cs
+++ src/NexusMods.DataModel/Games/GamePath.cs
@@ -6,7 +6,7 @@ namespace NexusMods.Paths;
 /// <summary>
 /// Stores the path for an individual game.
 /// </summary>
-public readonly struct GamePath : IPath, IEquatable<GamePath>
+public readonly struct GamePath : IPath, IPath<GamePath>, IEquatable<GamePath>
 {
     /// <summary>
     /// The path to this instance.
@@ -63,6 +63,53 @@ public readonly struct GamePath : IPath, IEquatable<GamePath>
     ///    The absolute path to combine with current relative path.
     /// </param>
     public AbsolutePath Combine(AbsolutePath folderPath) => folderPath.Combine(Path);
+
+    /// <inheritdoc />
+    public RelativePath Name => Path.Name;
+
+    /// <inheritdoc />
+    public GamePath Parent => new GamePath(Type, Path.Parent);
+
+    /// <inheritdoc />
+    public GamePath GetRootComponent => new GamePath(Type, "");
+
+    /// <inheritdoc />
+    public IEnumerable<RelativePath> Parts => Path.Parts;
+
+    /// <inheritdoc />
+    public IEnumerable<GamePath> GetAllParents()
+    {
+        var type = Type;
+        var root = new GamePath(type, "");
+        return Path.GetAllParents().Select(parentPath => new GamePath(type, parentPath)).Append(root);
+    }
+
+    /// <inheritdoc />
+    public RelativePath GetNonRootPart()
+    {
+        return Path;
+    }
+
+    /// <inheritdoc />
+    public bool IsRooted => true;
+
+    /// <inheritdoc />
+    public bool InFolder(GamePath parent)
+    {
+        return Type == parent.Type && Path.InFolder(parent.Path);
+    }
+
+    /// <inheritdoc />
+    public bool StartsWith(GamePath other)
+    {
+        return Type == other.Type && Path.StartsWith(other.Path);
+    }
+
+    /// <inheritdoc />
+    public bool EndsWith(RelativePath other)
+    {
+        return Path.EndsWith(other);
+    }
 }
 
 /// <summary>
