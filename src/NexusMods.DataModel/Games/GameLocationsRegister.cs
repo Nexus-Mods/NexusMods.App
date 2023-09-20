0113258dcd4c513baacbccf3a083caba64a018f2
AL <26797547+Al12rs@users.noreply.github.com>
2023-09-20 18:56:49 +0200

    Add comments to GameLocationsRegister

--- src/NexusMods.DataModel/Games/GameLocationsRegister.cs
+++ src/NexusMods.DataModel/Games/GameLocationsRegister.cs
@@ -3,12 +3,29 @@ using NexusMods.Paths;
 
 namespace NexusMods.DataModel.Games;
 
+/// <summary>
+/// Installation specific register of game locations <see cref="GameFolderType"/> and their resolved paths.
+/// <remarks>
+/// This class provides information about the locations, such as resolved paths, nested locations and top level parents.
+/// </remarks>
+/// </summary>
 public class GameLocationsRegister
 {
     private readonly Dictionary<GameFolderType, GameLocationDescriptor> _locations = new();
 
+    /// <summary>
+    /// Obtain the resolved path for a <see cref="GameFolderType"/>.
+    /// </summary>
+    /// <param name="id">The <see cref="GameFolderType"/> to lookup</param>
     public AbsolutePath this[GameFolderType id] => _locations[id].ResolvedPath;
 
+    /// <summary>
+    /// Construct a new instance of <see cref="GameLocationsRegister"/>.
+    /// </summary>
+    /// <remarks>
+    /// Computes the nested and top level parents relationships.
+    /// </remarks>
+    /// <param name="newLocations">A Dictionary of <see cref="GameFolderType"/>, <see cref="AbsolutePath"/> to register</param>
     public GameLocationsRegister(IReadOnlyDictionary<GameFolderType, AbsolutePath> newLocations)
     {
         foreach (var (newId, newPath) in newLocations)
@@ -57,33 +74,74 @@ public class GameLocationsRegister
             : child.TopLevelParent.Value;
     }
 
+    /// <summary>
+    /// Dictionary of <see cref="GameFolderType"/> and <see cref="GameLocationDescriptor"/>s.
+    /// </summary>
     public IReadOnlyDictionary<GameFolderType, GameLocationDescriptor> LocationDescriptors => _locations;
 
+    /// <summary>
+    /// True if the <see cref="GameFolderType"/> is a top level location, as no other location contains it.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public bool IsTopLevel(GameFolderType id)
     {
         return _locations[id].IsTopLevel;
     }
 
+    /// <summary>
+    /// Obtain the top level parent of a <see cref="GameFolderType"/>.
+    /// </summary>
+    /// <remarks>
+    /// If the <see cref="GameFolderType"/> is a top level location, it will return itself.
+    /// </remarks>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public GameFolderType GetTopLevelParent(GameFolderType id)
     {
         return _locations[id].TopLevelParent ?? id;
     }
 
+    /// <summary>
+    /// Returns the associated <see cref="AbsolutePath"/> for the current game installation.
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
     public AbsolutePath GetResolvedPath(GameFolderType id)
     {
         return _locations[id].ResolvedPath;
     }
 
+    /// <summary>
+    /// Translates the <see cref="GamePath"/> to a resolved <see cref="AbsolutePath"/> for the current game installation.
+    /// </summary>
+    /// <param name="path"></param>
+    /// <returns></returns>
     public AbsolutePath GetResolvedPath(GamePath path)
     {
         return _locations[path.Type].ResolvedPath.Combine(path.Path);
     }
 
+    /// <summary>
+    /// Returns a collection of <see cref="GameFolderType"/>s that are nested directories of the passed location.
+    /// </summary>
+    /// <param name="Id"></param>
+    /// <returns></returns>
     public IReadOnlyCollection<GameFolderType> GetNestedLocations(GameFolderType Id)
     {
         return _locations[Id].NestedLocations.ToArray();
     }
 
+    /// <summary>
+    /// Maps the <see cref="AbsolutePath"/> to a <see cref="GamePath"/> for the current game installation.
+    /// </summary>
+    /// <remarks>
+    /// In case of multiple nested locations containing the path,
+    /// this method will return a <see cref="GamePath"/> for the "closest" <see cref="GameFolderType"/> to the file.
+    /// E.g. if "Data" location is nested to "Game", and the path is "Game/Data/foo.bar", the GamePath will be relative to "Data".
+    /// </remarks>
+    /// <param name="absolutePath"></param>
+    /// <returns></returns>
     public GamePath ToGamePath(AbsolutePath absolutePath)
     {
         return _locations.Values.Where(location => absolutePath.InFolder(location.ResolvedPath))
@@ -91,6 +149,11 @@ public class GameLocationsRegister
             .MinBy(gamePath => gamePath.Path.Depth);
     }
 
+    /// <summary>
+    /// Returns the collection of game locations that are not nested to any other,
+    /// in the form of a collection of <see cref="KeyValuePair"/> of <see cref="GameFolderType"/>, <see cref="AbsolutePath"/> />
+    /// </summary>
+    /// <returns></returns>
     public IReadOnlyCollection<KeyValuePair<GameFolderType, AbsolutePath>> GetTopLevelLocations()
     {
         return LocationDescriptors
