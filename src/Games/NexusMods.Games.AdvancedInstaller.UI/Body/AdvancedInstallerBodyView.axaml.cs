93bd2202b7f3701817823d77d745137d57b63904
AL <26797547+Al12rs@users.noreply.github.com>
2023-09-25 15:29:56 +0200

    Template body of the installer

---
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/Body/AdvancedInstallerBodyView.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NexusMods.Games.AdvancedInstaller.UI;
+
+public partial class AdvancedInstallerBodyView : UserControl
+{
+    public AdvancedInstallerBodyView()
+    {
+        InitializeComponent();
+    }
+}
+
