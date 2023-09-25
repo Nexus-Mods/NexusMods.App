00475c49c559c0282b148ce2c72dfd10a7e2fd10
AL <26797547+Al12rs@users.noreply.github.com>
2023-09-25 14:48:06 +0200

    Add AdvancedInstller.UI project

---
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/Services.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NexusMods.Games.AdvancedInstaller.UI;
+
+public static class Services
+{
+    public static IServiceCollection AddGuidedInstallerUi(this IServiceCollection serviceCollection)
+    {
+        return serviceCollection;
+    }
+}
