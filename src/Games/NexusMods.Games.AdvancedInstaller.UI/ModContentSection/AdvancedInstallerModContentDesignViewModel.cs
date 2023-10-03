eae59ec2971a04d3f12dbc992184ba21d95ccf00
sewer56 <sewer56lol@googlemail.com>
2023-10-03 19:04:02 +0100

    Moved: Advanced Installer Content from Real ViewModel to Design ViewModel

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/ModContentSection/AdvancedInstallerModContentDesignViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/ModContentSection/AdvancedInstallerModContentDesignViewModel.cs
@@ -10,10 +10,44 @@ public class AdvancedInstallerModContentDesignViewModel : AdvancedInstallerModCo
 {
     private readonly TreeDataGridFileNode _testTree = new()
     {
-        FileName = "All mod files", IsDirectory = false, IsRoot = true,
+        FileName = "All mod files", IsDirectory = true, IsRoot = true,
+        Children = new ObservableCollection<TreeDataGridFileNode>()
+        {
+            new() { FileName = "BWS.bsa" },
+            new() { FileName = "BWS - Textures.bsa" },
+            new() { FileName = "Readme-BWS.txt" },
+            new()
+            {
+                FileName = "Textures", IsDirectory = true,
+                Children = new ObservableCollection<TreeDataGridFileNode>()
+                {
+                    new() { FileName = "greenBlade.dds" },
+                    new() { FileName = "greenBlade_n.dds" },
+                    new() { FileName = "greenHilt.dds" },
+                    new()
+                    {
+                        FileName = "Armors", IsDirectory = true,
+                        Children = new ObservableCollection<TreeDataGridFileNode>()
+                        {
+                            new() { FileName = "greenArmor.dds" },
+                            new() { FileName = "greenBlade.dds" },
+                            new() { FileName = "greenHilt.dds" },
+                        },
+                    },
+                },
+            },
+            new()
+            {
+                FileName = "Meshes", IsDirectory = true,
+                Children = new ObservableCollection<TreeDataGridFileNode>()
+                {
+                    new() { FileName = "greenBlade.nif" },
+                }
+            }
+        }
     };
 
-    public override HierarchicalTreeDataGridSource<TreeDataGridFileNode> Tree => new HierarchicalTreeDataGridSource<TreeDataGridFileNode>(_testTree)
+    public override HierarchicalTreeDataGridSource<TreeDataGridFileNode> Tree => new(_testTree)
     {
         Columns =
         {
