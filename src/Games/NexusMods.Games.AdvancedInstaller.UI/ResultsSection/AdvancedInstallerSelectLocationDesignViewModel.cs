5083d1a6f41412e60b5e3997381f3b4ec7ad7636
AL <26797547+Al12rs@users.noreply.github.com>
2023-10-04 16:14:07 +0200

    Add preview for Suggested Entries

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/ResultsSection/AdvancedInstallerSelectLocationDesignViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/ResultsSection/AdvancedInstallerSelectLocationDesignViewModel.cs
@@ -1,6 +1,17 @@
-﻿namespace NexusMods.Games.AdvancedInstaller.UI;
+﻿using System.Collections.ObjectModel;
+using NexusMods.App.UI.Controls.GameWidget;
+
+namespace NexusMods.Games.AdvancedInstaller.UI;
 
 public class AdvancedInstallerSelectLocationDesignViewModel : AdvancedInstallerSelectLocationViewModel
 {
+    public AdvancedInstallerSelectLocationDesignViewModel() : base()
+    {
+        var entries = Enumerable.Range(0, 4)
+            .Select(_ => new AdvancedInstallerSuggestedEntryDesignViewModel());
 
+        SuggestedEntries =
+            new ReadOnlyObservableCollection<IAdvancedInstallerSuggestedEntryViewModel>(
+                new ObservableCollection<IAdvancedInstallerSuggestedEntryViewModel>(entries));
+    }
 }
