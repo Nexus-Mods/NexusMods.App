19d49f3a6f4747b1302cee08ac80fc4551eb7082
AL <26797547+Al12rs@users.noreply.github.com>
2023-10-11 16:44:38 +0200

    Create temporary constructor without parameters to allow previews to work

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/TreeEntry/TreeEntryDesignViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/TreeEntry/TreeEntryDesignViewModel.cs
@@ -2,11 +2,18 @@
 using NexusMods.Games.AdvancedInstaller.UI.Content.Left;
 using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.PreviewView.PreviewEntry;
 using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.SelectLocation.SelectableDirectoryEntry;
+using NexusMods.Paths;
+using NexusMods.Paths.FileTree;
 using OneOf;
 
 namespace NexusMods.Games.AdvancedInstaller.UI;
 
 internal class TreeEntryDesignViewModel : TreeEntryViewModel
 {
-    public TreeEntryDesignViewModel(OneOf<IModContentNode, ISelectableDirectoryNode, IPreviewEntryNode> node) : base(node) { }
+    public TreeEntryDesignViewModel(OneOf<IModContentNode, ISelectableDirectoryNode, IPreviewEntryNode> node) :
+        base(node) { }
+
+    public TreeEntryDesignViewModel() : base(ModContentNode<int>.FromFileTree(
+        new FileTreeNode<RelativePath, int>(new RelativePath("BWS.bsa").GetRootComponent, new RelativePath("Textures"),
+            false, 1))) { }
 }
