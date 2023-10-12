4733f8b2bdd7c16cfd4b980232b1a688985cda42
erri120 <erri120@protonmail.com>
2023-10-12 13:12:29 +0200

    PanelTabHeaderView styling

--- src/NexusMods.App.UI/WorkspaceSystem/PanelTabHeader/PanelTabHeaderDesignViewModel.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/PanelTabHeader/PanelTabHeaderDesignViewModel.cs
@@ -1,7 +1,13 @@
+using System.Diagnostics;
 using System.Reactive;
+using Avalonia;
 using Avalonia.Media;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
+using SkiaSharp;
+using Svg.Skia;
 
 namespace NexusMods.App.UI.WorkspaceSystem;
 
@@ -11,10 +17,23 @@ public class PanelTabHeaderDesignViewModel : AViewModel<IPanelTabHeaderViewModel
 
     public string Title { get; set; } = "My Mods";
 
-    public IImage? Icon { get; set; } = Initializers.IImage;
+    public IImage? Icon { get; set; }
 
     [Reactive]
     public bool IsSelected { get; set; }
 
     public ReactiveCommand<Unit, Unit> CloseTabCommand => Initializers.EnabledReactiveCommand;
+
+    public PanelTabHeaderDesignViewModel()
+    {
+        try
+        {
+            using var stream = AssetLoader.Open(new Uri("avares://NexusMods.App.UI/Assets/DesignTime/cyberpunk_game.png"));
+            Icon = new Bitmap(stream);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
 }
