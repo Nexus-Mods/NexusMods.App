d67748ef567a0e35542abc43327c13fa21d7d091
erri120 <erri120@protonmail.com>
2023-10-16 13:54:47 +0100

    Update docs

--- src/NexusMods.App.UI/WorkspaceSystem/PanelTab/IPanelTabViewModel.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/PanelTab/IPanelTabViewModel.cs
@@ -2,11 +2,23 @@ namespace NexusMods.App.UI.WorkspaceSystem;
 
 public interface IPanelTabViewModel : IViewModelInterface
 {
+    /// <summary>
+    /// Gets the unique identifier of the tab.
+    /// </summary>
     public PanelTabId Id { get; }
 
+    /// <summary>
+    /// Gets or sets the index of the tab.
+    /// </summary>
     public PanelTabIndex Index { get; set; }
 
+    /// <summary>
+    /// Gets the header view model of the tab.
+    /// </summary>
     public IPanelTabHeaderViewModel Header { get; }
 
+    /// <summary>
+    /// Gets or sets the contents of tab.
+    /// </summary>
     public IViewModel? Contents { get; set; }
 }
