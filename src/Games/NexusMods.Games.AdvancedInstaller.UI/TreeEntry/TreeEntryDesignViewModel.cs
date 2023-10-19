021276bf66ac9a64ffb4199abeb706b8532405f5
Al12rs <26797547+Al12rs@users.noreply.github.com>
2023-10-19 14:09:58 +0100

    Some textbox fixes

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/TreeEntry/TreeEntryDesignViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/TreeEntry/TreeEntryDesignViewModel.cs
@@ -11,7 +11,9 @@ namespace NexusMods.Games.AdvancedInstaller.UI;
 internal class TreeEntryDesignViewModel : TreeEntryViewModel
 {
 
-    public TreeEntryDesignViewModel() : base(ModContentNode<int>.FromFileTree(
-        new FileTreeNode<RelativePath, int>(new RelativePath("BWS.bsa").GetRootComponent, new RelativePath("Textures"),
-            false, 1))) { }
+    // public TreeEntryDesignViewModel() : base(ModContentNode<int>.FromFileTree(
+    //     new FileTreeNode<RelativePath, int>(new RelativePath("BWS.bsa").GetRootComponent, new RelativePath("Textures"),
+    //         false, 1))) { }
+
+    public TreeEntryDesignViewModel() : base(new SelectableDirectoryNode{ Status = SelectableDirectoryNodeStatus.Editing}) { }
 }
