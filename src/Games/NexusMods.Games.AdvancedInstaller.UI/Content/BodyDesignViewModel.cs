b91307f7b443f3fe15dbcd8040b43e5bd7bfee97
Sewer56 <seweratwork@gmail.com>
2023-10-19 15:53:57 +0100

    Fixed: Borked DesignViewModels from Last Commit

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/BodyDesignViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/BodyDesignViewModel.cs
@@ -1,3 +1,22 @@
-﻿namespace NexusMods.Games.AdvancedInstaller.UI.Content;
+﻿using NexusMods.App.UI;
+using NexusMods.Games.AdvancedInstaller.UI.Content.Left;
+using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.EmptyPreview;
+using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.PreviewView;
+using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.SelectLocation;
 
-internal class BodyDesignViewModel : BodyViewModel { }
+namespace NexusMods.Games.AdvancedInstaller.UI.Content;
+
+internal class BodyDesignViewModel : AViewModel<IBodyViewModel>,
+    IBodyViewModel
+{
+    public IModContentViewModel ModContentViewModel { get; } = new ModContentDesignViewModel();
+    public IPreviewViewModel PreviewViewModel { get; } = new PreviewDesignViewModel();
+
+    public IEmptyPreviewViewModel EmptyPreviewViewModel { get; } =
+        new EmptyPreviewDesignViewModel();
+
+    public ISelectLocationViewModel SelectLocationViewModel { get; } = new SelectLocationDesignViewModel();
+
+    public IViewModel CurrentPreviewViewModel => PreviewViewModel;
+    public DeploymentData Data { get; } = new();
+}
