231149329ac4e1662dac87cdf51029d809137ae1
sewer56 <sewer56lol@googlemail.com>
2023-10-24 13:30:10 +0100

    Added: Wire up SelectLocationViewModel at Runtime

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/Right/Results/SelectLocation/SelectLocationTreeViewModel.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/Right/Results/SelectLocation/SelectLocationTreeViewModel.cs
@@ -1,12 +1,16 @@
-﻿using NexusMods.DataModel.Games;
+﻿using NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.SelectLocation.SelectableDirectoryEntry;
 using NexusMods.Paths;
 
 namespace NexusMods.Games.AdvancedInstaller.UI.Content.Right.Results.SelectLocation;
 
 public sealed class SelectLocationTreeViewModel : SelectLocationTreeDesignViewModel
 {
-    public SelectLocationTreeViewModel(GameLocationsRegister register, LocationId treeRoot)
+    private readonly TreeEntryViewModel _tree;
+
+    public SelectLocationTreeViewModel(AbsolutePath absPath, LocationId treeRoot, string? rootName)
     {
-        //TODO: construct the tree from the location id instead of using the design vm version.
+        _tree = TreeEntryViewModel.Create(absPath, new GamePath(treeRoot, ""), rootName ?? "");
     }
+
+    protected override ITreeEntryViewModel GetTreeData() => _tree;
 }
