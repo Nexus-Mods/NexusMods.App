2134f1c417809561c9e8892a6683c19c93154318
erri120 <erri120@protonmail.com>
2023-10-24 11:04:11 +0200

    Add Color

--- src/NexusMods.App.UI/Controls/Dummy/DummyViewModel.cs
+++ src/NexusMods.App.UI/Controls/Dummy/DummyViewModel.cs
@@ -1,3 +1,15 @@
+using Avalonia.Media;
+using ReactiveUI.Fody.Helpers;
+
 namespace NexusMods.App.UI.Controls;
 
-public class DummyViewModel : AViewModel<IDummyViewModel>, IDummyViewModel { }
+public class DummyViewModel : AViewModel<IDummyViewModel>, IDummyViewModel
+{
+    [Reactive] public Color Color { get; set; } = Color.FromRgb(GetRandomByte(), GetRandomByte(), GetRandomByte());
+
+    private static byte GetRandomByte()
+    {
+        var i = Random.Shared.Next(byte.MinValue, byte.MaxValue);
+        return (byte)i;
+    }
+}
