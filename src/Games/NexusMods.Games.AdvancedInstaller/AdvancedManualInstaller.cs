91c6558add169382263b51640d038a6527c55a0a
AL <26797547+Al12rs@users.noreply.github.com>
2023-11-07 13:54:40 +0100

    Reorganize code a bit

--- src/Games/NexusMods.Games.AdvancedInstaller/AdvancedManualInstaller.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller/AdvancedManualInstaller.cs
@@ -9,28 +9,14 @@ namespace NexusMods.Games.AdvancedInstaller;
 
 public class AdvancedManualInstaller : AModInstaller
 {
-    public static AdvancedManualInstaller Create(IServiceProvider provider) => new(provider);
-
     private readonly Lazy<IAdvancedInstallerHandler?> _handler;
+    public bool IsActive => _handler.Value != null;
+
+    public static AdvancedManualInstaller Create(IServiceProvider provider) => new(provider);
 
     public AdvancedManualInstaller(IServiceProvider serviceProvider) : base(serviceProvider)
     {
         _handler = new Lazy<IAdvancedInstallerHandler?>(() => GetAdvancedInstallerHandler(serviceProvider));
-    }
-
-    public bool IsActive => _handler.Value != null;
-
-    private static IAdvancedInstallerHandler? GetAdvancedInstallerHandler(IServiceProvider provider)
-    {
-        try
-        {
-            return provider.GetRequiredService<IAdvancedInstallerHandler>();
-        }
-        catch (InvalidOperationException)
-        {
-            // No handler registered -> no UI.
-            return null;
-        }
     }
 
     public override ValueTask<IEnumerable<ModInstallerResult>> GetModsAsync(
@@ -48,4 +34,18 @@ public class AdvancedManualInstaller : AModInstaller
 
         return _handler.Value!.GetModsAsync(gameInstallation, loadoutId, baseModId, archiveFiles, cancellationToken);
     }
+
+    private static IAdvancedInstallerHandler? GetAdvancedInstallerHandler(IServiceProvider provider)
+    {
+        try
+        {
+            return provider.GetRequiredService<IAdvancedInstallerHandler>();
+        }
+        catch (InvalidOperationException)
+        {
+            // No handler registered -> no UI.
+            return null;
+        }
+    }
+
 }
