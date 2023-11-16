e317133a535025b1a33dce679099176f534f8849
erri120 <erri120@protonmail.com>
2023-11-16 13:25:22 +0100

    Update NexusMods.Networking.NexusWebApi

--- src/Networking/NexusMods.Networking.NexusWebApi/Types/GameId.cs
+++ src/Networking/NexusMods.Networking.NexusWebApi/Types/GameId.cs
@@ -1,4 +1,4 @@
-using Vogen;
+using TransparentValueObjects;
 
 namespace NexusMods.Networking.NexusWebApi.Types;
 
@@ -6,4 +6,8 @@ namespace NexusMods.Networking.NexusWebApi.Types;
 /// Unique identifier for an individual game hosted on Nexus.
 /// </summary>
 [ValueObject<int>]
-public readonly partial struct GameId { }
+public readonly partial struct GameId : IAugmentWith<DefaultValueAugment>
+{
+    /// <inheritdoc/>
+    public static GameId DefaultValue => From(default);
+}
