ac86df00763bcdbc25642709b8969fcfbc6a46fd
Vitalii Mikhailov <personal@aragas.org>
2023-11-17 12:40:11 +0200

    Added MountAndBlade2BannerlordLoadoutSynchronizerTests
    Exposed ModSortRules
    Added SortRules propagation

--- src/Games/NexusMods.Games.MountAndBlade2Bannerlord/MountAndBlade2BannerlordLoadoutSynchronizer.cs
+++ src/Games/NexusMods.Games.MountAndBlade2Bannerlord/MountAndBlade2BannerlordLoadoutSynchronizer.cs
@@ -1,6 +1,7 @@
 using NexusMods.DataModel.Loadouts;
 using NexusMods.DataModel.Loadouts.Mods;
 using NexusMods.DataModel.LoadoutSynchronizer;
+using NexusMods.DataModel.Sorting.Rules;
 
 namespace NexusMods.Games.MountAndBlade2Bannerlord;
 
@@ -8,5 +9,6 @@ public sealed class MountAndBlade2BannerlordLoadoutSynchronizer : ALoadoutSynchr
 {
     public MountAndBlade2BannerlordLoadoutSynchronizer(IServiceProvider provider) : base(provider) { }
 
+    public new ValueTask<ISortRule<Mod, ModId>[]> ModSortRules(Loadout loadout, Mod mod) => base.ModSortRules(loadout, mod);
     public new Task<IEnumerable<Mod>> SortMods(Loadout loadout) => base.SortMods(loadout);
 }
