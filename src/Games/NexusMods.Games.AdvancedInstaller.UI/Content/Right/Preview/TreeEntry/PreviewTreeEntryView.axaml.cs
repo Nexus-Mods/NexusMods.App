496007ed4a71af3de3664696a49ade0a7b43041e
AL <26797547+Al12rs@users.noreply.github.com>
2023-11-22 13:05:27 +0100

    Follow Guidelines for codeBehind null check

--- src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/Right/Preview/TreeEntry/PreviewTreeEntryView.axaml.cs
+++ src/Games/NexusMods.Games.AdvancedInstaller.UI/Content/Right/Preview/TreeEntry/PreviewTreeEntryView.axaml.cs
@@ -13,20 +13,24 @@ public partial class PreviewTreeEntryView : ReactiveUserControl<IPreviewTreeEntr
         InitializeComponent();
         this.WhenActivated(disposable =>
         {
-            if (ViewModel == null)
-                return;
-
-            this.BindCommand(ViewModel, vm => vm.RemoveMappingCommand, v => v.XRoundedButton)
+            // Initialize the view if the view model is not null
+            this.WhenAnyValue(view => view.ViewModel)
+                .WhereNotNull()
+                .Subscribe(_ => { InitView(); })
                 .DisposeWith(disposable);
 
+            this.BindCommand(ViewModel, vm => vm.RemoveMappingCommand,
+                    v => v.XRoundedButton)
+                .DisposeWith(disposable);
+
+            this.OneWayBind(ViewModel, vm => vm.IsRemovable,
+                    view => view.XRoundedButton.IsVisible)
+                .DisposeWith(disposable);
+
+            // FolderMerged pill, only show if it is a folder and it is merged.
             this.WhenAnyValue(view => view.ViewModel!.IsFolderMerged)
-                .Subscribe(isMerged => FolderMergedPill.IsVisible = isMerged && ViewModel.IsDirectory)
+                .Subscribe(isMerged => FolderMergedPill.IsVisible = isMerged && ViewModel!.IsDirectory)
                 .DisposeWith(disposable);
-
-            this.OneWayBind(ViewModel, vm => vm.IsRemovable, view => view.XRoundedButton.IsVisible)
-                .DisposeWith(disposable);
-
-            InitView();
         });
     }
 
