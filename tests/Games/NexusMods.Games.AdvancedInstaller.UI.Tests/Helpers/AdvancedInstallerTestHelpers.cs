67176d3f9e36bd1cb2e497b6eb7bb5696f7d7247
AL <26797547+Al12rs@users.noreply.github.com>
2023-11-27 13:35:24 +0100

    Rework the tests for the new advanced Installer

---
+++ tests/Games/NexusMods.Games.AdvancedInstaller.UI.Tests/Helpers/AdvancedInstallerTestHelpers.cs
@@ -0,0 +1,60 @@
+using FluentAssertions;
+using NexusMods.DataModel.ModInstallers;
+using NexusMods.Games.AdvancedInstaller.UI.ModContent;
+using NexusMods.Hashing.xxHash64;
+using NexusMods.Paths;
+using NexusMods.Paths.FileTree;
+
+namespace NexusMods.Games.AdvancedInstaller.UI.Tests.Helpers;
+
+internal static class AdvancedInstallerTestHelpers
+{
+    internal static InMemoryFileSystem CreateInMemoryFs(AbsolutePath basePath) => new();
+
+    internal static FileTreeNode<RelativePath, ModSourceFileEntry> CreateTestTreeMSFE()
+    {
+        var fileEntries = new Dictionary<RelativePath, ModSourceFileEntry>
+        {
+            { new RelativePath("BWS.bsa"), null! },
+            { new RelativePath("BWS - Textures.bsa"), null! },
+            { new RelativePath("Readme-BWS.txt"), null! },
+            { new RelativePath("Textures/greenBlade.dds"), null! },
+            { new RelativePath("Textures/greenBlade_n.dds"), null! },
+            { new RelativePath("Textures/greenHilt.dds"), null! },
+            { new RelativePath("Textures/Armors/greenArmor.dds"), null! },
+            { new RelativePath("Textures/Armors/greenBlade.dds"), null! },
+            { new RelativePath("Textures/Armors/greenHilt.dds"), null! },
+            { new RelativePath("Meshes/greenBlade.nif"), null! }
+        };
+
+        return FileTreeNode<RelativePath, ModSourceFileEntry>.CreateTree(fileEntries);
+    }
+
+    internal static FileTreeNode<RelativePath, ModSourceFileEntry> CreateTestFileTree()
+    {
+        var mockModSourceFileEntry = new ModSourceFileEntry
+        {
+            StreamFactory = null!,
+            Hash = Hash.FromLong(0),
+            Size = Size.FromLong(0),
+        };
+
+        var fileEntries = new Dictionary<RelativePath, ModSourceFileEntry>
+        {
+            { new RelativePath("Blue Version/Data/file1.txt"), mockModSourceFileEntry },
+            { new RelativePath("Blue Version/Data/file2.txt"), mockModSourceFileEntry },
+            { new RelativePath("Blue Version/Data/pluginA.esp"), mockModSourceFileEntry },
+            { new RelativePath("Blue Version/Data/PluginB.esp"), mockModSourceFileEntry },
+            { new RelativePath("Blue Version/Data/Textures/textureA.dds"), mockModSourceFileEntry },
+            { new RelativePath("Blue Version/Data/Textures/textureB.dds"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/file1.txt"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/file3.txt"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/pluginA.esp"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/PluginC.esp"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/Textures/textureA.dds"), mockModSourceFileEntry },
+            { new RelativePath("Green Version/Data/Textures/textureC.dds"), mockModSourceFileEntry },
+        };
+
+        return FileTreeNode<RelativePath, ModSourceFileEntry>.CreateTree(fileEntries);
+    }
+}
