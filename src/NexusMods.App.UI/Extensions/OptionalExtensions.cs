a213e86045ad2faf6f73e25a587bcd4cefa4512b
erri120 <erri120@protonmail.com>
2023-11-29 11:35:14 +0100

    Update ValueOr

--- src/NexusMods.App.UI/Extensions/OptionalExtensions.cs
+++ src/NexusMods.App.UI/Extensions/OptionalExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using DynamicData.Kernel;
+using JetBrains.Annotations;
 
 namespace NexusMods.App.UI.Extensions;
 
+[PublicAPI]
 public static class OptionalExtensions
 {
     /// <summary>
@@ -10,8 +12,17 @@ public static class OptionalExtensions
     /// <param name="optional">The source.</param>
     /// <param name="alternativeValue">The alternative value.</param>
     /// <typeparam name="T">The Type of the item.</typeparam>
-    public static T ValueOr<T>(this Optional<T> optional, T alternativeValue) where T : notnull
+    public static T ValueOr<T>(this Optional<T> optional, T alternativeValue) where T : struct
     {
         return optional.HasValue ? optional.Value : alternativeValue;
+    }
+
+    /// <summary>
+    /// Returns the value if the optional has a value, otherwise calls <see cref="alternativeValueFunc"/> and returns
+    /// the return value.
+    /// </summary>
+    public static T ValueOr<T>(this Optional<T> optional, Func<T> alternativeValueFunc) where T : class
+    {
+        return optional.HasValue ? optional.Value : alternativeValueFunc();
     }
 }
