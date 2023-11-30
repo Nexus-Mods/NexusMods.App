fa377ce8151a7d79a275effc5a8ef4726d934479
AL <26797547+Al12rs@users.noreply.github.com>
2023-11-30 12:43:16 +0100

    Add a controls Gallery to preview themes with

---
+++ src/NexusMods.App.UI/Theme/ControlsGallery.axaml.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NexusMods.App.UI.Theme;
+
+public partial class ControlsGallery : UserControl
+{
+    public ControlsGallery()
+    {
+        InitializeComponent();
+    }
+}
+
