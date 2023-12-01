8799284e9a4b1959a13aad6af509f83900f8af0b
Timothy Baldridge <tbaldridge@gmail.com>
2023-12-01 06:50:41 -0700

    Fix #800 (#802)

--- src/NexusMods.App/Services.cs
+++ src/NexusMods.App/Services.cs
@@ -105,6 +105,7 @@ public static class Services
         }
 
         services
+            .AddFileSystem()
             .AddSingleton<IStartupHandler, StartupHandler>()
             .AddSingleProcess()
             .AddSingleton(s => new SingleProcessSettings
