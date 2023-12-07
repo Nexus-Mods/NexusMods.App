e168a53ec1cd494d93001acc705c47c98cd3bd93
erri120 <erri120@protonmail.com>
2023-12-07 17:22:43 +0100

    Add RowEnumerator

--- src/NexusMods.App.UI/WorkspaceSystem/WorkspaceGridState.RowEnumerator.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/WorkspaceGridState.RowEnumerator.cs
@@ -1,3 +1,5 @@
+using System.Collections.Immutable;
+
 namespace NexusMods.App.UI.WorkspaceSystem;
 
 public readonly partial struct WorkspaceGridState
@@ -16,6 +18,117 @@ public readonly partial struct WorkspaceGridState
         {
             Info = info;
             Columns = columns;
+        }
+    }
+
+    /// <summary>
+    /// Efficient row enumerator.
+    /// </summary>
+    public ref struct RowEnumerator
+    {
+        private ImmutableSortedSet<PanelGridState>.Enumerator _enumerator;
+
+        private readonly Span<RowInfo> _seenRows;
+        private int _numRows;
+        private int _currentRowIndex;
+
+        public RowEnumerator(WorkspaceGridState parent, Span<RowInfo> seenRows)
+        {
+            _enumerator = parent.GetEnumerator();
+            _seenRows = seenRows;
+
+            Setup();
+        }
+
+        public Row Current { get; private set; }
+
+        public bool MoveNext(Span<PanelGridState> columnBuffer)
+        {
+            if (_currentRowIndex == _numRows) return false;
+            var rowInfo = _seenRows[_currentRowIndex++];
+
+            var columnCount = 0;
+            while (_enumerator.MoveNext())
+            {
+                var current = _enumerator.Current;
+                var rect = current.Rect;
+
+                if (rect.Y.IsCloseTo(rowInfo.Y) && rect.Bottom.IsGreaterThanOrCloseTo(rowInfo.Bottom()))
+                {
+                    columnBuffer[columnCount++] = current;
+                } else if (rect.Bottom.IsCloseTo(rowInfo.Bottom()) && rect.Y.IsLessThanOrCloseTo(rowInfo.Y))
+                {
+                    columnBuffer[columnCount++] = current;
+                }
+            }
+
+            if (_currentRowIndex != _numRows) _enumerator.Reset();
+
+            // sort the columns by the X component
+            var slice = columnBuffer[..columnCount];
+            slice.Sort(XComparer.Instance);
+
+            Current = new Row(rowInfo, columnBuffer[..columnCount]);
+            return true;
+        }
+
+        private void Setup()
+        {
+            // Fill the Span with positive infinity values
+            // This eliminates the need for a running variable and allows us to use BinarySearch
+            _seenRows.Fill(new RowInfo(double.PositiveInfinity, double.PositiveInfinity));
+
+            while (_enumerator.MoveNext())
+            {
+                var (_, rect) = _enumerator.Current;
+                var info = new RowInfo(rect.Y, rect.Height);
+
+                if (_numRows == 0)
+                {
+                    _seenRows[_numRows++] = info;
+                }
+                else
+                {
+                    var index = _seenRows.BinarySearch(info, YComparer.Instance);
+                    if (index < 0)
+                    {
+                        _seenRows[~index] = info;
+                        _numRows += 1;
+                    }
+                    else
+                    {
+                        // the binary search only uses the Y component, we want the smallest rows only
+                        var other = _seenRows[index];
+                        if (other.Height > rect.Height)
+                        {
+                            _seenRows[index] = info;
+                        }
+                    }
+                }
+            }
+
+            _enumerator.Reset();
+        }
+
+        public void Dispose() => _enumerator.Dispose();
+
+        private class YComparer : IComparer<RowInfo>
+        {
+            public static readonly IComparer<RowInfo> Instance = new YComparer();
+            public int Compare(RowInfo a, RowInfo b)
+            {
+                return a.Y.CompareTo(b.Y);
+            }
+        }
+
+        private class XComparer : IComparer<PanelGridState>
+        {
+            public static readonly IComparer<PanelGridState> Instance = new XComparer();
+
+            public int Compare(PanelGridState a, PanelGridState b)
+            {
+                return a.Rect.X.CompareTo(b.Rect.X);
+            }
         }
     }
 }
---
+++ tests/NexusMods.UI.Tests/WorkspaceSystem/WorkspaceGridStateTests/RowEnumeratorTests.cs
@@ -0,0 +1,80 @@
+using System.Diagnostics.CodeAnalysis;
+using Avalonia;
+using FluentAssertions;
+using NexusMods.App.UI.WorkspaceSystem;
+
+namespace NexusMods.UI.Tests.WorkspaceSystem;
+
+public partial class WorkspaceGridStateTests
+{
+    [Theory]
+    [MemberData(nameof(TestData_RowEnumerator))]
+    public void Test_RowEnumerator(
+        WorkspaceGridState currentState,
+        (WorkspaceGridState.RowInfo Info, PanelGridState[] Columns)[] expectedOutputs)
+    {
+        GridUtils.IsPerfectGrid(currentState).Should().BeTrue();
+
+        Span<WorkspaceGridState.RowInfo> seenRows = stackalloc WorkspaceGridState.RowInfo[8];
+        using var rowEnumerator = new WorkspaceGridState.RowEnumerator(currentState, seenRows);
+
+        var actualOutputs = new List<(WorkspaceGridState.RowInfo Info, PanelGridState[] Columns)>();
+
+        Span<PanelGridState> columnBuffer = stackalloc PanelGridState[2];
+        while (rowEnumerator.MoveNext(columnBuffer))
+        {
+            var current = rowEnumerator.Current;
+            actualOutputs.Add((current.Info, current.Columns.ToArray()));
+        }
+
+        actualOutputs.Should().HaveCount(expectedOutputs.Length);
+        for (var i = 0; i < actualOutputs.Count; i++)
+        {
+            var actualOutput = actualOutputs[i];
+            var expectedOutput = expectedOutputs[i];
+
+            actualOutput.Info.Should().Be(expectedOutput.Info);
+            actualOutput.Columns.Should().Equal(expectedOutput.Columns);
+        }
+    }
+
+    [SuppressMessage("ReSharper", "HeapView.ObjectAllocation.Evident")]
+    [SuppressMessage("ReSharper", "HeapView.ObjectAllocation")]
+    [SuppressMessage("ReSharper", "HeapView.BoxingAllocation")]
+    public static IEnumerable<object[]> TestData_RowEnumerator()
+    {
+        var firstPanelId = PanelId.From(Guid.Parse("11111111-1111-1111-1111-111111111111"));
+        var secondPanelId = PanelId.From(Guid.Parse("22222222-2222-2222-2222-222222222222"));
+        var thirdPanelId = PanelId.From(Guid.Parse("33333333-3333-3333-3333-333333333333"));
+        var fourthPanelId = PanelId.From(Guid.Parse("44444444-4444-4444-4444-444444444444"));
+
+        yield return new object[]
+        {
+            CreateState(
+                isHorizontal: true,
+                new PanelGridState(firstPanelId, new Rect(0, 0, 0.5, 0.5)),
+                new PanelGridState(secondPanelId, new Rect(0, 0.5, 0.5, 0.5)),
+                new PanelGridState(thirdPanelId, new Rect(0.5, 0, 0.5, 1))
+            ),
+            new[]
+            {
+                (
+                    Info: new WorkspaceGridState.RowInfo(0, 0.5),
+                    Columns: new[]
+                    {
+                        new PanelGridState(firstPanelId, new Rect(0, 0, 0.5, 0.5)),
+                        new PanelGridState(thirdPanelId, new Rect(0.5, 0, 0.5, 1))
+                    }
+                ),
+                (
+                    Info: new WorkspaceGridState.RowInfo(0.5, 0.5),
+                    Columns: new[]
+                    {
+                        new PanelGridState(secondPanelId, new Rect(0, 0.5, 0.5, 0.5)),
+                        new PanelGridState(thirdPanelId, new Rect(0.5, 0, 0.5, 1))
+                    }
+                ),
+            }
+        };
+    }
+}
