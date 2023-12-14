1e4812a25ee23d552ca396a77cca0976394825a4
erri120 <erri120@protonmail.com>
2023-12-14 11:41:08 +0100

    Add Verify

--- tests/NexusMods.UI.Tests/WorkspaceSystem/IconUtilsTests.cs
+++ tests/NexusMods.UI.Tests/WorkspaceSystem/IconUtilsTests.cs
@@ -1,15 +1,13 @@
 using Avalonia;
-using FluentAssertions;
 using NexusMods.App.UI.WorkspaceSystem;
 
 namespace NexusMods.UI.Tests.WorkspaceSystem;
 
-public class IconUtilsTests : AUiTest
+[UsesVerify]
+public class IconUtilsTests(IServiceProvider provider) : AUiTest(provider)
 {
-    public IconUtilsTests(IServiceProvider provider) : base(provider) { }
-
     [Fact]
-    public void Test_StateToBitmap()
+    public Task Test_StateToBitmap()
     {
         var state = new Dictionary<PanelId, Rect>
         {
@@ -17,7 +15,13 @@ public class IconUtilsTests : AUiTest
             { PanelId.DefaultValue, new Rect(0.5, 0, 0.5, 1) }
         };
 
-        var bitmap = IconUtils.StateToBitmap(state);
-        bitmap.Size.Should().Be(new Size(150, 150));
+        using var stream = new MemoryStream();
+        using (var bitmap = IconUtils.StateToBitmap(state))
+        {
+            bitmap.Save(stream);
+            stream.Position = 0;
+        }
+
+        return Verify(stream, extension: "png").DisableDiff();
     }
 }
