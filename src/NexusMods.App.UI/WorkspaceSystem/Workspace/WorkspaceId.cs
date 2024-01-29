62f2295ea059c51ee12db1038eb04f72d7153726
erri120 <erri120@protonmail.com>
2024-01-29 14:51:56 +0100

    Fix JSON serialization for Ids

--- src/NexusMods.App.UI/WorkspaceSystem/Workspace/WorkspaceId.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/Workspace/WorkspaceId.cs
@@ -3,7 +3,7 @@ using TransparentValueObjects;
 namespace NexusMods.App.UI.WorkspaceSystem;
 
 [ValueObject<Guid>]
-public readonly partial struct WorkspaceId : IAugmentWith<DefaultValueAugment>
+public readonly partial struct WorkspaceId : IAugmentWith<DefaultValueAugment, JsonAugment>
 {
     /// <inheritdoc/>
     public static WorkspaceId DefaultValue { get; } = From(Guid.Empty);
