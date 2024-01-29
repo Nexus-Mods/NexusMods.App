4c3a1067ee90da5a19263b9f6878f1d12432a8ba
erri120 <erri120@protonmail.com>
2024-01-29 15:34:06 +0100

    Migrate to new code signing (#863)

--- scripts/sign.ps1
+++ scripts/sign.ps1
@@ -21,24 +21,31 @@ if (Test-Path $executableToSign -PathType Leaf) {
     exit 1;
 }
 
-# https://github.com/actions/runner-images/blob/main/images/win/Windows2022-Readme.md#installed-windows-sdks
-$rootDirectory = "C:\Program Files (x86)\Windows Kits\10\bin\";
-$sdkDirectory = Get-ChildItem -Path $rootDirectory -Name | Where-Object { $_ -like "10*" } | Sort-Object -Descending | Select-Object -First 1
-Write-Host "Sdk Directory: $sdkDirectory"
-
-$signToolPath = [System.IO.Path]::Combine($rootDirectory, $sdkDirectory, "x64", "signtool.exe")
-Write-Host "signtool path: $signToolPath"
-
-if (Test-Path $signToolPath -PathType Leaf) {
-    Write-Host "Found signtool.exe at: $signToolPath";
+$codeSignToolDir = $env:CodeSignToolDir
+if (Test-Path $codeSignToolDir -PathType Container) {
+    Write-Host "CodeSignTool directory $codeSignToolDir";
 } else {
-    Write-Error "Singing tool at $signToolPath doesn't exist!";
+    Write-Error "CodeSignTool directory $codeSignToolDir doesn't exist!";
     exit 1;
 }
 
-Write-Host "Signing $executableToSign";
+$codeSignToolPath = Join-Path $codeSignToolDir "CodeSignTool"
 
-& $signToolPath sign /f "$env:SigningCertificate" /p "$env:SigningCertificatePassword" /td sha256 /fd sha256 /tr "$env:TimestampServer" $executableToSign
+# CodeSignTool requires user interaction to confirm an overwrite of the original file.
+# We circumvent this by setting the output directory to some temp directory and replacing
+# the original file with the newly signed file.
+
+$tmpDir = Join-Path $(Resolve-Path .) "tmp"
+if (Test-Path $tmpDir -PathType Container) {
+    Remove-Item -Path $tmpDir -Recurse -Force
+}
+
+New-Item -Path $tmpDir -Type Directory
+
+$inputFile = $executableToSign
+$outputFile = Join-Path $tmpDir $(Get-Item $inputFile).Name
+
+& $codeSignToolPath sign -input_file_pathh="$inputFile" -output_dir_path="$tmpDir" -username="$env:ES_USERNAME" -password="$env:ES_PASSWORD" -credential_id="$env:ES_CREDENTIAL_ID" -totp_secret="$env:ES_TOTP_SECRET"
 $exitCode = $LASTEXITCODE
 
 if ($exitCode -eq 0) {
@@ -47,3 +54,8 @@ if ($exitCode -eq 0) {
     Write-Error "Signing failed with code $exitCode"
     exit $exitCode
 }
+
+Write-Host "Moving $outputFile to $inputFile"
+Move-File -Path $outputFile -Destination $inputFile -Force
+
+Remove-Item -Path $tmpDir -Recurse -Force
