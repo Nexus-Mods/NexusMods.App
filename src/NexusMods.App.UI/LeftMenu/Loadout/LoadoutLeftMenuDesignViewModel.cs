2100fce0e8c4ed86b2cd258094499706bf49a05e
AL <26797547+Al12rs@users.noreply.github.com>
2024-02-19 10:06:36 +0100

    Add design vm for Loadout Left Menu

---
+++ src/NexusMods.App.UI/LeftMenu/Loadout/LoadoutLeftMenuDesignViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using NexusMods.App.UI.LeftMenu.Items;
+using NexusMods.App.UI.WorkspaceSystem;
+
+namespace NexusMods.App.UI.LeftMenu.Loadout;
+
+public class LoadoutLeftMenuDesignViewModel : AViewModel<ILoadoutLeftMenuViewModel>, ILoadoutLeftMenuViewModel
+{
+    public ReadOnlyObservableCollection<ILeftMenuItemViewModel> Items { get; } = new([]);
+    public WorkspaceId WorkspaceId { get; } = new();
+    public ILaunchButtonViewModel LaunchButtonViewModel { get; } = new LaunchButtonDesignViewModel();
+}
