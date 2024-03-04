39d68079297e2ea7df234a9bf926a5deda437e13
AL <26797547+Al12rs@users.noreply.github.com>
2024-03-04 11:13:14 +0100

    Add missing comment

--- src/NexusMods.DataModel/ApplyService.cs
+++ src/NexusMods.DataModel/ApplyService.cs
@@ -88,6 +88,7 @@ public class ApplyService : IApplyService
         return loadout;
     }
 
+    /// <inheritdoc />
     public async Task<Loadout> Ingest(GameInstallation gameInstallation)
     {
         var lastAppliedRevision = GetLastAppliedLoadout(gameInstallation);
