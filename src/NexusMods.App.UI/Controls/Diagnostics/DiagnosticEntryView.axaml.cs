79eda0846aaf23a5fac6e85af53282684886d401
AL <26797547+Al12rs@users.noreply.github.com>
2024-03-18 10:09:27 +0100

    WIP: initial Diagnostic Entry view work

---
+++ src/NexusMods.App.UI/Controls/Diagnostics/DiagnosticEntryView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.ReactiveUI;
+
+namespace NexusMods.App.UI.Controls.Diagnostics;
+
+public partial class DiagnosticEntryView : ReactiveUserControl<IDiagnosticEntryViewModel>
+{
+    public DiagnosticEntryView()
+    {
+        InitializeComponent();
+    }
+}
+
---
+++ src/NexusMods.App.UI/Controls/Diagnostics/DiagnosticEntryViewModel.cs
@@ -0,0 +1,19 @@
+using System.Reactive;
+using NexusMods.Abstractions.Diagnostics;
+using ReactiveUI;
+
+namespace NexusMods.App.UI.Controls.Diagnostics;
+
+public class DiagnosticEntryViewModel : AViewModel<IDiagnosticEntryViewModel>, IDiagnosticEntryViewModel
+{
+    public DiagnosticEntryViewModel(Diagnostic diagnostic)
+    {
+        Summary = diagnostic.Summary.ToString();
+        Severity = diagnostic.Severity;
+        SeeDetailsCommand = ReactiveCommand.Create(() => { });
+    }
+
+    public string Summary { get; }
+    public DiagnosticSeverity Severity { get; }
+    public ReactiveCommand<Unit, Unit> SeeDetailsCommand { get; }
+}
