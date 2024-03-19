2a743ecccf1547ea5070678618f059e2494d0c46
erri120 <erri120@protonmail.com>
2024-03-19 14:25:32 +0100

    Fix

--- src/Games/NexusMods.Games.StardewValley/Diagnostics.cs
+++ src/Games/NexusMods.Games.StardewValley/Diagnostics.cs
@@ -17,7 +17,7 @@ internal static partial class Diagnostics
         .WithId(new DiagnosticId(Source, number: 1))
         .WithTitle("Missing required dependency")
         .WithSeverity(DiagnosticSeverity.Warning)
-        .WithSummary("Mod {Mod} is missing required dependency \"{MissingDependency}\"")
+        .WithSummary("Mod {Mod} is missing required dependency '{MissingDependency}'")
         .WithDetails("You can download the latest version at {NexusModsDependencyUri}.")
         .WithMessageData(messageBuilder => messageBuilder
             .AddDataReference<ModReference>("Mod")
