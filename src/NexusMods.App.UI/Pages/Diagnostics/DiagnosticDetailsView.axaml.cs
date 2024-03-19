c9c7bdca0c6b314745092ed471e9524d3b4ef9ac
AL <26797547+Al12rs@users.noreply.github.com>
2024-03-19 09:53:26 +0100

    Translationos

--- src/NexusMods.App.UI/Pages/Diagnostics/DiagnosticDetailsView.axaml.cs
+++ src/NexusMods.App.UI/Pages/Diagnostics/DiagnosticDetailsView.axaml.cs
@@ -2,6 +2,7 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using Avalonia.ReactiveUI;
 using NexusMods.Abstractions.Diagnostics;
+using NexusMods.App.UI.Resources;
 using ReactiveUI;
 
 namespace NexusMods.App.UI.Pages.Diagnostics;
@@ -29,24 +30,24 @@ public partial class DiagnosticDetailsView : ReactiveUserControl<IDiagnosticDeta
             case DiagnosticSeverity.Suggestion:
                 SeverityIcon.Classes.Add("HelpCircle");
                 SeverityIcon.Classes.Add("ForegroundInfoStrong");
-                SeverityTitleTextBlock.Text = "SUGGESTION";
+                SeverityTitleTextBlock.Text = Language.DiagnosticDetailsView_SeverityTitle_SUGGESTION;
                 SeverityTitleTextBlock.Classes.Add("ForegroundInfoStrong");
                 break;
             case DiagnosticSeverity.Warning:
                 SeverityIcon.Classes.Add("Alert");
                 SeverityIcon.Classes.Add("ForegroundWarningStrong");
-                SeverityTitleTextBlock.Text = "WARNING";
+                SeverityTitleTextBlock.Text = Language.DiagnosticDetailsView_SeverityTitle_WARNING;
                 SeverityTitleTextBlock.Classes.Add("ForegroundWarningStrong");
                 break;
             case DiagnosticSeverity.Critical:
                 SeverityIcon.Classes.Add("AlertOctagon");
                 SeverityIcon.Classes.Add("ForegroundDangerStrong");
-                SeverityTitleTextBlock.Text = "CRITICAL ERROR";
+                SeverityTitleTextBlock.Text = Language.DiagnosticDetailsView_SeverityTitle_CRITICAL_ERROR;
                 SeverityTitleTextBlock.Classes.Add("ForegroundDangerStrong");
                 break;
             default:
                 SeverityIcon.Classes.Add("Bell");
-                SeverityTitleTextBlock.Text = "HIDDEN";
+                SeverityTitleTextBlock.Text = Language.DiagnosticDetailsView_SeverityTitle_HIDDEN;
                 break;
         }
         
