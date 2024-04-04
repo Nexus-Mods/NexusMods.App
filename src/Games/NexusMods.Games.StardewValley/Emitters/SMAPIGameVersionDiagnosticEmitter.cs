132f7cc897d372fbe7dcb910d5c043e269aecba3
erri120 <erri120@protonmail.com>
2024-04-04 14:00:21 +0200

    Fix versions

--- src/Games/NexusMods.Games.StardewValley/Emitters/SMAPIGameVersionDiagnosticEmitter.cs
+++ src/Games/NexusMods.Games.StardewValley/Emitters/SMAPIGameVersionDiagnosticEmitter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
 using System.Net.Http.Json;
 using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
@@ -13,8 +14,8 @@ using NexusMods.Games.StardewValley.Models;
 
 namespace NexusMods.Games.StardewValley.Emitters;
 
-using SMAPIToGameMapping = ImmutableDictionary<string, SMAPIGameVersionDiagnosticEmitter.GameVersions>;
-using GameToSMAPIMapping = ImmutableDictionary<string, string[]>;
+using SMAPIToGameMapping = ImmutableDictionary<Version, SMAPIGameVersionDiagnosticEmitter.GameVersions>;
+using GameToSMAPIMapping = ImmutableDictionary<Version, Version>;
 
 [UsedImplicitly]
 public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
@@ -40,7 +41,9 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
         var gameToSMAPIMappings = await FetchGameToSMAPIMappings(cancellationToken);
         if (gameToSMAPIMappings is null) yield break;
 
-        var gameVersion = loadout.Installation.Version;
+        // var gameVersion = SimplifyVersion(new Version("1.5.6"));
+        var gameVersion = SimplifyVersion(loadout.Installation.Version);
+
         var smapiMod = loadout.Mods
             .Where(kv => kv.Value.Enabled)
             .Select(kv => kv.Value)
@@ -48,7 +51,7 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
 
         if (smapiMod is null) yield break;
         var smapiMarker = smapiMod.Metadata.OfType<SMAPIMarker>().First();
-        var smapiVersion = smapiMarker.Version!;
+        var smapiVersion = SimplifyVersion(smapiMarker.Version!);
 
         var diagnostic1 = GameVersionOlderThanMinimumGameVersion(
             smapiToGameMappings, gameToSMAPIMappings,
@@ -67,26 +70,19 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
         Version gameVersion,
         Version smapiVersion)
     {
-        if (!smapiToGameMappings.TryGetValue(smapiVersion.ToString(), out var supportedGameVersions))
+        if (!smapiToGameMappings.TryGetValue(smapiVersion, out var supportedGameVersions))
         {
             // ReSharper disable once InconsistentLogPropertyNaming
             _logger.LogWarning("Found no game version information for SMAPI version {SMAPIVersion}", smapiVersion);
             return null;
         }
 
-        var (sMinimumGameVersion, _) = supportedGameVersions;
-        if (sMinimumGameVersion is null) return null;
-
-        if (!Version.TryParse(sMinimumGameVersion, out var minimumGameVersion))
-        {
-            _logger.LogWarning("Unable to parse game version string `{VersionString}` as a Version", sMinimumGameVersion);
-            return null;
-        }
+        var (minimumGameVersion, _) = supportedGameVersions;
+        if (minimumGameVersion is null) return null;
 
         if (minimumGameVersion <= gameVersion) return null;
 
-        if (!gameToSMAPIMappings.TryGetValue(gameVersion.ToString(), out var supportedSMAPIVersions) ||
-            supportedSMAPIVersions.Length == 0)
+        if (!gameToSMAPIMappings.TryGetValue(gameVersion, out var supportedSMAPIVersion))
         {
             _logger.LogWarning("Found no supported SMAPI versions for game version {GameVersion}", gameVersion);
             return null;
@@ -95,9 +91,9 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
         return Diagnostics.CreateGameVersionOlderThanMinimumGameVersion(
             SMAPIMod: smapiMod.ToReference(loadout),
             SMAPIVersion: smapiVersion.ToString(),
-            MinimumGameVersion: sMinimumGameVersion,
+            MinimumGameVersion: minimumGameVersion.ToString(),
             CurrentGameVersion: gameVersion.ToString(),
-            LastSupportedSMAPIVersionForCurrentGameVersion: supportedSMAPIVersions.First(),
+            LastSupportedSMAPIVersionForCurrentGameVersion: supportedSMAPIVersion.ToString(),
             SMAPINexusModsLink: NexusModsSMAPILink
         );
     }
@@ -110,12 +106,82 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
 
     private async Task<SMAPIToGameMapping?> FetchSMAPIToGameMappings(CancellationToken cancellationToken)
     {
-        return _smapiToGameMappings ??= await FetchData<SMAPIToGameMapping>(GameToSMAPIMappingsDataUri, cancellationToken);
+        if (_smapiToGameMappings is not null) return _smapiToGameMappings;
+
+        var data = await FetchData<Dictionary<string, GameVersionsDTO>>(SMAPIToGameMappingsDataUri, cancellationToken);
+        if (data is null) return null;
+
+        var res = new Dictionary<Version, GameVersions>();
+        foreach (var kv in data)
+        {
+            var (sVersion, gameVersions) = kv;
+            var (sMinimumGameVersion, sMaximumGameVersion) = gameVersions;
+
+            if (!TryParseVersion(sVersion, out var version)) continue;
+
+            Version? minimumGameVersion = null, maximumGameVersion = null;
+            if (sMinimumGameVersion is not null) if (!TryParseVersion(sMinimumGameVersion, out minimumGameVersion)) continue;
+            if (sMaximumGameVersion is not null) if (!TryParseVersion(sMaximumGameVersion, out maximumGameVersion)) continue;
+
+            res[version] = new GameVersions
+            {
+                MinimumGameVersion = minimumGameVersion,
+                MaximumGameVersion = maximumGameVersion,
+            };
+        }
+
+        _smapiToGameMappings = res.ToImmutableDictionary(keyComparer: VersionComparer.Instance);
+        return _smapiToGameMappings;
+
     }
 
     private async Task<GameToSMAPIMapping?> FetchGameToSMAPIMappings(CancellationToken cancellationToken)
     {
-        return _gameToSMAPIMappings ??= await FetchData<GameToSMAPIMapping>(SMAPIToGameMappingsDataUri, cancellationToken);
+        if (_gameToSMAPIMappings is not null) return _gameToSMAPIMappings;
+
+        var data = await FetchData<Dictionary<string, string[]>>(GameToSMAPIMappingsDataUri, cancellationToken);
+        if (data is null) return null;
+
+        var res = new Dictionary<Version, Version>();
+        foreach (var kv in data)
+        {
+            var (sGameVersion, smapiVersions) = kv;
+            if (smapiVersions.Length == 0) continue;
+
+            if (!TryParseVersion(sGameVersion, out var gameVersion)) continue;
+
+            var sSMAPIVersion = smapiVersions.First();
+            if (!TryParseVersion(sSMAPIVersion, out var smapiVersion)) continue;
+
+            res[gameVersion] = smapiVersion;
+        }
+
+        _gameToSMAPIMappings = res.ToImmutableDictionary(keyComparer: VersionComparer.Instance);
+        return _gameToSMAPIMappings;
+    }
+
+    private bool TryParseVersion(string input, [NotNullWhen(true)] out Version? version)
+    {
+        version = null;
+        if (Version.TryParse(input, out var tmp))
+        {
+            version = SimplifyVersion(tmp);
+            return true;
+        }
+
+        _logger.LogWarning("Unable to parse string `{Input}` as a Version", input);
+        return false;
+    }
+
+    private static Version SimplifyVersion(Version input)
+    {
+        var major = input.Major;
+        var minor = input.Minor;
+        var build = input.Build == -1 ? 0 : input.Build;
+        var revision = input.Revision == 0 ? -1 : input.Revision;
+
+        if (revision != -1) return new Version(major, minor, build, revision);
+        return new Version(major, minor, build);
     }
 
     private async Task<T?> FetchData<T>(Uri dataUri, CancellationToken cancellationToken) where T : class
@@ -136,8 +202,40 @@ public class SMAPIGameVersionDiagnosticEmitter : ILoadoutDiagnosticEmitter
         }
     }
 
+    private class VersionComparer : IEqualityComparer<Version>
+    {
+        public static readonly IEqualityComparer<Version> Instance = new VersionComparer();
+
+        public bool Equals(Version? x, Version? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            return x.Equals(y);
+        }
+
+        public int GetHashCode(Version version)
+        {
+            return version.GetHashCode();
+        }
+    }
+
     [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
     public record GameVersions
+    {
+        public required Version? MinimumGameVersion { get; init; }
+
+        public required Version? MaximumGameVersion { get; init; }
+
+        public void Deconstruct(out Version? minimumGameVersion, out Version? maximumGameVersion)
+        {
+            minimumGameVersion = MinimumGameVersion;
+            maximumGameVersion = MaximumGameVersion;
+        }
+    }
+
+    [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
+    public record GameVersionsDTO
     {
         public required string? MinimumGameVersion { get; init; }
 
