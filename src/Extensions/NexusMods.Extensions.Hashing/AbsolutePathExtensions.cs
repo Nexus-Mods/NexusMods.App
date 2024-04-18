a6d27bb3bfd8142816e2c7bc7576b61d22e05e94
Sewer56 <seweratwork@gmail.com>
2024-04-18 02:45:56 +0100

    Changed: Use new MMF API in Memory Mapped XxHash

--- src/Extensions/NexusMods.Extensions.Hashing/AbsolutePathExtensions.cs
+++ src/Extensions/NexusMods.Extensions.Hashing/AbsolutePathExtensions.cs
@@ -39,33 +39,15 @@ public static class AbsolutePathExtensions
     /// <returns>The xxHash64 hash of the file.</returns>
     public static unsafe Hash XxHash64MemoryMapped(this AbsolutePath input, IActivitySource<Size>? job = null)
     {
-        #if DEBUG
-        // TODO: Assert that AbsolutePath uses RealFileSystem.
-        // This needs API update in paths, and an corresponding issue.
-        // If it's not RealFileSystem, we can fallback to the stream based version.
-        // Since we can't memory map, unless we add mmap ti Paths library.
-        #endif
-
-        var fullFilePath = input.GetFullPath();
         try
         {
-            using var memoryMappedFile = MemoryMappedFile.CreateFromFile(fullFilePath, FileMode.Open);
-            using var accessor = memoryMappedFile.CreateViewAccessor(0, 0, MemoryMappedFileAccess.Read);
-            var ptrData = (byte*)accessor.SafeMemoryMappedViewHandle.DangerousGetHandle();
-            var hashValue = XxHash64Algorithm.HashBytes(new ReadOnlySpan<byte>(ptrData, (int)accessor.Capacity));
-            job?.AddProgress(Size.FromLong(accessor.Capacity));
+            using var mmf =  input.FileSystem.CreateMemoryMappedFile(input, FileMode.Open, MemoryMappedFileAccess.Read);
+            var hashValue = XxHash64Algorithm.HashBytes(mmf.AsSpan());
+            job?.AddProgress(Size.FromLong((long)mmf.Length));
             return Hash.From(hashValue);
         }
         catch (ArgumentException)
         {
-            // TODO: A better way to catch this without a perf penalty.
-            // Wish there were lower level primitives for MemoryMappedFiles,
-            // but we have to live with CreateFromFile being the fastest for now.
-
-            // Empty file.
-            if (new FileInfo(fullFilePath).Length != 0) 
-                throw;
-
             return Hash.From(XxHash64Algorithm.HashOfEmptyFile);
         }
     }
