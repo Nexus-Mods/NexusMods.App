fdba6cbf5e04b7e7553d573d635b829c88e2e1bc
erri120 <erri120@protonmail.com>
2024-04-23 16:47:01 +0200

    Update

--- src/NexusMods.App.UI/Controls/MarkdownRenderer/MarkdownRendererView.axaml.cs
+++ src/NexusMods.App.UI/Controls/MarkdownRenderer/MarkdownRendererView.axaml.cs
@@ -1,5 +1,4 @@
 using System.Reactive.Disposables;
-using System.Reactive.Linq;
 using Avalonia.ReactiveUI;
 using JetBrains.Annotations;
 using ReactiveUI;
--- src/NexusMods.App.UI/Pages/Changelog/ChangelogPageViewModel.cs
+++ src/NexusMods.App.UI/Pages/Changelog/ChangelogPageViewModel.cs
@@ -2,8 +2,11 @@ using System.Reactive.Disposables;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using NexusMods.App.UI.Controls.MarkdownRenderer;
+using NexusMods.App.UI.Resources;
+using NexusMods.App.UI.Resources;
 using NexusMods.App.UI.Windows;
 using NexusMods.App.UI.WorkspaceSystem;
+using NexusMods.Icons;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
@@ -25,6 +28,10 @@ public class ChangelogPageViewModel : APageViewModel<IChangelogPageViewModel>, I
 
         this.WhenActivated(disposables =>
         {
+            var workspaceController = GetWorkspaceController();
+            workspaceController.SetTabTitle(Language.ChangelogPageViewModel_ChangelogPageViewModel_Changelog, WorkspaceId, PanelId, TabId);
+            workspaceController.SetIcon(IconValues.FileDocumentOutline, WorkspaceId, PanelId, TabId);
+
             MarkdownRendererViewModel.MarkdownUri = _changelogUri;
             Disposable.Create(() => { }).DisposeWith(disposables);
         });
