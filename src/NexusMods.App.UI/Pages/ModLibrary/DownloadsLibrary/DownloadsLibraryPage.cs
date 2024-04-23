5a61dbbdb908cda753c0dab2a89cc862e3ad9007
AL <26797547+Al12rs@users.noreply.github.com>
2024-04-23 12:35:35 +0200

    WIP: add page

---
+++ src/NexusMods.App.UI/Pages/ModLibrary/DownloadsLibrary/DownloadsLibraryPage.cs
@@ -0,0 +1,9 @@
+using NexusMods.Abstractions.Serialization.Attributes;
+using NexusMods.App.UI.WorkspaceSystem;
+
+namespace NexusMods.App.UI.Pages.ModLibrary;
+
+[JsonName("NexusMods.App.UI.Pages.ModLibrary.DownloadsLibrary.DownloadsLibraryPageContext")]
+public record DownloadsLibraryPageContext : IPageFactoryContext;
+
+
