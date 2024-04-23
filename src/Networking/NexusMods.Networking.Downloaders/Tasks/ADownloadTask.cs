302f8a1cb826a0e64de1e00fe69b6880a8691080
Timothy Baldridge <tbaldridge@gmail.com>
2024-04-23 17:04:49 -0600

    WIP

--- src/Networking/NexusMods.Networking.Downloaders/Tasks/ADownloadTask.cs
+++ src/Networking/NexusMods.Networking.Downloaders/Tasks/ADownloadTask.cs
@@ -12,7 +12,7 @@ using NexusMods.Paths;
 
 namespace NexusMods.Networking.Downloaders.Tasks;
 
-public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
+public abstract class ADownloadTask : IDownloadTask
 {
     private readonly IConnection _conn;
     protected DownloaderState.Model? PersistentState = null;
@@ -49,13 +49,13 @@ public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
     protected async Task<(string Name, Size Size)> GetNameAndSizeAsync(Uri uri)
     {
         if (uri.IsFile)
-            return new GetNameAndSizeResult(string.Empty, -1);
+            return (uri.FileName, uri.FileName.Size);
 
         var response = await HttpClient.GetAsync(uri, HttpCompletionOption.ResponseHeadersRead);
         if (!response.IsSuccessStatusCode)
         {
             Logger.LogWarning("HTTP request {Url} failed with status {ResponseStatusCode}", uri, response.StatusCode);
-            return new GetNameAndSizeResult(string.Empty, -1);
+            return default;
         }
 
         // Get the filename from the Content-Disposition header, or default to a temporary file name.
@@ -63,7 +63,9 @@ public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
                                        ?? response.Content.Headers.ContentDisposition?.FileName
                                        ?? Path.GetTempFileName();
 
-        return new GetNameAndSizeResult(contentDispositionHeader.Trim('"'), response.Content.Headers.ContentLength.GetValueOrDefault(0));
+        var name = contentDispositionHeader.Trim('"');
+        var size = Size.From((ulong)response.Content.Headers.ContentLength.GetValueOrDefault(0));
+        return (name, size);
     }
     
     protected async Task SetStatus(DownloadTaskStatus status)
@@ -71,9 +73,9 @@ public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
         using var tx = _conn.BeginTransaction();
         tx.Add(State!.Id, DownloaderState.Status, (byte)status);
         
-        if (_downloadState != null)
+        if (TransientState != null)
         {
-            var downloaded = _downloadState.ActivityStatus?.MakeTypedReport().Current.Value ?? Size.Zero;
+            var downloaded = TransientState.ActivityStatus?.MakeTypedReport().Current.Value ?? Size.Zero;
             tx.Add(State.Id, DownloaderState.Downloaded, downloaded);
         }
         
@@ -83,10 +85,10 @@ public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
     /// <inheritdoc />
     public Bandwidth CalculateThroughput()
     {
-        if (_downloadState!.Activity == null)
+        if (TransientState!.Activity == null)
             return Bandwidth.From(0);
 
-        var report = _downloadState.ActivityStatus?.GetReport() 
+        var report = TransientState.ActivityStatus?.GetReport() 
             as ActivityReport<Size>;
         var size = report?
             .Throughput
@@ -95,24 +97,11 @@ public abstract class ADownloadTask : IDownloadTask, INotifyPropertyChanged
         return Bandwidth.From(size.Value);
     }
 
-    public DownloaderState.Model State => _state!;
+    public DownloaderState.Model State => PersistentState!;
     
     public abstract Task StartAsync();
     public abstract void Cancel();
     public abstract void Suspend();
     public abstract Task Resume();
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string? propertyName = null)
-    {
-        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-        field = value;
-        OnPropertyChanged(propertyName);
-        return true;
-    }
+    public abstract Task AddInitData(ITransaction tx, DownloaderState.Model state);
 }
