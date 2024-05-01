ea7e1479f9c0ece8847ae2322a76c5522ba15728
halgari <tbaldridge@gmail.com>
2024-05-01 14:32:22 -0600

    Datamodel tests all pass

--- tests/NexusMods.DataModel.Tests/Harness/ALoadoutSynrconizerTest.cs
+++ tests/NexusMods.DataModel.Tests/Harness/ALoadoutSynrconizerTest.cs
@@ -55,17 +55,17 @@ public class ALoadoutSynrchonizerTest<T> : ADataModelTest<T>
 
         public Task<Dictionary<Hash, byte[]>> ExtractFiles(IEnumerable<Hash> files, CancellationToken token = default)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public Task<Stream> GetFileStream(Hash hash, CancellationToken token = default)
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public HashSet<ulong> GetFileHashes()
         {
-            throw new NotImplementedException();
+            throw new NotSupportedException();
         }
 
         public async Task ExtractFiles(IEnumerable<(Hash Src, IStreamFactory Dest)> files, CancellationToken token = default)
@@ -96,72 +96,3 @@ public class ALoadoutSynrchonizerTest<T> : ADataModelTest<T>
         }
     }
 }
-
-/*
-[JsonName("NexusMods.DataModel.Tests.Harness.TestGeneratedFile")]
-public record TestGeneratedFile : AModFile, IToFile, ITriggerFilter<ModFilePair, Plan>
-{
-    public ITriggerFilter<ModFilePair, Plan> TriggerFilter => this;
-    public Task<Hash> GenerateAsync(Stream stream, ApplyPlan loadout, CancellationToken cancellationToken = default)
-    {
-        throw new NotImplementedException();
-    }
-
-    public required GamePath To { get; init; }
-    public Hash GetFingerprint(ModFilePair self, Plan plan)
-    {
-        using var printer = Fingerprinter.Create();
-        foreach (var mod in plan.Loadout.Mods)
-        {
-            if (mod.Value.Enabled == false)
-                continue;
-
-            foreach (var file in mod.Value.Files)
-                if (!file.Value.Id.Equals(self.File.Id))
-                    printer.Add(file.Value.DataStoreId);
-        }
-
-        return printer.Digest();
-    }
-}
-
-
-/// <summary>
-/// Example generated sort rule that sorts all mods alphabetically
-/// </summary>
-[JsonName("NexusMods.DataModel.Tests.Harness.TestGeneratedSortRule")]
-public class AlphabeticalSort : IGeneratedSortRule, ISortRule<Mod, ModId>, ITriggerFilter<ModId, Loadout>
-{
-    public ITriggerFilter<ModId, Loadout> TriggerFilter => this;
-
-#pragma warning disable CS1998 // Async method lacks 'await' operators and will run synchronously
-    public async IAsyncEnumerable<ISortRule<Mod, ModId>> GenerateSortRules(ModId selfId, Loadout loadout)
-#pragma warning restore CS1998 // Async method lacks 'await' operators and will run synchronously
-    {
-        var thisMod = loadout.Mods[selfId];
-        foreach (var (modId, other) in loadout.Mods)
-        {
-            if (modId.Equals(selfId)) continue;
-            if (string.Compare(other.Name, thisMod.Name, StringComparison.Ordinal) > 0)
-            {
-                yield return new Before<Mod, ModId> { Other = other.Id };
-            }
-            else
-            {
-                yield return new After<Mod, ModId> { Other = modId };
-            }
-        }
-    }
-
-    public Hash GetFingerprint(ModId self, Loadout loadout)
-    {
-        using var fp = Fingerprinter.Create();
-        fp.Add(loadout.Mods[self].DataStoreId);
-        foreach (var name in loadout.Mods.Values.Select(n => n.Name).Order())
-        {
-            fp.Add(name);
-        }
-        return fp.Digest();
-    }
-}
-*/
