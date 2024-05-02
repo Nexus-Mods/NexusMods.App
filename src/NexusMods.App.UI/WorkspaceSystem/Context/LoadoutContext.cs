8a38e5984228f30ffb5e378ca8ef33c55edc935b
Sewer56 <sewer56lol@googlemail.com>
2024-05-02 15:19:55 +0100

    Added: Note on why we sanitize LoadoutContext.

--- src/NexusMods.App.UI/WorkspaceSystem/Context/LoadoutContext.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/Context/LoadoutContext.cs
@@ -11,6 +11,18 @@ public record LoadoutContext : IWorkspaceContext
 
     public bool IsValid(IServiceProvider serviceProvider)
     {
+        /*
+            Note(Sewer)
+            
+            In App, we create a loadout workspace for each loadout.
+
+            It may be possible that this loadout no longer exists, as a result
+            of removal from the CLI (UI not running) or a crash during removal.
+            [i.e. in cases we may have not run explicit workspace delete]
+            
+            In such cases, we may need to discard invalid loadouts, which we
+            do here.
+        */
         var loadoutRegistry = serviceProvider.GetRequiredService<ILoadoutRegistry>();
         return loadoutRegistry.Contains(LoadoutId);
     }
