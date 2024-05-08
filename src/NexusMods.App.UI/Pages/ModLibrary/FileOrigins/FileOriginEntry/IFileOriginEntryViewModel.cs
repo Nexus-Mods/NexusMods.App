2b557df04624493c9eed21ba277308e757e98967
AL <26797547+Al12rs@users.noreply.github.com>
2024-05-08 16:31:43 +0200

    Cleanup interface

--- src/NexusMods.App.UI/Pages/ModLibrary/FileOrigins/FileOriginEntry/IFileOriginEntryViewModel.cs
+++ src/NexusMods.App.UI/Pages/ModLibrary/FileOrigins/FileOriginEntry/IFileOriginEntryViewModel.cs
@@ -9,11 +9,7 @@ public interface IFileOriginEntryViewModel : IViewModelInterface
     string Name { get; init;}
     string Version { get; init; }
     Size Size { get; init; }    
-    DateTime ArchiveDate { get; init; }
     string DisplayArchiveDate { get; }
-    
-    DateTime LastInstalledDate { get; set; }
-    
     string DisplayLastInstalledDate { get; }
     ReactiveCommand<Unit, Unit> AddToLoadoutCommand { get; init; }
 }
