ef0f1f9933c5f2a343057d91cbde04f07e1ac538
AL <26797547+Al12rs@users.noreply.github.com>
2024-05-09 18:51:43 +0200

    This prevents the Design Preview process from locking the database, blocking actually running the app on windows after opening any xaml file in the IDE.

--- src/NexusMods.App/Program.cs
+++ src/NexusMods.App/Program.cs
@@ -9,6 +9,7 @@ using NexusMods.Abstractions.Settings;
 using NexusMods.Abstractions.Telemetry;
 using NexusMods.App.BuildInfo;
 using NexusMods.App.UI;
+using NexusMods.DataModel;
 using NexusMods.Paths;
 using NexusMods.Settings;
 using NexusMods.SingleProcess;
@@ -40,7 +41,8 @@ public class Program
         var host = BuildHost(
             slimMode: !isMain,
             telemetrySettings,
-            loggingSettings
+            loggingSettings,
+            isAvaloniaDesigner: true
         );
 
         // Okay to do wait here, as we are in the main process thread.
@@ -160,14 +162,30 @@ public class Program
     /// <summary>
     /// Constructs the host for the application, if <paramref name="slimMode"/> is true, the host will not register
     /// most of the services, and will only register what is required to proxy the app to the main process.
+    /// <paramref name="isAvaloniaDesigner"/> should be set to true when constructing the host for the Avalonia Designer
+    /// and will use the in memory database 
     /// </summary>
     private static IHost BuildHost(
         bool slimMode,
         TelemetrySettings telemetrySettings,
-        LoggingSettings loggingSettings)
+        LoggingSettings loggingSettings,
+        bool isAvaloniaDesigner = false)
     {
         var host = new HostBuilder()
-            .ConfigureServices(services => services.AddApp(telemetrySettings, slimMode: slimMode).Validate())
+            .ConfigureServices(services =>
+                {
+                    var s = services.AddApp(telemetrySettings, slimMode: slimMode).Validate();
+
+                    if (isAvaloniaDesigner)
+                    {
+                        s.OverrideSettingsForTests<DataModelSettings>(settings => settings with
+                            {
+                                UseInMemoryDataModel = true,
+                            }
+                        );
+                    }
+                }
+            )
             .ConfigureLogging((_, builder) => AddLogging(builder, loggingSettings, isMainProcess: !slimMode))
             .Build();
 
@@ -233,8 +251,16 @@ public class Program
     // ReSharper disable once UnusedMember.Global
     public static AppBuilder BuildAvaloniaApp()
     {
-        var host = BuildHost(slimMode: false, telemetrySettings: new TelemetrySettings(), LoggingSettings.CreateDefault(OSInformation.Shared));
+        var host = BuildHost(slimMode: false, 
+            telemetrySettings: new TelemetrySettings(), 
+            LoggingSettings.CreateDefault(OSInformation.Shared),
+            isAvaloniaDesigner: true);
+        
+        // We have to initialize async services here, waiting is bad, but we don't have many alternatives
+        host.StartAsync().Wait(timeout: TimeSpan.FromMinutes(5));
+        
         DesignerUtils.Activate(host.Services);
+        
         return Startup.BuildAvaloniaApp(host.Services);
     }
 }
