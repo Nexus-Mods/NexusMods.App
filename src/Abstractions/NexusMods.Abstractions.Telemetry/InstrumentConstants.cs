11eafe33861e5f0de4ce194240acfb3ceddbae80
erri120 <erri120@fossmailer.de>
2024-05-29 13:11:11 +0200

    Add Managed Games Counter

--- src/Abstractions/NexusMods.Abstractions.Telemetry/InstrumentConstants.cs
+++ src/Abstractions/NexusMods.Abstractions.Telemetry/InstrumentConstants.cs
@@ -5,6 +5,7 @@ internal static class InstrumentConstants
     private const string Prefix = "app_";
 
     public const string NameActiveUsers        = Prefix + "active_users";
+    public const string NameManagedGamesCount   = Prefix + "managed_games_count";
     public const string NameUsersPerLanguage   = Prefix + "users_per_language";
     public const string NameUsersPerMembership = Prefix + "users_per_membership";
     public const string NameUsersPerOS         = Prefix + "users_per_os";
--- src/NexusMods.App/TelemetryProvider.cs
+++ src/NexusMods.App/TelemetryProvider.cs
@@ -1,5 +1,9 @@
 using System.Reactive.Disposables;
+using DynamicData.Binding;
 using Microsoft.Extensions.DependencyInjection;
+using NexusMods.Abstractions.Games.DTO;
+using NexusMods.Abstractions.Loadouts;
+using NexusMods.Abstractions.MnemonicDB.Attributes;
 using NexusMods.Abstractions.Telemetry;
 using NexusMods.App.UI;
 using NexusMods.Networking.NexusWebApi;
@@ -9,12 +13,14 @@ namespace NexusMods.App;
 
 internal sealed class TelemetryProvider : ITelemetryProvider, IDisposable
 {
-    private readonly CompositeDisposable _disposable;
+    private readonly CompositeDisposable _disposable = new();
+    private readonly IRepository<Loadout.Model> _loadoutRepository;
 
     public TelemetryProvider(IServiceProvider serviceProvider)
     {
-        _disposable = new CompositeDisposable();
+        _loadoutRepository = serviceProvider.GetRequiredService<IRepository<Loadout.Model>>();
 
+        // membership status
         var loginManager = serviceProvider.GetRequiredService<LoginManager>();
         loginManager.IsPremium.SubscribeWithErrorLogging(value => _isPremium = value).DisposeWith(_disposable);
     }
@@ -25,12 +31,21 @@ internal sealed class TelemetryProvider : ITelemetryProvider, IDisposable
         meterConfig.CreateUsersPerOSCounter();
         meterConfig.CreateUsersPerLanguageCounter();
         meterConfig.CreateUsersPerMembershipCounter(GetMembership);
+        meterConfig.CreateManagedGamesCounter(GetManagedGamesCount);
     }
 
     private bool _isPremium;
     private OneOf<MembershipStatus.None, MembershipStatus.Premium> GetMembership()
     {
         return _isPremium ? new MembershipStatus.Premium() : new MembershipStatus.None();
+    }
+
+    private int GetManagedGamesCount()
+    {
+        return _loadoutRepository.All
+            .Select(x => x.Installation.Game.Domain)
+            .Distinct()
+            .Count();
     }
 
     public void Dispose()
