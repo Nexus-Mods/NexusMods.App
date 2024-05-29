8862f213d7995eb26b434e57701e4042b0070705
Timothy Baldridge <tbaldridge@gmail.com>
2024-05-28 21:23:38 -0600

    Fix the structure a bit

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -4,5 +4,6 @@ rm -rf bin/macos-arm64
 dotnet clean
 dotnet restore
 dotnet publish src/NexusMods.App/NexusMods.App.csproj -c Release -r osx-arm64 --output bin/macos-arm64/NexusMods.app/Contents --self-contained -p:PublishReadyToRun=true #-p:PublishSingleFile=true
-mkdir bin/macos/NexusMods.App/Contents/MacOS
-cp -Rv build/macoOS/* bin/macos/NexusMods.app/Contents/MacOS
+cp -Rv build/macOS/structure/* bin/macos/NexusMods.app/
+mkdir bin/macos-arm64/NexusMods.App/Contents/MacOS
+
