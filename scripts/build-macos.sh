2df7b44a91d2372104ed5337790d09624542324d
Timothy Baldridge <tbaldridge@gmail.com>
2024-05-28 22:17:47 -0600

    WIP

--- scripts/build-macos.sh
+++ scripts/build-macos.sh
@@ -3,7 +3,6 @@
 rm -rf bin/macos-arm64
 dotnet clean
 dotnet restore
-dotnet publish src/NexusMods.App/NexusMods.App.csproj -c Release -r osx-arm64 --output bin/macos-arm64/NexusMods.app/Contents --self-contained -p:PublishReadyToRun=true #-p:PublishSingleFile=true
-cp -Rv build/macOS/structure/* bin/macos/NexusMods.app/
-mkdir bin/macos-arm64/NexusMods.App/Contents/MacOS
+dotnet publish src/NexusMods.App/NexusMods.App.csproj -c Release -r osx-arm64 --output bin/macos-arm64/NexusMods.app/Contents/MacOS --self-contained -p:PublishReadyToRun=true #-p:PublishSingleFile=true
+cp -Rv build/macOS/structure/* bin/macos-arm64/NexusMods.app/
 
