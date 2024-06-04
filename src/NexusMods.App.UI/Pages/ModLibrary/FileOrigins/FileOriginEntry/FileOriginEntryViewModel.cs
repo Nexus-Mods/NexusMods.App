1b8a148b948260ab4de41df2f1a8d8c6044b01b8
Sewer56 <sewer56lol@googlemail.com>
2024-06-04 17:38:55 +0100

    Removed Redundant Comment

--- src/NexusMods.App.UI/Pages/ModLibrary/FileOrigins/FileOriginEntry/FileOriginEntryViewModel.cs
+++ src/NexusMods.App.UI/Pages/ModLibrary/FileOrigins/FileOriginEntry/FileOriginEntryViewModel.cs
@@ -69,8 +69,7 @@ public class FileOriginEntryViewModel : AViewModel<IFileOriginEntryViewModel>, I
         ArchiveDate = fileOrigin.GetCreatedAt();
 
         var loadout = conn.Db.Get<Loadout.Model>(loadoutId.Value);
-        
-        // Get
+
         _isModAddedToLoadout = conn.Revisions(loadoutId)
             .StartWith(loadout)
             .Select(rev => rev.Mods.Any(mod => mod.Contains(Mod.Source) && mod.SourceId.Equals(fileOrigin.Id)))
