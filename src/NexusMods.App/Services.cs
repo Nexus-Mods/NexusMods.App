a93ff7e39f5769d5db23b5dbe8e09c02901de49b
halgari <tbaldridge@gmail.com>
2024-06-26 21:50:50 -0600

    Disable Darkest Dungeon

--- src/NexusMods.App/Services.cs
+++ src/NexusMods.App/Services.cs
@@ -109,7 +109,6 @@ public static class Services
         {
             Games.BethesdaGameStudios.Services.AddBethesdaGameStudios(services);
             Games.RedEngine.Services.AddRedEngineGames(services);
-            Games.DarkestDungeon.Services.AddDarkestDungeon(services);
         }
         
         Games.StardewValley.Services.AddStardewValley(services);
