79477f9b5e2c285aa4d6aa5a1567b035e661489f
AL <26797547+Al12rs@users.noreply.github.com>
2024-07-04 12:45:40 +0200

    Add designVM

---
+++ src/NexusMods.App.UI/Controls/LoadoutBadge/LoadoutBadgeDesignVM.cs
@@ -0,0 +1,64 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace NexusMods.App.UI.Controls.LoadoutBadge;
+
+public class LoadoutBadgeDesignVM : AViewModel<ILoadoutBadgeVM>, ILoadoutBadgeVM
+{
+    private enum LoadoutAppliedState
+    {
+        NotApplied,
+        Applying,
+        Applied,
+    }
+    private LoadoutAppliedState _loadoutAppliedState = LoadoutAppliedState.NotApplied;
+    
+    public LoadoutBadgeDesignVM()
+    {
+        this.WhenActivated(d =>
+            {
+                // Cycle thorough all the states for preview purposes
+                
+                Observable.Interval(TimeSpan.FromSeconds(0.5))
+                    .Subscribe(_ =>
+                    {
+                        IsLoadoutSelected = !IsLoadoutSelected;
+                    })
+                    .DisposeWith(d);
+                
+                Observable.Interval(TimeSpan.FromSeconds(1))
+                    .Subscribe(_ =>
+                    {
+                        switch (_loadoutAppliedState)
+                        {
+                            case LoadoutAppliedState.NotApplied:
+                                IsLoadoutApplied = false;
+                                IsLoadoutInProgress = false;
+                                _loadoutAppliedState = LoadoutAppliedState.Applying;
+                                break;
+                            case LoadoutAppliedState.Applying:
+                                IsLoadoutApplied = false;
+                                IsLoadoutInProgress = true;
+                                _loadoutAppliedState = LoadoutAppliedState.Applied;
+                                break;
+                            case LoadoutAppliedState.Applied:
+                                IsLoadoutApplied = true;
+                                IsLoadoutInProgress = false;
+                                _loadoutAppliedState = LoadoutAppliedState.NotApplied;
+                                break;
+                            default:
+                                throw new ArgumentOutOfRangeException();
+                        }
+                    })
+                    .DisposeWith(d);
+            }
+        );
+    }
+    
+    public string LoadoutShortName { get; } = "A";
+    [Reactive] public bool IsLoadoutSelected { get; private set; } = false;
+    [Reactive] public bool IsLoadoutApplied { get; private set; } = false;
+    [Reactive] public bool IsLoadoutInProgress { get; private set; } = false;
+}
