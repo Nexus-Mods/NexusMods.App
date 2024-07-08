894ea882326e3fbff1534bffc8b6fc555b1e371f
AL <26797547+Al12rs@users.noreply.github.com>
2024-07-08 11:20:32 +0200

    WIP: LoadoutCardView initial structure

---
+++ src/NexusMods.App.UI/Controls/LoadoutCard/LoadoutCardView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.ReactiveUI;
+
+namespace NexusMods.App.UI.Controls.LoadoutCard;
+
+public partial class LoadoutCardView : ReactiveUserControl<ILoadoutCardVM>
+{
+    public LoadoutCardView()
+    {
+        InitializeComponent();
+    }
+}
+
