cbe0c2093648b612cffca726b15a17425b62722d
AL <26797547+Al12rs@users.noreply.github.com>
2024-07-09 18:01:11 +0200

    LoadoutCardVM - LoadoutCardViewModel

--- src/NexusMods.App.UI/Controls/LoadoutCard/ILoadoutCardVM.cs
+++
@@ -1,6 +0,0 @@
-namespace NexusMods.App.UI.Controls.LoadoutCard;
-
-public interface ILoadoutCardVM : IViewModelInterface
-{
-    
-}
--- src/NexusMods.App.UI/Controls/LoadoutCard/LoadoutCardView.axaml.cs
+++ src/NexusMods.App.UI/Controls/LoadoutCard/LoadoutCardView.axaml.cs
@@ -2,7 +2,7 @@ using Avalonia.ReactiveUI;
 
 namespace NexusMods.App.UI.Controls.LoadoutCard;
 
-public partial class LoadoutCardView : ReactiveUserControl<ILoadoutCardVM>
+public partial class LoadoutCardView : ReactiveUserControl<ILoadoutCardViewModel>
 {
     public LoadoutCardView()
     {
