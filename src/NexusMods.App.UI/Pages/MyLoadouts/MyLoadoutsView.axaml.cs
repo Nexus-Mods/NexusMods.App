f2ad6ca0a7efb0c51fbf76f685b6de1a8e262572
AL <26797547+Al12rs@users.noreply.github.com>
2024-07-16 12:17:26 +0200

    Add starting layout for MyLoadouts page view

---
+++ src/NexusMods.App.UI/Pages/MyLoadouts/MyLoadoutsView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.ReactiveUI;
+
+namespace NexusMods.App.UI.Pages.MyLoadouts;
+
+public partial class MyLoadoutsView : ReactiveUserControl<IMyLoadoutsViewModel>
+{
+    public MyLoadoutsView()
+    {
+        InitializeComponent();
+    }
+}
+
