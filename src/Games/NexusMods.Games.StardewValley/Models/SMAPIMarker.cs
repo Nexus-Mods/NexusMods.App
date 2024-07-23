429d4be499e7453ac88c3800b921706fdc99e40d
erri120 <erri120@fossmailer.de>
2024-07-23 13:00:24 +0200

    Update Stardew Valley models

--- src/Games/NexusMods.Games.StardewValley/Models/SMAPIMarker.cs
+++ src/Games/NexusMods.Games.StardewValley/Models/SMAPIMarker.cs
@@ -5,9 +5,10 @@ namespace NexusMods.Games.StardewValley.Models;
 /// <summary>
 /// Marker attribute for SMAPI
 /// </summary>
+[Obsolete($"To be replaced by {nameof(SMAPILoadoutItem)}")]
 public static class SMAPIMarker
 {
     private const string Namespace = "NexusMods.Games.StardewValley.Models.SMAPIMarker";
-    
+
     public static readonly StringAttribute Version = new(Namespace, nameof(Version));
 }
---
+++ src/Games/NexusMods.Games.StardewValley/Models/SMAPIModDatabaseLoadoutFile.cs
@@ -0,0 +1,13 @@
+using NexusMods.Abstractions.Loadouts;
+using NexusMods.MnemonicDB.Abstractions.Attributes;
+using NexusMods.MnemonicDB.Abstractions.Models;
+
+namespace NexusMods.Games.StardewValley.Models;
+
+[Include<LoadoutFile>]
+public partial class SMAPIModDatabaseLoadoutFile : IModelDefinition
+{
+    private const string Namespace = "NexusMods.StardewValley.SMAPIModDatabaseLoadoutFile";
+
+    public static readonly MarkerAttribute IsModDatabaseFileMarker = new(Namespace, nameof(IsModDatabaseFileMarker));
+}
