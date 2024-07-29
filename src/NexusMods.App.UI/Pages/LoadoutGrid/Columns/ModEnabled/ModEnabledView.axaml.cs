863bb9244bd0beb770460e5e4394569cb61ad456
erri120 <erri120@fossmailer.de>
2024-07-29 12:56:09 +0200

    Remove delete button

--- src/NexusMods.App.UI/Pages/LoadoutGrid/Columns/ModEnabled/ModEnabledView.axaml.cs
+++ src/NexusMods.App.UI/Pages/LoadoutGrid/Columns/ModEnabled/ModEnabledView.axaml.cs
@@ -11,7 +11,6 @@ namespace NexusMods.App.UI.Pages.LoadoutGrid.Columns.ModEnabled;
 
 public partial class ModEnabledView : ReactiveUserControl<IModEnabledViewModel>
 {
-    
     public ModEnabledView()
     {
         InitializeComponent();
@@ -23,18 +22,12 @@ public partial class ModEnabledView : ReactiveUserControl<IModEnabledViewModel>
                 .BindTo(this, view => view.EnabledToggleSwitch.IsChecked)
                 .DisposeWith(d);
 
-            // this.BindCommand(ViewModel, vm => vm.DeleteModCommand, view => view.DeleteButton)
-            //     .DisposeWith(d);
-
-            var isCheckedObservable = this.WhenAnyValue(view => view.EnabledToggleSwitch.IsChecked)
+            var isCheckedObservable = this
+                .WhenAnyValue(view => view.EnabledToggleSwitch.IsChecked)
                 .Select(isChecked => isChecked ?? false);
-            
-            this.BindCommand(ViewModel, 
-                    vm => vm.ToggleEnabledCommand, 
-                    view => view.EnabledToggleSwitch,
-                    isCheckedObservable)
+
+            this.BindCommand(ViewModel, vm => vm.ToggleEnabledCommand, view => view.EnabledToggleSwitch, isCheckedObservable)
                 .DisposeWith(d);
-            
         });
     }
 }
