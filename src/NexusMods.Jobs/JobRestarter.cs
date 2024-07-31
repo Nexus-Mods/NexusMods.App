1fe651193cedb79557155543ea3f59d95a1f8e17
halgari <tbaldridge@gmail.com>
2024-07-31 14:25:12 -0600

    Add logging to the job restarter

--- src/NexusMods.Jobs/JobRestarter.cs
+++ src/NexusMods.Jobs/JobRestarter.cs
@@ -1,10 +1,11 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using NexusMods.Abstractions.Jobs;
 using NexusMods.MnemonicDB.Abstractions;
 
 namespace NexusMods.Jobs;
 
-public class JobRestarter(IConnection connection) : IHostedService
+public class JobRestarter(IConnection connection, ILogger<JobRestarter> logger) : IHostedService
 {
     public async Task StartAsync(CancellationToken cancellationToken)
     {
@@ -16,8 +17,15 @@ public class JobRestarter(IConnection connection) : IHostedService
 
         foreach (var jobState in jobsToRestart)
         {
-            var job = jobState.Worker.LoadJob(jobState);
-            await job.StartAsync(CancellationToken.None);
+            try
+            {
+                var job = jobState.Worker.LoadJob(jobState);
+                await job.StartAsync(CancellationToken.None);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e, "Failed to restart job {JobId}", jobState.Id);
+            }
         }
     }
 
