8361785b4b1c751e9f05283cfe1445ef332b8625
Timothy Baldridge <tbaldridge@gmail.com>
2024-08-07 06:25:10 -0600

    Revive RedMod Deployment (#1851)
    * Call the deploy tool as part of the sync code
    
    * Fixed some of the deploy bugs, a few odd ones left
    
    * Fix ignored paths
    
    * :facepalm: the word is `tools` halgari, not `rools`
    
    * Use IProcessFactory
    
    * Don't recursively delete loadouts so we end up deleting the game metadata as well
    
    * Update src/Games/NexusMods.Games.RedEngine/Cyberpunk2077/Cyberpunk2077Synchronizer.cs
    
    Co-authored-by: erri120 <erri120@fossmailer.de>
    
    ---------
    
    Co-authored-by: erri120 <erri120@fossmailer.de>

--- src/Games/NexusMods.Games.RedEngine/RedModDeployTool.cs
+++ src/Games/NexusMods.Games.RedEngine/RedModDeployTool.cs
@@ -4,18 +4,22 @@ using Microsoft.Extensions.Logging;
 using NexusMods.Abstractions.GameLocators;
 using NexusMods.Abstractions.Games.DTO;
 using NexusMods.Abstractions.Loadouts;
+using NexusMods.CrossPlatform.Process;
 
 namespace NexusMods.Games.RedEngine;
 
 public class RedModDeployTool : ITool
 {
     private static readonly GamePath RedModPath = new(LocationId.Game, "tools/redmod/bin/redmod.exe");
+    private static readonly GamePath RedModDeployFolder = new(LocationId.Game, "r6/cache/modded");
 
     private readonly ILogger<RedModDeployTool> _logger;
+    private readonly IProcessFactory _processFactory;
 
-    public RedModDeployTool(ILogger<RedModDeployTool> logger)
+    public RedModDeployTool(ILogger<RedModDeployTool> logger, IProcessFactory processFactory)
     {
         _logger = logger;
+        _processFactory = processFactory;
     }
 
     public IEnumerable<GameDomain> Domains => new[] { Cyberpunk2077.Cyberpunk2077Game.StaticDomain };
@@ -23,23 +27,16 @@ public class RedModDeployTool : ITool
     public async Task Execute(Loadout.ReadOnly loadout, CancellationToken cancellationToken)
     {
         var exe = RedModPath.CombineChecked(loadout.InstallationInstance);
+        var deployFolder = RedModDeployFolder.CombineChecked(loadout.InstallationInstance);
+        
+        // RedMod deploys to this folder and freaks out if it doesn't exist, but our synchronizer will
+        // delete it if it's empty, so we need to recreate it here.
+        if (!deployFolder.DirectoryExists())
+            deployFolder.CreateDirectory();
 
-        var stdOutBuffer = new StringBuilder();
-        var stdErrBuffer = new StringBuilder();
-
-        _logger.LogInformation("Running {Program}", exe);
-        var result = await Cli.Wrap(exe.ToString())
+        await _processFactory.ExecuteAsync(Cli.Wrap(exe.ToString())
             .WithArguments("deploy")
-            .WithWorkingDirectory(loadout.InstallationInstance.LocationsRegister[LocationId.Game].ToString())
-            .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
-            .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
-            .ExecuteAsync();
-        _logger.LogInformation("Finished running {Program}", exe);
-
-        if (result.ExitCode != 0)
-        {
-            _logger.LogError("RedMod Deploy failed with exit code {ExitCode}", result.ToString());
-        }
+            .WithWorkingDirectory(exe.Parent.ToString()), cancellationToken: cancellationToken);
     }
 
     public string Name => "RedMod Deploy";
