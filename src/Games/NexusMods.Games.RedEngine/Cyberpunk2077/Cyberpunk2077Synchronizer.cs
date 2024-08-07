f4cbb6ce47c571b986c6a31d250a7ec0d9fa921f
Timothy Baldridge <tbaldridge@gmail.com>
2024-08-07 06:00:50 -0600

    Update src/Games/NexusMods.Games.RedEngine/Cyberpunk2077/Cyberpunk2077Synchronizer.cs
    Co-authored-by: erri120 <erri120@fossmailer.de>

--- src/Games/NexusMods.Games.RedEngine/Cyberpunk2077/Cyberpunk2077Synchronizer.cs
+++ src/Games/NexusMods.Games.RedEngine/Cyberpunk2077/Cyberpunk2077Synchronizer.cs
@@ -62,6 +62,12 @@ public class Cyberpunk2077Synchronizer : ALoadoutSynchronizer
         if (!hasRedMods) 
             return loadout;
         
+        if (!OSInformation.Shared.IsWindows)
+        {
+            // TODO: run redmod on non-Windows systems (https://github.com/Nexus-Mods/NexusMods.App/issues/308)
+            return loadout;
+        }
+
         await _redModTool.Execute(loadout, CancellationToken.None);
         return await base.Synchronize(loadout);
 
