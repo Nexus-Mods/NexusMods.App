33614c7506dc5228477ca2bcf31374de1a3bb387
erri120 <erri120@fossmailer.de>
2024-08-08 11:14:56 +0200

    Fix tests

--- src/Networking/NexusMods.Networking.HttpDownloader/Services.cs
+++ src/Networking/NexusMods.Networking.HttpDownloader/Services.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using NexusMods.Abstractions.HttpDownloader;
 using NexusMods.Abstractions.Jobs;
 using NexusMods.Abstractions.Settings;
 
@@ -9,6 +10,7 @@ public static class Services
     public static IServiceCollection AddHttpDownloader(this IServiceCollection services)
     {
         return services
+            .AddSingleton<IHttpDownloader, SimpleHttpDownloader>()
             .AddSettings<HttpDownloaderSettings>()
             .AddWorker<HttpDownloadJobWorker>()
             .AddHttpDownloadJobPersistedStateModel();
