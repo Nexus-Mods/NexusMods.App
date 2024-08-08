98c4a315eb2387c0fb9c634bc9651e6c7902f6e3
AL <26797547+Al12rs@users.noreply.github.com>
2024-08-08 15:32:08 +0200

    Update LoadoutNameProvider to improve performance (@erri120)

--- src/Abstractions/NexusMods.Abstractions.Loadouts/LoadoutNameProvider.cs
+++ src/Abstractions/NexusMods.Abstractions.Loadouts/LoadoutNameProvider.cs
@@ -5,33 +5,48 @@ namespace NexusMods.Abstractions.Loadouts;
 /// </summary>
 public static class LoadoutNameProvider
 {
-    private static readonly string[] Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".Select(c => c.ToString()).ToArray();
-    private static readonly string[] AllCombinations = GenerateAllCombinations();
-    
-    private static string[] GenerateAllCombinations()
-    {
-        var combinations = new List<string>(Alphabet);
-        combinations.AddRange(from first in Alphabet from second in Alphabet select first + second);
-        return combinations.ToArray();
-    }
-
     /// <summary>
     /// Returns a capital one or two letter short name for a loadout.
     /// It will cycle through the alphabet, starting with 'A' and continue to AA.
     /// If all two letter combinations are used, a duplicate name will be returned.
     /// Short names should not be assumed to be unique, they are only used for user disambiguation.
     /// </summary>
-    public static string GetNewShortName(string[] existingLoadoutsShortNames)
+    public static string GetNewShortName(ReadOnlySpan<string> existingLoadoutsShortNames)
     {
-        var usedNames = new HashSet<string>(existingLoadoutsShortNames);
-        foreach (var name in AllCombinations)
+        for (var i = 'A'; i <= 'Z'; i++)
         {
-            if (!usedNames.Contains(name))
+            var found = false;
+            foreach (var name in existingLoadoutsShortNames)
             {
-                return name;
+                if (name.Length != 1) continue;
+                if (name[0] != i) continue;
+
+                found = true;
+                break;
+            }
+
+            if (!found) return $"{i}";
+        }
+
+        for (var i = 'A'; i <= 'Z'; i++)
+        {
+            for (var j = 'A'; j <= 'Z'; j++)
+            {
+                var found = false;
+                foreach (var name in existingLoadoutsShortNames)
+                {
+                    if (name.Length != 2) continue;
+                    if (name[0] != i || name[1] != j) continue;
+
+                    found = true;
+                    break;
+                }
+
+                if (!found) return $"{i}{j}";
             }
         }
-        // If all combinations are used, use "ZZ".
+
         return "ZZ";
     }
+    
 }
