9b78127d93ce4f823cfa3128736d922392166756
erri120 <erri120@fossmailer.de>
2024-08-15 14:26:07 +0200

    Fix free user downloads (#1882)
    * Fix
    
    * Fix download for non-premium users

--- src/Networking/NexusMods.Networking.NexusWebApi/NexusModsLibrary.cs
+++ src/Networking/NexusMods.Networking.NexusWebApi/NexusModsLibrary.cs
@@ -1,9 +1,11 @@
+using DynamicData.Kernel;
 using JetBrains.Annotations;
 using Microsoft.Extensions.DependencyInjection;
 using NexusMods.Abstractions.Games.DTO;
 using NexusMods.Abstractions.Jobs;
 using NexusMods.Abstractions.NexusModsLibrary;
 using NexusMods.Abstractions.NexusWebApi;
+using NexusMods.Abstractions.NexusWebApi.DTOs;
 using NexusMods.Abstractions.NexusWebApi.Types;
 using NexusMods.Extensions.BCL;
 using NexusMods.MnemonicDB.Abstractions;
@@ -22,11 +24,11 @@ public class NexusModsLibrary
     /// <summary>
     /// Constructor.
     /// </summary>
-    public NexusModsLibrary(IServiceProvider serviceProvider, IConnection connection, INexusApiClient apiClient)
+    public NexusModsLibrary(IServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
-        _connection = connection;
-        _apiClient = apiClient;
+        _connection = serviceProvider.GetRequiredService<IConnection>();
+        _apiClient = serviceProvider.GetRequiredService<INexusApiClient>();
     }
 
     public async Task<NexusModsModPageMetadata.ReadOnly> GetOrAddModPage(
@@ -81,9 +83,36 @@ public class NexusModsLibrary
         return txResults.Remap(newFile);
     }
 
-    public async Task<Uri> GetDownloadUri(NexusModsFileMetadata.ReadOnly file, CancellationToken cancellationToken = default)
+    public async Task<Uri> GetDownloadUri(
+        NexusModsFileMetadata.ReadOnly file,
+        Optional<(NXMKey, DateTime)> nxmData,
+        CancellationToken cancellationToken = default)
     {
-        var links = await _apiClient.DownloadLinksAsync(file.ModPage.GameDomain.ToString(), file.ModPage.ModId, file.FileId, token: cancellationToken);
+        Response<DownloadLink[]> links;
+
+        if (nxmData.HasValue)
+        {
+            // NOTE(erri120): the key and expiration date are required for free users to be able to download anything
+            var (key, expirationDate) = nxmData.Value;
+            links = await _apiClient.DownloadLinksAsync(
+                file.ModPage.GameDomain.ToString(),
+                file.ModPage.ModId,
+                file.FileId,
+                key: key,
+                expireTime: expirationDate,
+                token: cancellationToken
+            );
+        }
+        else
+        {
+            // NOTE(erri120): premium-only API
+            links = await _apiClient.DownloadLinksAsync(
+                file.ModPage.GameDomain.ToString(),
+                file.ModPage.ModId,
+                file.FileId,
+                token: cancellationToken
+            );
+        }
 
         // NOTE(erri120): The first download link is the preferred download location as
         // set by the user in their settings. By default, this will be the CDN, which
@@ -99,7 +128,8 @@ public class NexusModsLibrary
         var modPage = await GetOrAddModPage(url.ModId, GameDomain.From(url.Game), cancellationToken);
         var file = await GetOrAddFile(url.FileId, modPage, GameDomain.From(url.Game), cancellationToken);
 
-        var uri = await GetDownloadUri(file, cancellationToken: cancellationToken);
+        var nxmData = url.Key is not null && url.ExpireTime is not null ? (url.Key.Value, url.ExpireTime.Value) : Optional.None<(NXMKey, DateTime)>();
+        var uri = await GetDownloadUri(file, nxmData, cancellationToken: cancellationToken);
 
         var worker = _serviceProvider.GetRequiredService<NexusModsDownloadJobWorker>();
 
