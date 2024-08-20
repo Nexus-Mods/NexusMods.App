92aabc824499280c8c83c39247a05f3748c2333c
halgari <tbaldridge@gmail.com>
2024-08-20 09:43:05 -0600

    Button and all that works

--- src/NexusMods.App.UI/Controls/Spine/Buttons/Download/SpineDownloadButtonDesignerViewModel.cs
+++ src/NexusMods.App.UI/Controls/Spine/Buttons/Download/SpineDownloadButtonDesignerViewModel.cs
@@ -1,6 +1,6 @@
 using System.Reactive;
 using DynamicData.Kernel;
-using NexusMods.Abstractions.Activities;
+using NexusMods.Abstractions.Jobs;
 using NexusMods.App.UI.Resources;
 using NexusMods.App.UI.WorkspaceSystem;
 using ReactiveUI;
