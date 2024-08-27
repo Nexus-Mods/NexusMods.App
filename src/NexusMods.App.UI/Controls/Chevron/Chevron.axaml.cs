0742a4a1641b221f487b3576a4dfd6cdaf0e0296
Sewer56 <sewer56lol@googlemail.com>
2024-08-27 20:13:47 +0100

    Added: Chevron to Diagnostic Items

---
+++ src/NexusMods.App.UI/Controls/Chevron/Chevron.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace NexusMods.App.UI.Controls.Chevron;
+
+public partial class Chevron : UserControl
+{
+    public Chevron() => InitializeComponent();
+}
+
