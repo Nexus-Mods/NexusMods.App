271ad5aad9a5e1a8bb69c016e8e8e464e32efdb2
AL <26797547+Al12rs@users.noreply.github.com>
2024-08-28 15:21:49 +0200

    Add Indent line to TreeDataGrid styling and fix indent padding

--- src/Themes/NexusMods.Themes.NexusFluentDark/Converters/TreeDataGridIndentToPaddingConverter.cs
+++ src/Themes/NexusMods.Themes.NexusFluentDark/Converters/TreeDataGridIndentToPaddingConverter.cs
@@ -4,18 +4,26 @@ using Avalonia.Data.Converters;
 
 namespace NexusMods.Themes.NexusFluentDark.Converters;
 
+/// <summary>
+/// Converts the indent level of a TreeDataGrid item to a padding values for the first column.
+/// </summary>
 public class TreeDataGridIndentToPaddingConverter : IValueConverter
 {
+    private const int LeftPadding = 12;
+    private const int OffsetWidth = 20;
+    private const int RightPadding = 8;
+
     public static TreeDataGridIndentToPaddingConverter Instance { get; } = new TreeDataGridIndentToPaddingConverter();
 
     public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        if (value is int indent)
-        {
-            return new Thickness(12 + 24 * indent, 0, 12, 0);
-        }
-
-        return new Thickness();
+        if (value is not int indent) return new Thickness();
+        
+        // For indent 1 and greater, view has an additional element (indent line) of OffsetWidth width,
+        // which we don't want to include in the padding, so we subtract 1 to get the correct offset.
+        var leftPadding = LeftPadding + (indent < 1 ? 0 : OffsetWidth * (indent - 1));
+        
+        return new Thickness(leftPadding, 0, RightPadding, 0);
     }
 
     public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
