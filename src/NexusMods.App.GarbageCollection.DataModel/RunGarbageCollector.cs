e831653bba94bc3c0af27f8ec5c30784bb4d3e3c
Sewer56 <sewer56lol@googlemail.com>
2024-09-19 15:08:09 +0100

    WIP Commit: Don't allow yielding to potential non-GC code from within GC.

--- src/NexusMods.App.GarbageCollection.DataModel/RunGarbageCollector.cs
+++ src/NexusMods.App.GarbageCollection.DataModel/RunGarbageCollector.cs
@@ -1,7 +1,9 @@
 using NexusMods.Abstractions.IO;
 using NexusMods.Abstractions.Settings;
 using NexusMods.App.GarbageCollection.Nx;
+using NexusMods.Hashing.xxHash64;
 using NexusMods.MnemonicDB.Abstractions;
+using NexusMods.Paths;
 namespace NexusMods.App.GarbageCollection.DataModel;
 
 /// <summary>
@@ -10,25 +12,40 @@ namespace NexusMods.App.GarbageCollection.DataModel;
 /// </summary>
 public static class RunGarbageCollector
 {
+    private static AsyncFriendlyReaderWriterLock _gcLock = new();
+    
     /// <summary/>
     /// <param name="archiveLocations">The archive locations, usually obtained from 'DataModelSettings'.</param>
     /// <param name="store">The <see cref="IFileStore"/> that requires locking for concurrent access.</param>
     /// <param name="connection">The MneumonicDB <see cref="IConnection"/> to the DataModel.</param>
     public static void Do(Span<ConfigurablePath> archiveLocations, IFileStore store, IConnection connection)
     {
-        using var lck = store.Lock();
-        var updater = new NxFileStoreUpdater(connection);
-        var gc = new ArchiveGarbageCollector<NxParsedHeaderState, FileEntryWrapper>();
-        DataStoreNxArchiveFinder.FindAllArchives(archiveLocations, gc);
-        DataStoreReferenceMarker.MarkUsedFiles(connection, gc);
-        gc.CollectGarbage(new Progress<double>(), (progress, toArchive, toRemove, archive) =>
+        using var gcLock = _gcLock.WriteLock();
+        var toUpdateInDataStore = new List<ToUpdateInDataStoreEntry>();
+
+        using (store.Lock())
         {
-            NxRepacker.RepackArchive(progress, toArchive, toRemove, archive, false, out var newArchivePath);
-            updater.UpdateNxFileStore(toRemove, archive.FilePath, newArchivePath);
+            var gc = new ArchiveGarbageCollector<NxParsedHeaderState, FileEntryWrapper>();
+            DataStoreNxArchiveFinder.FindAllArchives(archiveLocations, gc);
+            DataStoreReferenceMarker.MarkUsedFiles(connection, gc);
+            gc.CollectGarbage(new Progress<double>(), (progress, toArchive, toRemove, archive) =>
+            {
+                NxRepacker.RepackArchive(progress, toArchive, toRemove, archive, false, out var newArchivePath);
+                toUpdateInDataStore.Add(new ToUpdateInDataStoreEntry(toRemove, archive.FilePath, newArchivePath));
+            });
+        }
+
+        var updater = new NxFileStoreUpdater(connection);
+        foreach (var entry in toUpdateInDataStore)
+        {
+            updater.UpdateNxFileStore(entry.ToRemove, entry.OldFilePath, entry.NewFilePath);
             // Delete original archive. We do this in a delayed fashion such that
             // a power loss during the UpdateNxFileStore operation does not lead
             // to an inconsistent state
-            archive.FilePath.Delete();
-        });
+            entry.OldFilePath.Delete();
+        }
+        
     }
+
+    private record struct ToUpdateInDataStoreEntry(List<Hash> ToRemove, AbsolutePath OldFilePath, AbsolutePath NewFilePath);
 }
