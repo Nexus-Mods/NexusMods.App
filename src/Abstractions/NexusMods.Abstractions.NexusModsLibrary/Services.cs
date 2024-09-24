b39ce666458b3432cee8346578c2b48be9b9258d
halgari <tbaldridge@gmail.com>
2024-09-24 15:48:45 -0600

    Invert the way we query GQL to return less revisions and more revision data

--- src/Abstractions/NexusMods.Abstractions.NexusModsLibrary/Services.cs
+++ src/Abstractions/NexusMods.Abstractions.NexusModsLibrary/Services.cs
@@ -21,6 +21,7 @@ public static class Services
             .AddNexusModsLibraryFileModel()
             .AddCollectionMetadataModel()
             .AddCollectionRevisionMetadataModel()
+            .AddCollectionRevisionModFileModel()
             .AddCollectionTagModel()
             .AddUserModel()
             .AddNexusModsCollectionLibraryFileModel();
