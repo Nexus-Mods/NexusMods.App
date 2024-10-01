3f54862842e45b694787fd36c07ec46f3db1ade3
erri120 <erri120@fossmailer.de>
2024-10-01 18:41:55 +0200

    Add ThenDo

--- src/Abstractions/NexusMods.Abstractions.Resources/Extensions.cs
+++ src/Abstractions/NexusMods.Abstractions.Resources/Extensions.cs
@@ -36,4 +36,26 @@ public static partial class Extensions
     {
         return factory(state, innerLoader);
     }
+
+    public static IResourceLoader<TResourceIdentifier, TData> ThenDo<TResourceIdentifier, TData, TInnerData, TState>(
+        this IResourceLoader<TResourceIdentifier, TInnerData> innerLoader,
+        TState state,
+        Func<TState, TResourceIdentifier, Resource<TInnerData>, CancellationToken, ValueTask<Resource<TData>>> func)
+        where TResourceIdentifier : notnull
+        where TData : notnull
+        where TInnerData : notnull
+    {
+        return ResourceLoader.Create<TResourceIdentifier, TData, ValueTuple<
+            IResourceLoader<TResourceIdentifier, TInnerData>,
+            TState,
+            Func<TState, TResourceIdentifier, Resource<TInnerData>, CancellationToken, ValueTask<Resource<TData>>>
+        >>
+        ((innerLoader, state, func), static async (outerState, resourceIdentifier, cancellationToken) =>
+        {
+            var (innerLoader, innerState, func) = outerState;
+
+            var resource = await innerLoader.LoadResourceAsync(resourceIdentifier, cancellationToken);
+            return await func(innerState, resourceIdentifier, resource, cancellationToken);
+        });
+    }
 }
