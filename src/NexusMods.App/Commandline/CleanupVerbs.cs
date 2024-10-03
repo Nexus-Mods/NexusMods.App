cdf509ec422d2129fcdbc834272d80726a87855a
Sewer56 <admin@sewer56.dev>
2024-10-03 10:01:08 +0100

    Removed: Old Hack for Legacy Race Condition

--- src/NexusMods.App/Commandline/CleanupVerbs.cs
+++ src/NexusMods.App/Commandline/CleanupVerbs.cs
@@ -36,10 +36,6 @@ internal static class CleanupVerbs
         [Injected] ISettingsManager settingsManager,
         [Injected] IFileSystem fileSystem)
     {
-        // Prevent a race condition where `IRepository` is not fully done initializing.
-        // https://github.com/Nexus-Mods/NexusMods.App/issues/1396
-        Thread.Sleep(1000);
-        
         // Step 1: Revert the managed games to their original state
         var db = conn.Db;
         var managedInstallations = Loadout.All(db)
