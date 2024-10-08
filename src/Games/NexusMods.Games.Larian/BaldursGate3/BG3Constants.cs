20b94e109fa840e0055c537b39de4938d86aab45
AL <26797547+Al12rs@users.noreply.github.com>
2024-10-08 15:10:17 +0200

    Cleanup constants

--- src/Games/NexusMods.Games.Larian/BaldursGate3/BG3Constants.cs
+++ src/Games/NexusMods.Games.Larian/BaldursGate3/BG3Constants.cs
@@ -5,7 +5,7 @@ namespace NexusMods.Games.Larian.BaldursGate3;
 
 public static class Bg3Constants
 {
-    public static readonly Extension PakFileExtension = new Extension(".pak");
+    public static readonly Extension PakFileExtension = new(".pak");
     
     public static readonly LocationId ModsLocationId = LocationId.From("Mods");
 }
