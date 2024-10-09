1b20e4f2286ece8e45d3b946344d6637e4ce279a
insomnious <simon.davies@nexusmods.com>
2024-10-09 10:44:18 +0100

    renamed component and moved to using property style selectors

--- src/NexusMods.App.UI/Controls/StandardButton.axaml.cs
+++ src/NexusMods.App.UI/Controls/StandardButton.axaml.cs
@@ -8,11 +8,10 @@ using NexusMods.Icons;
 
 namespace NexusMods.App.UI.Controls;
 
-[PseudoClasses(":size-medium", ":size-small", ":fill-none", ":fill-strong", ":fill-weak", ":type-primary", ":type-secondary", ":type-tertiary")]
 [TemplatePart("PART_LeftIcon",  typeof(UnifiedIcon))]
 [TemplatePart("PART_RightIcon", typeof(UnifiedIcon))]
 [TemplatePart("PART_Label", typeof(TextBlock))]
-public class CustomButton : Button
+public class StandardButton : Button
 {
     //protected override Type StyleKeyOverride { get; } = typeof(Button);
 
@@ -27,15 +26,15 @@ public class CustomButton : Button
     private ContentPresenter? _content = null;
     private Border? _border = null;
     
-    public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<CustomButton, string?>(nameof(Text), defaultValue: "Custom Button");
-    public static readonly StyledProperty<IconValue?> LeftIconProperty = AvaloniaProperty.Register<CustomButton, IconValue?>(nameof(LeftIcon), defaultValue: IconValues.ChevronDown);
-    public static readonly StyledProperty<IconValue?> RightIconProperty = AvaloniaProperty.Register<CustomButton, IconValue?>(nameof(RightIcon), defaultValue: IconValues.ChevronUp);
+    public static readonly StyledProperty<string?> TextProperty = AvaloniaProperty.Register<StandardButton, string?>(nameof(Text), defaultValue: "Standard Button");
+    public static readonly StyledProperty<IconValue?> LeftIconProperty = AvaloniaProperty.Register<StandardButton, IconValue?>(nameof(LeftIcon), defaultValue: IconValues.ChevronDown);
+    public static readonly StyledProperty<IconValue?> RightIconProperty = AvaloniaProperty.Register<StandardButton, IconValue?>(nameof(RightIcon), defaultValue: IconValues.ChevronUp);
     
-    public static readonly AttachedProperty<VisibleIcons> VisibleIconProperty = AvaloniaProperty.RegisterAttached<CustomButton, TemplatedControl, VisibleIcons>("VisibleIcon", defaultValue: VisibleIcons.None);
-    public static readonly AttachedProperty<Types> TypeProperty = AvaloniaProperty.RegisterAttached<CustomButton, TemplatedControl, Types>("Type", defaultValue: Types.None);
-    public static readonly AttachedProperty<Sizes> SizeProperty = AvaloniaProperty.RegisterAttached<CustomButton, TemplatedControl, Sizes>("Size", defaultValue: Sizes.Medium);
-    public static readonly AttachedProperty<Fills> FillProperty = AvaloniaProperty.RegisterAttached<CustomButton, TemplatedControl, Fills>("Fill", defaultValue: Fills.None);
-    public static readonly AttachedProperty<bool> ShowLabelProperty = AvaloniaProperty.RegisterAttached<CustomButton, TemplatedControl, bool>("ShowLabel", defaultValue: true);
+    public static readonly AttachedProperty<VisibleIcons> VisibleIconProperty = AvaloniaProperty.RegisterAttached<StandardButton, TemplatedControl, VisibleIcons>("VisibleIcon", defaultValue: VisibleIcons.None);
+    public static readonly AttachedProperty<Types> TypeProperty = AvaloniaProperty.RegisterAttached<StandardButton, TemplatedControl, Types>("Type", defaultValue: Types.None);
+    public static readonly AttachedProperty<Sizes> SizeProperty = AvaloniaProperty.RegisterAttached<StandardButton, TemplatedControl, Sizes>("Size", defaultValue: Sizes.Medium);
+    public static readonly AttachedProperty<Fills> FillProperty = AvaloniaProperty.RegisterAttached<StandardButton, TemplatedControl, Fills>("Fill", defaultValue: Fills.None);
+    public static readonly AttachedProperty<bool> ShowLabelProperty = AvaloniaProperty.RegisterAttached<StandardButton, TemplatedControl, bool>("ShowLabel", defaultValue: true);
     
     public string? Text
     {
@@ -83,13 +82,6 @@ public class CustomButton : Button
     {
         get => GetValue(FillProperty);
         set => SetValue(FillProperty, value);
-    }
-
-    protected override void OnClick()
-    {
-        base.OnClick();
-        
-        Console.WriteLine("Button Clicked");
     }
 
     /// <inheritdoc/>
@@ -143,61 +135,6 @@ public class CustomButton : Button
             default:
                 _leftIcon!.IsVisible = false;
                 _rightIcon!.IsVisible = false;
-                break;
-        }
-        
-        switch (Size)
-        {
-            case Sizes.Medium:
-                PseudoClasses.Add(":size-medium");
-                PseudoClasses.Remove(":size-small");
-                break;
-            case Sizes.Small:
-                PseudoClasses.Remove(":size-medium");
-                PseudoClasses.Add(":size-small");
-                break;
-        }
-        
-        switch (Fill)
-        {
-            case Fills.None:
-                PseudoClasses.Add(":fill-none");
-                PseudoClasses.Remove(":fill-strong");
-                PseudoClasses.Remove(":fill-weak");
-                break;
-            case Fills.Strong:
-                PseudoClasses.Remove(":fill-none");
-                PseudoClasses.Add(":fill-strong");
-                PseudoClasses.Remove(":fill-weak");
-                break;
-            case Fills.Weak:
-                PseudoClasses.Remove(":fill-none");
-                PseudoClasses.Remove(":fill-strong");
-                PseudoClasses.Add(":fill-weak");
-                break;
-        }
-
-        switch (Type) 
-        {
-            case Types.None:
-                PseudoClasses.Remove(":type-primary");
-                PseudoClasses.Remove(":type-secondary");
-                PseudoClasses.Remove(":type-tertiary");
-                break;
-            case Types.Primary:
-                PseudoClasses.Add(":type-primary");
-                PseudoClasses.Remove(":type-secondary");
-                PseudoClasses.Remove(":type-tertiary");
-                break;
-            case Types.Secondary:
-                PseudoClasses.Remove(":type-primary");
-                PseudoClasses.Add(":type-secondary");
-                PseudoClasses.Remove(":type-tertiary");
-                break;
-            case Types.Tertiary:
-                PseudoClasses.Remove(":type-primary");
-                PseudoClasses.Remove(":type-secondary");
-                PseudoClasses.Add(":type-tertiary");
                 break;
         }
     }
