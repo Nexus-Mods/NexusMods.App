558a13577e1ea931c6a3c24d9a193200ba488360
Timothy Baldridge <tbaldridge@gmail.com>
2024-10-10 06:54:05 -0600

    Switch LocationId (and GamePaths) over to an ushort instead of a string (#2153)

--- src/Abstractions/NexusMods.Abstractions.MnemonicDB.Attributes/GamePathParentAttribute.cs
+++ src/Abstractions/NexusMods.Abstractions.MnemonicDB.Attributes/GamePathParentAttribute.cs
@@ -10,16 +10,16 @@ namespace NexusMods.Abstractions.MnemonicDB.Attributes;
 /// An attribute that combines an EntityId, LocationId and RelativePath into a single attribute. This is used to represent GamePaths prefixed
 /// with a parent entity so that range queries only return the paths that are children of the parent entity.
 /// </summary>
-public class GamePathParentAttribute(string ns, string name) : TupleAttribute<EntityId, ulong, LocationId, string, RelativePath, string>(ValueTags.Reference, ValueTags.Ascii, ValueTags.Utf8, ns, name) 
+public class GamePathParentAttribute(string ns, string name) : TupleAttribute<EntityId, ulong, LocationId, ushort, RelativePath, string>(ValueTags.Reference, ValueTags.UInt16, ValueTags.Utf8, ns, name) 
 {
     /// <inheritdoc />
-    protected override (EntityId, LocationId, RelativePath) FromLowLevel((ulong, string, string) value)
+    protected override (EntityId, LocationId, RelativePath) FromLowLevel((ulong, ushort, string) value)
     {
         return (EntityId.From(value.Item1), LocationId.From(value.Item2), RelativePath.FromUnsanitizedInput(value.Item3));
     }
 
     /// <inheritdoc />
-    protected override (ulong, string, string) ToLowLevel((EntityId, LocationId, RelativePath) value)
+    protected override (ulong, ushort, string) ToLowLevel((EntityId, LocationId, RelativePath) value)
     {
         return (value.Item1.Value, value.Item2.Value, value.Item3);
     }
