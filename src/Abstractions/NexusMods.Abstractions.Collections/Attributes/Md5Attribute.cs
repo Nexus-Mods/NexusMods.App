037476bf6540580f3d6cc74af6cda431966ae566
halgari <tbaldridge@gmail.com>
2024-10-24 15:25:13 +0100

    Fix build failures

--- src/Abstractions/NexusMods.Abstractions.Collections/Attributes/Md5Attribute.cs
+++ src/Abstractions/NexusMods.Abstractions.Collections/Attributes/Md5Attribute.cs
@@ -2,23 +2,19 @@ using NexusMods.Abstractions.Collections.Types;
 using NexusMods.MnemonicDB.Abstractions;
 using NexusMods.MnemonicDB.Abstractions.Attributes;
 using NexusMods.MnemonicDB.Abstractions.ElementComparers;
+using NexusMods.MnemonicDB.Abstractions.ValueSerializers;
 
 namespace NexusMods.Abstractions.Collections.Attributes;
 
 /// <summary>
 /// An attribute representing an MD5 hash value.
 /// </summary>
-public class Md5Attribute(string ns, string name) : ScalarAttribute<Md5HashValue, UInt128>(ValueTag.UInt128, ns, name)
+public class Md5Attribute(string ns, string name) : ScalarAttribute<Md5HashValue, UInt128, UInt128Serializer>(ns, name)
 {
     /// <inheritdoc />
-    protected override UInt128 ToLowLevel(Md5HashValue value)
-    {
-        return value.Value;
-    }
+    protected override UInt128 ToLowLevel(Md5HashValue value) => value.Value;
 
     /// <inheritdoc />
-    protected override Md5HashValue FromLowLevel(UInt128 value, AttributeResolver resolver)
-    {
-        return Md5HashValue.From(value);
-    }
+    protected override Md5HashValue FromLowLevel(UInt128 value, AttributeResolver resolver) 
+        => Md5HashValue.From(value);
 }
