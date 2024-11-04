96168119f31cd6a29679214a58451780bce15b63
halgari <tbaldridge@gmail.com>
2024-11-04 10:44:34 -0700

    Tests and start of the migration failsafes

---
+++ src/NexusMods.DataModel.Migrations/SchemaFingerprint.cs
@@ -0,0 +1,32 @@
+using System.IO.Hashing;
+using System.Text;
+using NexusMods.Hashing.xxHash3;
+using NexusMods.MnemonicDB.Abstractions;
+
+namespace NexusMods.DataModel.Migrations;
+
+/// <summary>
+/// Tools for generating a hash of all the attributes of a schema so that we can detect changes.
+/// </summary>
+public class SchemaFingerprint
+{
+    public static Hash GenerateFingerprint(IDb db)
+    {
+        StringBuilder sb = new();
+        var cache = db.AttributeCache;
+        
+        foreach (var id in cache.AllAttributeIds.OrderBy(id => id.Id))
+        {
+            var aid = cache.GetAttributeId(id);
+            sb.AppendLine(id.ToString());
+            sb.AppendLine(cache.GetValueTag(aid).ToString());
+            sb.AppendLine(cache.IsIndexed(aid).ToString());
+            sb.AppendLine(cache.IsReference(aid).ToString());
+            sb.AppendLine(cache.IsCardinalityMany(aid).ToString());
+            sb.AppendLine(cache.IsNoHistory(aid).ToString());
+            sb.AppendLine("--");
+        }
+        return sb.ToString().xxHash3AsUtf8();
+    }
+    
+}
