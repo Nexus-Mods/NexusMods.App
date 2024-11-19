b0393f8db13dc705a7c4b4d8e94eb045152b8322
erri120 <erri120@fossmailer.de>
2024-11-19 14:13:11 +0100

    Update download page

--- src/NexusMods.App.UI/Pages/CollectionDownload/CollectionDownloadView.axaml.cs
+++ src/NexusMods.App.UI/Pages/CollectionDownload/CollectionDownloadView.axaml.cs
@@ -14,60 +14,76 @@ public partial class CollectionDownloadView : ReactiveUserControl<ICollectionDow
     {
         InitializeComponent();
 
-        TreeDataGridViewHelper.SetupTreeDataGridAdapter<CollectionDownloadView, ICollectionDownloadViewModel, ILibraryItemModel, EntityId>(this, RequiredModsTree, vm => vm.TreeDataGridAdapter);
+        TreeDataGridViewHelper.SetupTreeDataGridAdapter<CollectionDownloadView, ICollectionDownloadViewModel, ILibraryItemModel, EntityId>(this, RequiredDownloadsTree, vm => vm.RequiredDownloadsAdapter);
+        TreeDataGridViewHelper.SetupTreeDataGridAdapter<CollectionDownloadView, ICollectionDownloadViewModel, ILibraryItemModel, EntityId>(this, OptionalDownloadsTree, vm => vm.OptionalDownloadsAdapter);
 
         this.WhenActivated(d =>
-            {
-                this.OneWayBind(ViewModel, vm => vm.TreeDataGridAdapter.Source.Value, view => view.RequiredModsTree.Source)
-                    .DisposeWith(d);
+        {
+            this.OneWayBind(ViewModel, vm => vm.RequiredDownloadsAdapter.Source.Value, view => view.RequiredDownloadsTree.Source)
+                .DisposeWith(d);
 
-                 this.WhenAnyValue(view => view.ViewModel!.BackgroundImage)
-                     .WhereNotNull()
-                     .SubscribeWithErrorLogging(image => HeaderBorderBackground.Background = new ImageBrush { Source = image, Stretch = Stretch.UniformToFill, AlignmentY = AlignmentY.Top})
-                     .DisposeWith(d);
-                
-                this.WhenAnyValue(view => view.ViewModel!.TileImage)
-                    .WhereNotNull()
-                    .SubscribeWithErrorLogging(image => CollectionImage.Source = image)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.Name, view => view.Heading.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.AuthorName, view => view.AuthorName.Text)
-                    .DisposeWith(d);
+            this.OneWayBind(ViewModel, vm => vm.OptionalDownloadsAdapter.Source.Value, view => view.OptionalDownloadsTree.Source)
+                .DisposeWith(d);
 
-                this.OneWayBind(ViewModel, vm => vm.Summary, view => view.Summary.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.ModCount, view => view.ModCount.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.EndorsementCount, view => view.Endorsements.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.DownloadCount, view => view.Downloads.Text)
-                    .DisposeWith(d);
+             this.WhenAnyValue(view => view.ViewModel!.BackgroundImage)
+                 .WhereNotNull()
+                 .SubscribeWithErrorLogging(image => HeaderBorderBackground.Background = new ImageBrush { Source = image, Stretch = Stretch.UniformToFill, AlignmentY = AlignmentY.Top})
+                 .DisposeWith(d);
 
-                this.OneWayBind(ViewModel, vm => vm.TotalSize, view => view.TotalSize.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.OverallRating, view => view.OverallRating.Text)
-                    .DisposeWith(d);
-                
-                this.OneWayBind(ViewModel, vm => vm.RequiredModCount, view => view.RequiredModsCount.Text)
-                    .DisposeWith(d);
+            this.WhenAnyValue(view => view.ViewModel!.TileImage)
+                .WhereNotNull()
+                .SubscribeWithErrorLogging(image => CollectionImage.Source = image)
+                .DisposeWith(d);
 
-                this.OneWayBind(ViewModel, vm => vm.OptionalModCount, view => view.OptionalModsCount.Text)
-                    .DisposeWith(d);
+            this.OneWayBind(ViewModel, vm => vm.Name, view => view.Heading.Text)
+                .DisposeWith(d);
 
-                this.OneWayBind(ViewModel, vm => vm.CollectionStatusText, view => view.CollectionStatusText.Text)
-                    .DisposeWith(d);
+            this.OneWayBind(ViewModel, vm => vm.AuthorName, view => view.AuthorName.Text)
+                .DisposeWith(d);
 
-            }
-        );
+            this.OneWayBind(ViewModel, vm => vm.AuthorAvatar, view => view.AuthorAvatar.Source)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.Summary, view => view.Summary.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.DownloadCount, view => view.NumDownloads.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.EndorsementCount, view => view.Endorsements.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.TotalDownloads, view => view.TotalDownloads.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.TotalSize, view => view.TotalSize.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.OverallRating, view => view.OverallRating.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.RequiredDownloadsCount, view => view.RequiredDownloadsCount.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.OptionalDownloadsCount, view => view.OptionalDownloadsCount.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.CollectionStatusText, view => view.CollectionStatusText.Text)
+                .DisposeWith(d);
+
+            this.OneWayBind(ViewModel, vm => vm.RevisionNumber, view => view.Revision.Text)
+                .DisposeWith(d);
+
+            this.WhenAnyValue(view => view.ViewModel)
+                .WhereNotNull()
+                .Subscribe(vm =>
+                {
+                    if (vm.OptionalDownloadsCount == 0)
+                    {
+                        TabControl.IsVisible = false;
+                    }
+                }).DisposeWith(d);
+        });
     }
-    
-    
 }
 
