6bb4bfc93c6445b5df429e31f4570f4c6c9cf953
Timothy Baldridge <tbaldridge@gmail.com>
2024-12-05 13:06:22 -0700

    Rename the app from `NexusMods` to `Nexus Mods` on macOS

--- scripts/build_macos_bundle.sh
+++ scripts/build_macos_bundle.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p ./bin/macOS
-cp -r ./src/NexusMods.App/dist/macOS/NexusMods.app ./bin/macOS
-dotnet publish src/NexusMods.App/NexusMods.App.csproj -c Release -r osx-arm64 -o bin/macOS/NexusMods.app/Contents/MacOS
+cp -r "./src/NexusMods.App/dist/macOS/Nexus Mods.app" ./bin/macOS
+dotnet publish "src/NexusMods.App/NexusMods.App.csproj" -c Release -r osx-arm64 -o "bin/macOS/Nexus Mods.app/Contents/MacOS"
