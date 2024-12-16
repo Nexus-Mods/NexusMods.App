752486b2103d3160025609e60ec9bb6bd154626d
Timothy Baldridge <tbaldridge@gmail.com>
2024-12-16 06:34:00 -0700

    Update src/Networking/NexusMods.Networking.Steam/Session.cs
    Co-authored-by: erri120 <erri120@fossmailer.de>

--- src/Networking/NexusMods.Networking.Steam/Session.cs
+++ src/Networking/NexusMods.Networking.Steam/Session.cs
@@ -168,11 +168,14 @@ public class Session : ISteamSession
     public async Task<ProductInfo> GetProductInfoAsync(AppId appId, CancellationToken cancellationToken = default)
     {
         await ConnectedAsync(cancellationToken);
-        var jobs = await _steamApps.PICSGetProductInfo(new SteamApps.PICSRequest(appId.Value), null);
-        if (jobs.Failed)
-            throw new Exception("Failed to get product info for app " + appId.Value);
 
-        return ProductInfoParser.Parse(jobs.Results![0]);
+        var jobs = await _steamApps.PICSGetProductInfo(new SteamApps.PICSRequest(appId.Value), null);
+        if (jobs.Failed) throw new Exception($"Failed to get product info for app `{appId}`");
+
+        var results = jobs.Results;
+        if (results is null || results.Count == 0) throw new Exception($"Found no product info for app `{appId}`");
+
+        return ProductInfoParser.Parse(results[0]);
     }
 
     /// <summary>
