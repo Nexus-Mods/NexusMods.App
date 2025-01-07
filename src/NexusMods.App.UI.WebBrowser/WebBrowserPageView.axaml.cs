f4959449473b43f03c68e37a5f1bc7363a74928f
halgari <tbaldridge@gmail.com>
2025-01-07 10:54:40 -0700

    Switch around a few config things, just getting unblocked for now

--- src/NexusMods.App.UI.WebBrowser/WebBrowserPageView.axaml.cs
+++ src/NexusMods.App.UI.WebBrowser/WebBrowserPageView.axaml.cs
@@ -1,33 +1,41 @@
-using System.Diagnostics;
 using System.Reactive.Disposables;
-using System.Reactive.Linq;
-using Avalonia.Controls;
 using Avalonia.Interactivity;
 using Avalonia.ReactiveUI;
+using DotNetBrowser.Browser;
 using ReactiveUI;
-using WebViewControl;
 
 namespace NexusMods.App.UI.WebBrowser;
 
 public partial class WebBrowserPageView : ReactiveUserControl<IWebBrowserPageViewModel>
 {
+    private readonly IBrowser _browser;
+
     public WebBrowserPageView()
     {
+        _browser = WebEngineProvider.GetBrowser();
+        _browser.Navigation.LoadUrl("https://www.nexusmods.com");
+        
         InitializeComponent();
+
+
+        BrowserControl.InitializeFrom(_browser);
+
+        
         this.WhenActivated(d =>
             {
                 this.WhenAnyValue(v => v.ViewModel!.Address)
                     .Subscribe(uri =>
-                        {
-                            BrowserControl.Address = uri.ToString();
-                        })
-                    .DisposeWith(d);
-                
-                this.WhenAnyValue(v => v.BrowserControl.Address)
-                    .Subscribe(uri => ViewModel!.NavigatedTo(new Uri(uri)))
+                    {
+                        _browser.Navigation.LoadUrl(uri.ToString());
+                    })
                     .DisposeWith(d);
             }
         );
+        
+        _browser.Navigation.NavigationStarted += (_, args) =>
+        {
+            ViewModel!.NavigatedTo(new Uri(args.Url));
+        };
     }
 
     private void DoIt_OnClick(object? sender, RoutedEventArgs e)
