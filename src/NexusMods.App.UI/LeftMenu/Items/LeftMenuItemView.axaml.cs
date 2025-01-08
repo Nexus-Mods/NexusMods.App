9749c970f105602b10bc4a0c9e9ad40576a43458
AL <26797547+Al12rs@users.noreply.github.com>
2025-01-08 15:24:48 +0100

    Add new LeftMenuItemView

---
+++ src/NexusMods.App.UI/LeftMenu/Items/LeftMenuItemView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.ReactiveUI;
+
+namespace NexusMods.App.UI.LeftMenu.Items;
+
+public partial class LeftMenuItemView : ReactiveUserControl<ILeftMenuItemViewModel>
+{
+    public LeftMenuItemView()
+    {
+        InitializeComponent();
+    }
+}
+
