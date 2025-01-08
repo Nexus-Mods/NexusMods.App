a2fc31b45d8a04561c76a9f2f2ee7fcef5016d9e
halgari <tbaldridge@gmail.com>
2025-01-08 13:15:43 -0700

    Fix up the CLI on windows, add module support to the CLI

--- src/NexusMods.ProxyConsole.Abstractions/VerbDefinitions/VerbAttribute.cs
+++ src/NexusMods.ProxyConsole.Abstractions/VerbDefinitions/VerbAttribute.cs
@@ -5,18 +5,27 @@ namespace NexusMods.ProxyConsole.Abstractions.VerbDefinitions;
 /// <summary>
 /// Defines a method that should be exposed as a verb.
 /// </summary>
-/// <param name="name"></param>
-/// <param name="description"></param>
 [AttributeUsage(AttributeTargets.Method)]
-public class VerbAttribute(string name, string description) : Attribute
+public class VerbAttribute : Attribute
 {
+    /// <summary>
+    /// Defines a method that should be exposed as a verb.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="description"></param>
+    public VerbAttribute(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+    
     /// <summary>
     /// The name of the verb.
     /// </summary>
-    public string Name { get; } = name;
+    public string Name { get; }
 
     /// <summary>
     /// Help text for the verb.
     /// </summary>
-    public string Description { get; } = description;
+    public string Description { get; }
 }
