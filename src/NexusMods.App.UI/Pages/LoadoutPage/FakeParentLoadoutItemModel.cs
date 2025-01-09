Nexus-Mods/NexusMods.App

commit 16e9ccc8305c788f23fbba50ac39148ae5e6e844

Moved: Thumbnail Acquisition to FakeParentLoadoutItemModel
Dispose: ChildrenObservable with FakeParentLoadoutItemModel

src/NexusMods.App.UI/Pages/LoadoutPage/FakeParentLoadoutItemModel.cs
@@ -1,8 +1,9 @@
+using System.Reactive.Linq;
 using DynamicData;
 using Microsoft.Extensions.DependencyInjection;
 using NexusMods.Abstractions.Loadouts;
+using NexusMods.Abstractions.NexusModsLibrary;
 using NexusMods.Abstractions.UI.Extensions;
-using NexusMods.App.UI.Extensions;
 using NexusMods.MnemonicDB.Abstractions;
 using ObservableCollections;
 using R3;
@@ -20,13 +21,37 @@ public class FakeParentLoadoutItemModel : LoadoutItemModel
 
     private readonly IDisposable _modelActivationDisposable;
     private readonly IDisposable _loadoutItemIdsDisposable;
+    private readonly IDisposable _childrenObservableDisposable;
 
-    public FakeParentLoadoutItemModel(IObservable<IChangeSet<LoadoutItemId, EntityId>> loadoutItemIdsObservable, IServiceProvider provider) : base(default(LoadoutItemId), provider, provider.GetRequiredService<IConnection>(), true)
+    public FakeParentLoadoutItemModel(IObservable<IChangeSet<LoadoutItemId, EntityId>> loadoutItemIdsObservable, IServiceProvider provider, IConnection connection) : base(default(LoadoutItemId), provider, provider.GetRequiredService<IConnection>(), true)
     {
         LoadoutItemIdsObservable = loadoutItemIdsObservable;
         _loadoutItemIdsDisposable = LoadoutItemIdsObservable.OnUI().SubscribeWithErrorLogging(changeSet => LoadoutItemIds.ApplyChanges(changeSet));
         
-        
+        // Inherit the icon from the first child
+        _childrenObservableDisposable = ChildrenObservable.FirstAsync().Subscribe(set =>
+        {
+            foreach (var item in set)
+            {
+                if (item.Reason != ChangeReason.Add) continue;
+                        
+                // Note(sewer):
+                // The child may not be activated, so we can't just copy the thumbnail, from it as it may
+                // not have been loaded yet. We need to manually load it.
+                var current = item.Current;
+                var modPageThumbnailPipeline = ImagePipelines.GetModPageThumbnailPipeline(provider);
+                var libraryLinkedItem = LibraryLinkedLoadoutItem.Load(connection.Db, current.GetLoadoutItemIds().First());
+                if (libraryLinkedItem.IsValid() && libraryLinkedItem.LibraryItem.TryGetAsNexusModsLibraryItem(out var nexusLibraryItem))
+                {
+                    ImagePipelines.CreateObservable(nexusLibraryItem.ModPageMetadataId, modPageThumbnailPipeline)
+                        .Take(1)
+                        .ObserveOnUIThreadDispatcher()
+                        .Subscribe(this, (bitmap, _) => Thumbnail.Value = bitmap);
+                }
+                        
+                return;
+            }
+        });
         
         _modelActivationDisposable = WhenModelActivated(this, static (model, disposables) =>
         {
@@ -41,7 +66,7 @@ public class FakeParentLoadoutItemModel : LoadoutItemModel
         {
             if (disposing)
             {
-                Disposable.Dispose(_modelActivationDisposable, _loadoutItemIdsDisposable);
+                Disposable.Dispose(_modelActivationDisposable, _loadoutItemIdsDisposable, _childrenObservableDisposable);
             }
 
             LoadoutItemIds = null!;

src/NexusMods.App.UI/Pages/LocalFileDataProvider.cs
@@ -155,7 +155,7 @@ internal class LocalFileDataProvider : ILibraryDataProvider, ILoadoutDataProvide
                         return isEnabled.HasValue ? isEnabled.Value : null;
                     }).DistinctUntilChanged(x => x is null ? -1 : x.Value ? 1 : 0);
 
-                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider)
+                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection)
                 {
                     NameObservable = Observable.Return(libraryFile.AsLibraryItem().Name),
                     InstalledAtObservable = installedAtObservable,

src/NexusMods.App.UI/Pages/NexusModsDataProvider.cs
@@ -364,7 +364,7 @@ public class NexusModsDataProvider : ILibraryDataProvider, ILoadoutDataProvider
                         return isEnabled.HasValue ? isEnabled.Value : null;
                     }).DistinctUntilChanged(x => x is null ? -1 : x.Value ? 1 : 0);
 
-                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider)
+                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection)
                 {
                     NameObservable = System.Reactive.Linq.Observable.Return(modPage.Name),
                     InstalledAtObservable = installedAtObservable,
@@ -373,31 +373,6 @@ public class NexusModsDataProvider : ILibraryDataProvider, ILoadoutDataProvider
                     HasChildrenObservable = hasChildrenObservable,
                     ChildrenObservable = childrenObservable,
                 };
-
-                // Inherit the icon from the first child
-                model.ChildrenObservable.FirstAsync().Subscribe(set =>
-                {
-                    foreach (var item in set)
-                    {
-                        if (item.Reason != ChangeReason.Add) continue;
-                        
-                        // Note(sewer):
-                        // The child may not be activated, so we can't just copy the thumbnail, from it as it may
-                        // not have been loaded yet. We need to manually load it.
-                        var current = item.Current;
-                        var modPageThumbnailPipeline = ImagePipelines.GetModPageThumbnailPipeline(_serviceProvider);
-                        var libraryLinkedItem = LibraryLinkedLoadoutItem.Load(_connection.Db, current.GetLoadoutItemIds().First());
-                        if (libraryLinkedItem.IsValid() && libraryLinkedItem.LibraryItem.TryGetAsNexusModsLibraryItem(out var nexusLibraryItem))
-                        {
-                            ImagePipelines.CreateObservable(nexusLibraryItem.ModPageMetadataId, modPageThumbnailPipeline)
-                                .Take(1)
-                                .ObserveOnUIThreadDispatcher()
-                                .Subscribe(this, (bitmap, _) => model.Thumbnail.Value = bitmap);
-                        }
-                        
-                        return;
-                    }
-                });
 
                 return model;
             });

commit 94b996b774b12aca8f3f129881bf520b09128881

Fixed: Accidental regression in 16e9ccc8305c788f23fbba50ac39148ae5e6e844 due to unready childrenObservable

src/NexusMods.App.UI/Pages/LoadoutPage/FakeParentLoadoutItemModel.cs
@@ -7,6 +7,7 @@ using NexusMods.Abstractions.UI.Extensions;
 using NexusMods.MnemonicDB.Abstractions;
 using ObservableCollections;
 using R3;
+using Observable = System.Reactive.Linq.Observable;
 
 namespace NexusMods.App.UI.Pages.LoadoutPage;
 
@@ -23,9 +24,14 @@ public class FakeParentLoadoutItemModel : LoadoutItemModel
     private readonly IDisposable _loadoutItemIdsDisposable;
     private readonly IDisposable _childrenObservableDisposable;
 
-    public FakeParentLoadoutItemModel(IObservable<IChangeSet<LoadoutItemId, EntityId>> loadoutItemIdsObservable, IServiceProvider provider, IConnection connection) : base(default(LoadoutItemId), provider, provider.GetRequiredService<IConnection>(), true)
+    public FakeParentLoadoutItemModel(IObservable<IChangeSet<LoadoutItemId, EntityId>> loadoutItemIdsObservable, 
+        IServiceProvider provider, IConnection connection, IObservable<bool> hasChildrenObservable, 
+        IObservable<IChangeSet<LoadoutItemModel, EntityId>> childrenObservable) 
+        : base(default(LoadoutItemId), provider, provider.GetRequiredService<IConnection>(), true)
     {
         LoadoutItemIdsObservable = loadoutItemIdsObservable;
+        ChildrenObservable = childrenObservable;
+        HasChildrenObservable = hasChildrenObservable;
         _loadoutItemIdsDisposable = LoadoutItemIdsObservable.OnUI().SubscribeWithErrorLogging(changeSet => LoadoutItemIds.ApplyChanges(changeSet));
         
         // Inherit the icon from the first child

src/NexusMods.App.UI/Pages/LocalFileDataProvider.cs
@@ -155,14 +155,11 @@ internal class LocalFileDataProvider : ILibraryDataProvider, ILoadoutDataProvide
                         return isEnabled.HasValue ? isEnabled.Value : null;
                     }).DistinctUntilChanged(x => x is null ? -1 : x.Value ? 1 : 0);
 
-                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection)
+                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection, Observable.Return(true), childrenObservable)
                 {
                     NameObservable = Observable.Return(libraryFile.AsLibraryItem().Name),
                     InstalledAtObservable = installedAtObservable,
                     IsEnabledObservable = isEnabledObservable,
-
-                    HasChildrenObservable = Observable.Return(true),
-                    ChildrenObservable = childrenObservable,
                 };
 
                 return model;

src/NexusMods.App.UI/Pages/NexusModsDataProvider.cs
@@ -364,14 +364,11 @@ public class NexusModsDataProvider : ILibraryDataProvider, ILoadoutDataProvider
                         return isEnabled.HasValue ? isEnabled.Value : null;
                     }).DistinctUntilChanged(x => x is null ? -1 : x.Value ? 1 : 0);
 
-                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection)
+                LoadoutItemModel model = new FakeParentLoadoutItemModel(loadoutItemIdsObservable, _serviceProvider, _connection, hasChildrenObservable, childrenObservable)
                 {
                     NameObservable = System.Reactive.Linq.Observable.Return(modPage.Name),
                     InstalledAtObservable = installedAtObservable,
                     IsEnabledObservable = isEnabledObservable,
-
-                    HasChildrenObservable = hasChildrenObservable,
-                    ChildrenObservable = childrenObservable,
                 };
 
                 return model;