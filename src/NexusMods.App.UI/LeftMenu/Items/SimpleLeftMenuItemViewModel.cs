212e85eb70627b38471b013c3f22a7f7f88ba931
AL <26797547+Al12rs@users.noreply.github.com>
2025-01-13 09:47:58 +0100

    Add support for Active and Selected states depending on Worskpace state (no single panel case yet)

--- src/NexusMods.App.UI/LeftMenu/Items/SimpleLeftMenuItemViewModel.cs
+++ src/NexusMods.App.UI/LeftMenu/Items/SimpleLeftMenuItemViewModel.cs
@@ -26,61 +26,58 @@ public class SimpleLeftMenuItemViewModel : AViewModel<INewLeftMenuItemViewModel>
         IWorkspaceController workspaceController,
         WorkspaceId workspaceId,
         PageData pageData
-        )
+    )
     {
         IsActive = false;
         IsSelected = false;
-        
-        NavigateCommand = ReactiveCommand.Create<NavigationInformation>((info) =>
-        {
-            var behavior = workspaceController.GetOpenPageBehavior(pageData, info);
-            workspaceController.OpenPage(workspaceId, pageData, behavior);
-        });
-        
-        
-        this.WhenActivated(d =>
-        {
-            var isActiveObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
-                .Where(workspace => workspace.Id == workspaceId)
-                .Select(workspace =>
-                    {
-                        return workspace.Panels.ToObservableChangeSet()
-                            .QueryWhenChanged(panels =>
-                                {
-                                    return panels.Any(panel =>
-                                        {
-                                            var context = panel.SelectedTab.Contents.PageData.Context;
-                                            var pageId = panel.SelectedTab.Contents.PageData.FactoryId;
-                                            return pageId == pageData.FactoryId && context == pageData.Context;
-                                        }
-                                    );
-                                }
-                            );
-                    }
-                )
-                .Switch();
-            
-            isActiveObservable.Subscribe(isActive => IsActive = isActive)
-                .DisposeWith(d);
-            
-            var isSelectedObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
-                .Where(workspace => workspace.Id == workspaceId)
-                .Select(workspace =>
-                    {
-                        return workspace.WhenAnyValue(w => w.SelectedTab);
-                    }
-                )
-                .Switch()
-                .Select(selectedTab =>
-                {
-                    var context = selectedTab.Contents.PageData.Context;
-                    var pageId = selectedTab.Contents.PageData.FactoryId;
-                    return pageId == pageData.FactoryId && context == pageData.Context;
-                });
-            
-            isSelectedObservable.Subscribe(isSelected => IsSelected = isSelected)
-                .DisposeWith(d);
 
-        });
+        NavigateCommand = ReactiveCommand.Create<NavigationInformation>((info) =>
+            {
+                var behavior = workspaceController.GetOpenPageBehavior(pageData, info);
+                workspaceController.OpenPage(workspaceId, pageData, behavior);
+            }
+        );
+
+        var isActiveObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
+            .Where(workspace => workspace.Id == workspaceId)
+            .Select(workspace =>
+                workspace.Panels.ToObservableChangeSet()
+                    .FilterOnObservable(panel =>
+                        panel.WhenAnyValue(p => p.SelectedTab.Contents)
+                            .Select(selectedTabContents =>
+                                {
+                                    // The SelectedTab Contents can be null at startup
+                                    var context = selectedTabContents?.PageData?.Context;
+                                    var pageId = selectedTabContents?.PageData?.FactoryId;
+                                    return pageData.FactoryId == pageId && pageData.Context.Equals(context);
+                                }
+                            )
+                    )
+                    .QueryWhenChanged(matchingPanels => matchingPanels.Count != 0)
+            )
+            .Switch();
+
+        var isSelectedObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
+            .Where(workspace => workspace.Id == workspaceId)
+            .Select(workspace => workspace.WhenAnyValue(w => w.SelectedTab.Contents))
+            .Switch()
+            .Select(selectedTabContents =>
+                {
+                    // The SelectedTab Contents can apparently be null at startup
+                    var context = selectedTabContents?.PageData?.Context;
+                    var pageId = selectedTabContents?.PageData?.FactoryId;
+                    return pageData.FactoryId == pageId && pageData.Context.Equals(context);
+                }
+            );
+
+        this.WhenActivated(d =>
+            {
+                isActiveObservable.Subscribe(isActive => IsActive = isActive)
+                    .DisposeWith(d);
+
+                isSelectedObservable.Subscribe(isSelected => IsSelected = isSelected)
+                    .DisposeWith(d);
+            }
+        );
     }
 }
