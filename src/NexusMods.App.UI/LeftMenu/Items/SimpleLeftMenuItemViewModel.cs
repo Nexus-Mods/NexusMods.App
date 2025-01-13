00516d71f6e872ba82a4825d2cae0f92112c265a
AL <26797547+Al12rs@users.noreply.github.com>
2025-01-13 12:30:17 +0100

    Add single Panel exclusion for Selected state

--- src/NexusMods.App.UI/LeftMenu/Items/SimpleLeftMenuItemViewModel.cs
+++ src/NexusMods.App.UI/LeftMenu/Items/SimpleLeftMenuItemViewModel.cs
@@ -3,7 +3,6 @@ using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using DynamicData;
 using DynamicData.Binding;
-using Microsoft.Extensions.DependencyInjection;
 using NexusMods.Abstractions.UI;
 using NexusMods.App.UI.Controls.Navigation;
 using NexusMods.App.UI.WorkspaceSystem;
@@ -38,8 +37,11 @@ public class SimpleLeftMenuItemViewModel : AViewModel<INewLeftMenuItemViewModel>
             }
         );
 
-        var isActiveObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
-            .Where(workspace => workspace.Id == workspaceId)
+        var workspaceIsActiveObservable = workspaceController
+            .WhenAnyValue(controller => controller.ActiveWorkspace)
+            .Where(workspace => workspace.Id == workspaceId);
+
+        var isActiveObservable = workspaceIsActiveObservable
             .Select(workspace =>
                 workspace.Panels.ToObservableChangeSet()
                     .FilterOnObservable(panel =>
@@ -47,8 +49,10 @@ public class SimpleLeftMenuItemViewModel : AViewModel<INewLeftMenuItemViewModel>
                             .Select(selectedTabContents =>
                                 {
                                     // The SelectedTab Contents can be null at startup
+                                    // ReSharper disable ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
                                     var context = selectedTabContents?.PageData?.Context;
                                     var pageId = selectedTabContents?.PageData?.FactoryId;
+                                    // ReSharper restore ConditionalAccessQualifierIsNonNullableAccordingToAPIContract
                                     return pageData.FactoryId == pageId && pageData.Context.Equals(context);
                                 }
                             )
@@ -56,9 +60,14 @@ public class SimpleLeftMenuItemViewModel : AViewModel<INewLeftMenuItemViewModel>
                     .QueryWhenChanged(matchingPanels => matchingPanels.Count != 0)
             )
             .Switch();
-
-        var isSelectedObservable = workspaceController.WhenAnyValue(controller => controller.ActiveWorkspace)
-            .Where(workspace => workspace.Id == workspaceId)
+        
+        var workspaceHasSinglePanelObservable = workspaceIsActiveObservable
+            .Select(workspace => workspace.WhenAnyValue(w => w.Panels.Count))
+            .Switch()
+            .Select(panelCount => panelCount == 1)
+            .Prepend(workspaceController.ActiveWorkspace.Panels.Count == 1);
+        
+        var isSelectedObservable = workspaceIsActiveObservable
             .Select(workspace => workspace.WhenAnyValue(w => w.SelectedTab.Contents))
             .Switch()
             .Select(selectedTabContents =>
@@ -68,7 +77,10 @@ public class SimpleLeftMenuItemViewModel : AViewModel<INewLeftMenuItemViewModel>
                     var pageId = selectedTabContents?.PageData?.FactoryId;
                     return pageData.FactoryId == pageId && pageData.Context.Equals(context);
                 }
-            );
+            )
+            .Prepend(pageData.Context.Equals(workspaceController.ActiveWorkspace.SelectedTab?.Contents?.PageData?.Context))
+            // No Selected state if there is only one panel in the workspace
+            .CombineLatest(workspaceHasSinglePanelObservable, (isSelected, hasSinglePanel) => isSelected && !hasSinglePanel);
 
         this.WhenActivated(d =>
             {
