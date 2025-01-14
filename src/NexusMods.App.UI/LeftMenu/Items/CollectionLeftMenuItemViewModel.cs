9b394077a89750f3387deb4c4449e30a49239924
AL <26797547+Al12rs@users.noreply.github.com>
2025-01-14 12:58:25 +0100

    Basic CollectionLeftMenuItemVM setup

---
+++ src/NexusMods.App.UI/LeftMenu/Items/CollectionLeftMenuItemViewModel.cs
@@ -0,0 +1,39 @@
+using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using NexusMods.Abstractions.Loadouts;
+using NexusMods.Abstractions.Loadouts.Extensions;
+using NexusMods.App.UI.WorkspaceSystem;
+using NexusMods.MnemonicDB.Abstractions;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace NexusMods.App.UI.LeftMenu.Items;
+
+public class CollectionLeftMenuItemViewModel : LeftMenuItemViewModel
+{
+    [Reactive] public bool IsEnabled { get; private set; }
+    
+    private CollectionGroupId _collectionGroupId;
+    
+    public CollectionLeftMenuItemViewModel(
+        IWorkspaceController workspaceController,
+        WorkspaceId workspaceId,
+        PageData pageData,
+        IServiceProvider serviceProvider,
+        CollectionGroupId collectionGroupId) : base(workspaceController, workspaceId, pageData)
+    {
+        _collectionGroupId = collectionGroupId;
+        var conn = serviceProvider.GetRequiredService<IConnection>();
+
+        var isEnabledObservable = CollectionGroup.Observe(conn, collectionGroupId)
+            .Select(collectionGroup => collectionGroup.AsLoadoutItemGroup().AsLoadoutItem().IsEnabled());
+        
+        this.WhenActivated(d =>
+        {
+            isEnabledObservable.Subscribe(isEnabled => IsEnabled = isEnabled)
+                .DisposeWith(d);
+        });
+
+    }
+}
