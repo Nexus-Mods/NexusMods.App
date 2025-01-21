157861ca9968ac3a95c29af5a07a6aebd22db427
erri120 <erri120@fossmailer.de>
2025-01-21 11:46:34 +0100

    Rebase fixes

--- src/NexusMods.App.UI/Pages/LoadoutPage/LoadoutView.axaml.cs
+++ src/NexusMods.App.UI/Pages/LoadoutPage/LoadoutView.axaml.cs
@@ -15,7 +15,6 @@ public partial class LoadoutView : ReactiveUserControl<ILoadoutViewModel>
         InitializeComponent();
 
         TreeDataGridViewHelper.SetupTreeDataGridAdapter<LoadoutView, ILoadoutViewModel, CompositeItemModel<EntityId>, EntityId>(this, TreeDataGrid, vm => vm.Adapter);
-        // TreeDataGridViewHelper.SetupTreeDataGridAdapter<LoadoutView, ILoadoutViewModel, LoadoutItemModel, EntityId>(this, TreeDataGrid, vm => vm.Adapter);
 
         this.WhenActivated(disposables =>
         {
