f250bfed53448fb761ed2901ff1453bee7b23849
Sewer56 <sewer56lol@googlemail.com>
2025-01-27 22:40:44 +0000

    Added: Seperate API for notifying for updates separately to updating metadata

--- src/Networking/NexusMods.Networking.NexusWebApi/IModUpdateService.cs
+++ src/Networking/NexusMods.Networking.NexusWebApi/IModUpdateService.cs
@@ -11,23 +11,35 @@ public interface IModUpdateService
 {
     /// <summary>
     /// Checks for available updates and updates mod information from Nexus.
-    /// After a successful update check, every file is notified of a potential update.
+    /// After a successful update check, every file and page is notified of a potential update if
+    /// the parameter to <paramref name="notify"/> is set to true.
     /// </summary>
     /// <param name="token">Cancellation token</param>
+    /// <param name="notify">True if external listeners should be notified after an update check.</param>
     /// <returns>Updated mod information</returns>
-    Task<PerFeedCacheUpdaterResult<PageMetadataMixin>> CheckAndUpdateMods(CancellationToken token);
+    Task<PerFeedCacheUpdaterResult<PageMetadataMixin>> CheckAndUpdateModPages(CancellationToken token, bool notify = true);
 
+    /// <summary>
+    /// Notifies of updates to mod files and mod pages based on our existing metadata.
+    /// If you need more up-to-date data, call <see cref="CheckAndUpdateModPages"/>.
+    /// </summary>
+    /// <remarks>
+    ///     Used when opening new views in the App, without an explicit update check,
+    ///     e.g. switching or opening new views.
+    /// </remarks>
+    void NotifyForUpdates();
+    
     /// <summary>
     /// Returns an observable for the newest version of a file.
     /// </summary>
     /// <returns>An observable that signals the newest version of a file.</returns>
-    public IObservable<NexusModsFileMetadata.ReadOnly> GetNewestFileVersionObservable(NexusModsFileMetadata.ReadOnly current);
+    IObservable<NexusModsFileMetadata.ReadOnly> GetNewestFileVersionObservable(NexusModsFileMetadata.ReadOnly current);
 
     /// <summary>
     /// Returns an observable when any file on a mod page is updated. 
     /// </summary>
     /// <param name="current">The current mod page to listen for changes in.</param>
-    public IObservable<NewestModPageVersionData> GetNewestModPageVersionObservable(NexusModsModPageMetadata.ReadOnly current);
+    IObservable<NewestModPageVersionData> GetNewestModPageVersionObservable(NexusModsModPageMetadata.ReadOnly current);
 }
 
 /// <summary>
