ff1bdc973f0b8c42f2a8865d06910959f29ab85f
Sewer56 <sewer56lol@googlemail.com>
2025-01-27 20:58:36 +0000

    Changed: Migrate Update Check to ModUpdateService

---
+++ src/Networking/NexusMods.Networking.NexusWebApi/ModUpdateService.cs
@@ -0,0 +1,55 @@
+using Microsoft.Extensions.Logging;
+using NexusMods.Abstractions.NexusWebApi;
+using NexusMods.MnemonicDB.Abstractions;
+using NexusMods.Networking.ModUpdates;
+using NexusMods.Networking.ModUpdates.Mixins;
+namespace NexusMods.Networking.NexusWebApi;
+
+public class ModUpdateService : IModUpdateService
+{
+    private readonly IConnection _connection;
+    private readonly INexusApiClient _nexusApiClient;
+    private readonly IGameDomainToGameIdMappingCache _gameIdMappingCache;
+    private readonly ILogger<ModUpdateService> _logger;
+    private readonly NexusGraphQLClient _gqlClient;
+
+    public ModUpdateService(
+        IConnection connection,
+        INexusApiClient nexusApiClient,
+        IGameDomainToGameIdMappingCache gameIdMappingCache,
+        ILogger<ModUpdateService> logger,
+        NexusGraphQLClient gqlClient)
+    {
+        _connection = connection;
+        _nexusApiClient = nexusApiClient;
+        _gameIdMappingCache = gameIdMappingCache;
+        _logger = logger;
+        _gqlClient = gqlClient;
+    }
+
+    public async Task<PerFeedCacheUpdaterResult<PageMetadataMixin>> CheckAndUpdateMods(CancellationToken token)
+    {
+        // Identify all mod pages needing a refresh
+        var updateCheckResult = await RunUpdateCheck.CheckForModPagesWhichNeedUpdating(
+            _connection.Db, 
+            _nexusApiClient, 
+            _gameIdMappingCache);
+        
+        // Start a transaction with updated info if at least 1 item needs
+        // updating with upstream server
+        if (updateCheckResult.AnyItemNeedsUpdate())
+        {
+            using var tx = _connection.BeginTransaction();
+            await RunUpdateCheck.UpdateModFilesForOutdatedPages(
+                _connection.Db,
+                tx,
+                _logger,
+                _gqlClient,
+                updateCheckResult,
+                token);
+            await tx.Commit();
+        }
+
+        return updateCheckResult;
+    }
+}
