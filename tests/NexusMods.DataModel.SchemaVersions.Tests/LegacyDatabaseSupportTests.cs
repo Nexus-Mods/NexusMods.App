577ceffd0d4b936bd2ff1d5d475d73da32632278
halgari <tbaldridge@gmail.com>
2025-01-27 17:16:40 -0700

    Added tests for the timestamp migration

--- tests/NexusMods.DataModel.SchemaVersions.Tests/LegacyDatabaseSupportTests.cs
+++ tests/NexusMods.DataModel.SchemaVersions.Tests/LegacyDatabaseSupportTests.cs
@@ -1,64 +1,19 @@
-using FluentAssertions;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using NexusMods.Abstractions.FileExtractor;
 using NexusMods.Abstractions.Loadouts;
-using NexusMods.FileExtractor.FileSignatures;
-using NexusMods.Hashing.xxHash3;
-using NexusMods.MnemonicDB;
 using NexusMods.MnemonicDB.Abstractions;
-using NexusMods.MnemonicDB.Abstractions.ElementComparers;
-using NexusMods.MnemonicDB.Abstractions.IndexSegments;
-using NexusMods.MnemonicDB.Storage;
-using NexusMods.MnemonicDB.Storage.RocksDbBackend;
-using NexusMods.Paths;
 
 namespace NexusMods.DataModel.SchemaVersions.Tests;
 
-public class LegacyDatabaseSupportTests(IServiceProvider provider, IFileExtractor extractor)
+public class LegacyDatabaseSupportTests(IServiceProvider provider) : ALegacyDatabaseTest(provider)
 {
-    private readonly SignatureChecker _zipSignatureChecker = new(FileType.ZIP);
-    
     [Theory]
     [MemberData(nameof(DatabaseNames))]
     public async Task TestDatabase(string name)
     {
-        var path = DatabaseFolder().Combine(name); 
-        path.FileExists.Should().BeTrue("the database file should exist");
+        using var tempConnection = await ConnectionFor(name);
         
-        await using (var stream = path.Read())
-        {
-            var isZip = await _zipSignatureChecker.MatchesAnyAsync(stream);
-            isZip.Should().BeTrue("the database file should be a ZIP archive, you may need to pull the file from LFS (`git lfs pull`)");
-        }
-
-        var basePath = FileSystem.Shared.GetKnownPath(KnownPath.EntryDirectory).Combine("Temp").Combine($"Test-{Guid.NewGuid()}");
-        basePath.CreateDirectory();
-        basePath.DirectoryExists().Should().BeTrue();
-
-        using var temporaryFileManager = new TemporaryFileManager(FileSystem.Shared, basePath: basePath);
-
-        await using var workingFolder = temporaryFileManager.CreateFolder();
-        await extractor.ExtractAllAsync(path, workingFolder.Path);
-
-        var datamodelFolder = workingFolder.Path.Combine("MnemonicDB.rocksdb");
-        datamodelFolder.DirectoryExists().Should().BeTrue("the extracted database folder should exist");
-        datamodelFolder.EnumerateFiles().Should().NotBeEmpty("the extracted database folder should contain files");
-
-        using var backend = new Backend();
-        var settings = new DatomStoreSettings
-        {
-            Path = datamodelFolder,
-        };
-        using var datomStore = new DatomStore(provider.GetRequiredService<ILogger<DatomStore>>(), settings, backend);
-        var connection = new Connection(provider.GetRequiredService<ILogger<Connection>>(), datomStore, provider, provider.GetServices<IAnalyzer>());
-        
-        var oldMigrationId = RecordedVersion(connection.Db);
-        
-        var migrationService = new MigrationService(provider.GetRequiredService<ILogger<MigrationService>>(), connection, provider, provider.GetServices<MigrationDefinition>());
-        await migrationService.MigrateAll();
-        
-        await Verify(GetStatistics(connection.Db, name, oldMigrationId)).UseParameters(name);
+        await Verify(GetStatistics(tempConnection.Connection.Db, name, tempConnection.OldId)).UseParameters(name);
     }
 
     private MigrationId RecordedVersion(IDb db)
@@ -108,22 +63,5 @@ public class LegacyDatabaseSupportTests(IServiceProvider provider, IFileExtracto
         public int Files { get; init; }
         public int Collections { get; init; }
         public string Created { get; init; }
-    }
-    
-
-    public static IEnumerable<object[]> DatabaseNames()
-    {
-        var databaseFolder = DatabaseFolder();
-        foreach (var file in databaseFolder.EnumerateFiles("*.zip").Order())
-        {
-            yield return [file.Name];
-        }
-    }
-
-    private static AbsolutePath DatabaseFolder()
-    {
-        var basePath = FileSystem.Shared.GetKnownPath(KnownPath.EntryDirectory).Parent.Parent.Parent;
-        var databaseFolder = basePath.Combine("Resources/Databases");
-        return databaseFolder;
     }
 }
