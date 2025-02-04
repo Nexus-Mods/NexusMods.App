fbcd99480f3aa08f65161523fe6ced72e5f8e418
Sewer56 <sewer56lol@googlemail.com>
2025-02-04 23:26:47 +0000

    Removed: Unused 'using' directives from 'ILibraryItemModel'

--- src/NexusMods.App.UI/Pages/LibraryPage/ILibraryItemModel.cs
+++ src/NexusMods.App.UI/Pages/LibraryPage/ILibraryItemModel.cs
@@ -1,14 +1,5 @@
-using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
-using DynamicData;
-using JetBrains.Annotations;
-using NexusMods.Abstractions.Library.Models;
-using NexusMods.Abstractions.Loadouts;
-using NexusMods.Abstractions.UI.Extensions;
 using NexusMods.App.UI.Controls;
 using NexusMods.MnemonicDB.Abstractions;
-using ObservableCollections;
-using R3;
 
 namespace NexusMods.App.UI.Pages.LibraryPage;
 
