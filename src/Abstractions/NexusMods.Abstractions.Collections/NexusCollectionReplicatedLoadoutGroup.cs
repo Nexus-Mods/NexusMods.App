4bc5d69402965889877392802f2fd406e4c504ae
erri120 <erri120@fossmailer.de>
2025-02-06 11:09:15 +0100

    Add docs

--- src/Abstractions/NexusMods.Abstractions.Collections/NexusCollectionReplicatedLoadoutGroup.cs
+++ src/Abstractions/NexusMods.Abstractions.Collections/NexusCollectionReplicatedLoadoutGroup.cs
@@ -3,6 +3,11 @@ using NexusMods.MnemonicDB.Abstractions.Models;
 
 namespace NexusMods.Abstractions.Collections;
 
+/// <summary>
+/// Represents a collection mod that was installed using the "replicated"-method. Instead of using an installer,
+/// the collection contains information about where to put each file of the mod, making these an exact replica of
+/// what the collection author had on their machine.
+/// </summary>
 [Include<NexusCollectionItemLoadoutGroup>]
 public partial class NexusCollectionReplicatedLoadoutGroup : IModelDefinition
 {
