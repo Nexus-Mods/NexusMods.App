53726a55668e70771a1b5023f80d674a693afcc1
erri120 <erri120@fossmailer.de>
2025-02-17 13:19:35 +0100

    Add migration and tests

---
+++ src/NexusMods.DataModel.SchemaVersions/Migrations/_0006_DirectDownload.cs
@@ -0,0 +1,29 @@
+using NexusMods.Abstractions.Collections;
+using NexusMods.Abstractions.Library.Models;
+using NexusMods.MnemonicDB.Abstractions;
+
+namespace NexusMods.DataModel.SchemaVersions.Migrations;
+
+/// <summary>
+/// Turns all DirectDownloadLibraryFile entities into LocalFile entities by adding the LocalFile.OriginalPath attribute.
+/// </summary>
+internal class _0006_DirectDownload : ITransactionalMigration
+{
+    public static (MigrationId Id, string Name) IdAndName { get; } = MigrationId.ParseNameAndId(nameof(_0006_DirectDownload));
+
+    private DirectDownloadLibraryFile.ReadOnly[] _entitiesToUpdate = [];
+
+    public Task Prepare(IDb db)
+    {
+        _entitiesToUpdate = DirectDownloadLibraryFile.All(db).Where(entity => !LocalFile.OriginalPath.IsIn(entity)).ToArray();
+        return Task.CompletedTask;
+    }
+
+    public void Migrate(ITransaction tx, IDb db)
+    {
+        foreach (var entity in _entitiesToUpdate)
+        {
+            tx.Add(entity.Id, LocalFile.OriginalPath, entity.LogicalFileName);
+        }
+    }
+}
