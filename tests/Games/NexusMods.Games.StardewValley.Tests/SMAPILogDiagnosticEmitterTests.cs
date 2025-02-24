3904cbb1f0a390e3bbad8071b97b6641cb9ce1fc
Mike Watling <mike.watling@nexusmods.com>
2025-02-24 10:34:35 +0000

    Started adding InMemoryFileSystem for the tests, need to figure out how it works.

--- tests/Games/NexusMods.Games.StardewValley.Tests/SMAPILogDiagnosticEmitterTests.cs
+++ tests/Games/NexusMods.Games.StardewValley.Tests/SMAPILogDiagnosticEmitterTests.cs
@@ -1,8 +1,20 @@
 
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NexusMods.Abstractions.Diagnostics;
+using NexusMods.Abstractions.NexusWebApi.Types.V2;
+using NexusMods.Games.StardewValley.Emitters;
+using NexusMods.Games.TestFramework;
+using NexusMods.StandardGameLocators.TestHelpers;
+using Xunit.Abstractions;
+using NexusMods.Paths;
+
 namespace NexusMods.Games.StardewValley.Tests;
 
-public class SMAPILogDiagnosticEmitterTests : ALoadoutDiagnosticEmitterTest<SMAPIGameVersionDiagnosticEmitterTests, StardewValley, SMAPIGameVersionDiagnosticEmitter>
+public class SMAPILogDiagnosticEmitterTests : ALoadoutDiagnosticEmitterTest<SMAPIGameVersionDiagnosticEmitterTests, StardewValley, SMAPILogDiagnosticEmitter>
 {
+    public static readonly InMemoryFileSystem _fileSystem = new InMemoryFileSystem();
+    
     public SMAPILogDiagnosticEmitterTests(ITestOutputHelper testOutputHelper) : base(testOutputHelper) { }
 
     protected override IServiceCollection AddServices(IServiceCollection services)
@@ -13,7 +25,19 @@ public class SMAPILogDiagnosticEmitterTests : ALoadoutDiagnosticEmitterTest<SMAP
     }
 
     [Fact]
-    public async Task Test_SMAPILogNotPresent()
+    public async Task Test_SMAPILogFolderNotPresent()
+    {
+        var loadout = await CreateLoadout();
+
+        // SMAPI 4.1.10 (https://www.nexusmods.com/stardewvalley/mods/2400?tab=files)
+        await InstallModFromNexusMods(loadout, ModId.From(2400), FileId.From(119630));
+
+        // Create log files
+
+    }
+
+    [Fact]
+    public async Task Test_SMAPIErrorLogIsLatest()
     {
         var loadout = await CreateLoadout();
 
