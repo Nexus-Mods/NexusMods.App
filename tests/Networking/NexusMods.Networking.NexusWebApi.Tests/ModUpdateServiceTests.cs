07118ae90daefb63b47537a0f0bbce88a1e69b8e
Sewer56 <sewer56lol@googlemail.com>
2025-02-26 16:43:37 +0000

    Added: Basic tests around receiving update notification events.

--- tests/Networking/NexusMods.Networking.NexusWebApi.Tests/ModUpdateServiceTests.cs
+++ tests/Networking/NexusMods.Networking.NexusWebApi.Tests/ModUpdateServiceTests.cs
@@ -6,6 +6,12 @@ using NexusMods.Abstractions.NexusWebApi;
 using NexusMods.MnemonicDB.Abstractions;
 using NexusMods.Networking.ModUpdates;
 using NexusMods.Paths;
+using System.Reactive.Linq;
+using DynamicData.Kernel;
+using NexusMods.Abstractions.GC;
+using NexusMods.Abstractions.NexusWebApi.Types.V2;
+using NexusMods.Games.TestFramework;
+using Xunit.Abstractions;
 
 namespace NexusMods.Networking.NexusWebApi.Tests;
 
@@ -19,7 +25,7 @@ namespace NexusMods.Networking.NexusWebApi.Tests;
 ///     - Correct result of updates.
 /// </summary>
 [Trait("RequiresNetworking", "True")]
-public class ModUpdateServiceTests
+public class ModUpdateServiceTests : ACyberpunkIsolatedGameTest<ModUpdateServiceTests> // game doesn't matter here, we just don't have SDV.
 {
     private readonly IConnection _connection;
     private readonly IGameDomainToGameIdMappingCache _gameIdMappingCache;
@@ -33,6 +39,7 @@ public class ModUpdateServiceTests
     private readonly FakeTimeProvider _timeProvider;
 
     public ModUpdateServiceTests(
+        ITestOutputHelper outputHelper,
         IConnection connection,
         INexusApiClient nexusApiClient,
         IGameDomainToGameIdMappingCache gameIdMappingCache,
@@ -40,7 +47,7 @@ public class ModUpdateServiceTests
         NexusGraphQLClient gqlClient,
         TemporaryFileManager temporaryFileManager,
         ILibraryService libraryService,
-        NexusModsLibrary nexusModsLibrary)
+        NexusModsLibrary nexusModsLibrary) : base(outputHelper)
     {
         _connection = connection;
         _nexusApiClient = nexusApiClient;
@@ -143,6 +150,160 @@ public class ModUpdateServiceTests
     }
 
     [Fact]
+    public async Task GetNewestFileVersionObservable_ShouldNotifyOnLibraryItemAdd()
+    {
+        // Arrange
+        var spaceCoreData = StaticTestData.SpaceCoreModData;
+
+        // Download an old version of SpaceCore
+        await using var tempFile = _temporaryFileManager.CreateFile();
+        var downloadJob = await _nexusModsLibrary.CreateDownloadJob(
+            destination: tempFile,
+            gameId: (GameId)spaceCoreData.GameId,
+            modId: (ModId)spaceCoreData.ModId,
+            fileId: (FileId)spaceCoreData.FileId
+        );
+    
+        // Setup our listening.
+        var observable = _modUpdateService.GetNewestFileVersionObservable(downloadJob.Job.FileMetadata);
+    
+        // Create collection for results
+        var results = new List<ModUpdateOnPage>();
+        using var subscription = observable.Subscribe(val => results.Add(val.Value));
+    
+        // Right into the library.
+        await _libraryService.AddDownload(downloadJob);
+
+        // Assert
+        // At least this amount of updates
+        results.Should().HaveCount(1);
+        var updateOnPage = results[0];
+        updateOnPage.NewerFiles.Should().HaveCountGreaterThanOrEqualTo(spaceCoreData.Updates.Length);
+
+        // And check all expected updates were found.
+        AssertUpdatesContainAllResults(spaceCoreData.Updates, updateOnPage);
+    }
+    
+    [Fact]
+    public async Task GetNewestFileVersionObservable_ShouldNotifyOnLibraryItemRemove()
+    {
+        // Arrange
+        var spaceCoreData = StaticTestData.SpaceCoreModData;
+
+        // Download an old version of SpaceCore
+        await using var tempFile = _temporaryFileManager.CreateFile();
+        var downloadJob = await _nexusModsLibrary.CreateDownloadJob(
+            destination: tempFile,
+            gameId: (GameId)spaceCoreData.GameId,
+            modId: (ModId)spaceCoreData.ModId,
+            fileId: (FileId)spaceCoreData.FileId
+        );
+    
+        // Setup our listening.
+        var observable = _modUpdateService.GetNewestFileVersionObservable(downloadJob.Job.FileMetadata);
+    
+        // Create collection for results
+        var receivedRemove = false;
+        using var subscription = observable.Subscribe(val =>
+            {
+                if (!val.HasValue)
+                    receivedRemove = true;
+            }
+        );
+    
+        // Right into the library.
+        // This fires the event.
+        var libraryFile = await _libraryService.AddDownload(downloadJob);
+        receivedRemove.Should().BeFalse();
+
+        // Now remove it from the library.
+        await _libraryService.RemoveItems([libraryFile.AsLibraryItem()], GarbageCollectorRunMode.DoNotRun);
+        receivedRemove.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task GetNewestModPageVersionObservable_ShouldNotifyOnLibraryItemAdd()
+    {
+        // Arrange
+        var spaceCoreData = StaticTestData.SpaceCoreModData;
+
+        // Download an old version of SpaceCore
+        await using var tempFile = _temporaryFileManager.CreateFile();
+        var downloadJob = await _nexusModsLibrary.CreateDownloadJob(
+            destination: tempFile,
+            gameId: (GameId)spaceCoreData.GameId,
+            modId: (ModId)spaceCoreData.ModId,
+            fileId: (FileId)spaceCoreData.FileId
+        );
+
+        // Get the mod page metadata
+        var modPageMetadata = downloadJob.Job.FileMetadata.ModPage;
+
+        // Setup our listening for mod page updates
+        var observable = _modUpdateService.GetNewestModPageVersionObservable(modPageMetadata);
+
+        // Create collection for results
+        var results = new List<ModUpdatesOnModPage>();
+        using var subscription = observable.Subscribe(val => results.Add(val.Value));
+
+        // Right into the library.
+        await _libraryService.AddDownload(downloadJob);
+
+        // Assert
+        // At least this amount of updates
+        results.Should().HaveCount(1);
+        var updatesOnModPage = results[0];
+        var newestFile = updatesOnModPage.NewestFile();
+
+        // Check the returned file is 'sensible'
+        newestFile.Uid.GameId.Should().Be((GameId)spaceCoreData.GameId);
+        newestFile.ModPage.Uid.ModId.Should().Be((ModId)spaceCoreData.ModId);
+        
+        // And check for all expected update files.
+        AssertUpdatesContainAllResults(spaceCoreData.Updates, updatesOnModPage.FileMappings[0]);
+    }
+
+    [Fact]
+    public async Task GetNewestModPageVersionObservable_ShouldNotifyOnLibraryItemRemove()
+    {
+        // Arrange
+        var spaceCoreData = StaticTestData.SpaceCoreModData;
+
+        // Download an old version of SpaceCore
+        await using var tempFile = _temporaryFileManager.CreateFile();
+        var downloadJob = await _nexusModsLibrary.CreateDownloadJob(
+            destination: tempFile,
+            gameId: (GameId)spaceCoreData.GameId,
+            modId: (ModId)spaceCoreData.ModId,
+            fileId: (FileId)spaceCoreData.FileId
+        );
+
+        // Get the mod page metadata
+        var modPageMetadata = downloadJob.Job.FileMetadata.ModPage;
+
+        // Setup our listening for mod page updates
+        var observable = _modUpdateService.GetNewestModPageVersionObservable(modPageMetadata);
+
+        // Create collection for results
+        var receivedRemove = false;
+        using var subscription = observable.Subscribe(val =>
+            {
+                if (!val.HasValue)
+                    receivedRemove = true;
+            }
+        );
+
+        // Right into the library.
+        // This fires the event.
+        var libraryFile = await _libraryService.AddDownload(downloadJob);
+        receivedRemove.Should().BeFalse();
+
+        // Now remove it from the library.
+        await _libraryService.RemoveItems([libraryFile.AsLibraryItem()], GarbageCollectorRunMode.DoNotRun);
+        receivedRemove.Should().BeTrue();
+    }
+
+    [Fact]
     public void Dispose_ShouldNotThrow()
     {
         // Arrange
@@ -153,6 +314,24 @@ public class ModUpdateServiceTests
         // Act & Assert
         var action = () => ((IDisposable)modUpdateService).Dispose();
         action.Should().NotThrow();
+    }
+    
+    private static void AssertUpdatesContainAllResults(StaticTestData.TestModData[] updates, ModUpdateOnPage modUpdate)
+    {
+        foreach (var expectedUpdate in updates)
+        {
+            // Find the fle by update ID
+            var found = modUpdate.NewerFiles.FirstOrDefault(x => x.Uid.FileId == expectedUpdate.FileId);
+            
+            // Did we find it?
+            found.Should().NotBeNull();
+            
+            // Check the details match.
+            expectedUpdate.Version.Should().Be(found.Version);
+            expectedUpdate.Name.Should().Be(found.Name);
+            expectedUpdate.ModId.Should().Be(found.ModPage.Uid.ModId.Value);
+            expectedUpdate.GameId.Should().Be(found.Uid.GameId.Value);
+        }
     }
 }
 
@@ -182,9 +361,14 @@ internal static class StaticTestData
         public uint GameId { get; init; }
 
         /// <summary>
+        /// 32-bit ID of mod on NexusMods.com
+        /// </summary>
+        public uint ModId { get; init; }
+
+        /// <summary>
         /// Version as attached to the file to Nexus.
         /// </summary>
-        public string Version { get; init; }
+        public string Version { get;init; }
 
         /// <summary>
         /// Expected updates for this file.
@@ -196,9 +380,10 @@ internal static class StaticTestData
     public static readonly TestModData SmapiModData = new()
     {
         Name = "SMAPI 4.0.8",
-        FileId = 5001,
+        FileId = 94412,
         GameId = 1303,
         Version = "4.0.8",
+        ModId = 2400,
         Updates =
         [
             new TestModData
@@ -207,6 +392,7 @@ internal static class StaticTestData
                 FileId = 109556,
                 GameId = 1303,
                 Version = "4.1.0-beta.1",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -214,6 +400,7 @@ internal static class StaticTestData
                 FileId = 110769,
                 GameId = 1303,
                 Version = "4.1.0-beta.2",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -221,6 +408,7 @@ internal static class StaticTestData
                 FileId = 112400,
                 GameId = 1303,
                 Version = "4.1.0-beta.3",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -228,6 +416,7 @@ internal static class StaticTestData
                 FileId = 112827,
                 GameId = 1303,
                 Version = "4.1.0-beta.4",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -235,6 +424,7 @@ internal static class StaticTestData
                 FileId = 113649,
                 GameId = 1303,
                 Version = "4.1.0-beta.5",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -242,6 +432,7 @@ internal static class StaticTestData
                 FileId = 115051,
                 GameId = 1303,
                 Version = "4.1.0",
+                ModId = 2400,
             },
             new TestModData
             {
@@ -249,6 +440,7 @@ internal static class StaticTestData
                 FileId = 115121,
                 GameId = 1303,
                 Version = "4.1.1",
+                ModId = 2400,
             },
         ],
     };
@@ -257,53 +449,60 @@ internal static class StaticTestData
     public static readonly TestModData SpaceCoreModData = new()
     {
         Name = "1.2-SpaceCore-1.0.1.zip",
-        FileId = 94412,
+        FileId = 5001,
         GameId = 1303,
         Version = "1.0.1",
-        Updates = new TestModData[]
-        {
-            new()
+        ModId = 1348,
+        Updates =
+        [
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.0.2.zip",
                 FileId = 5018,
                 GameId = 1303,
                 Version = "1.0.2",
+                ModId = 1348,
             },
-            new()
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.0.3.zip",
                 FileId = 5071,
                 GameId = 1303,
                 Version = "1.0.3",
+                ModId = 1348,
             },
-            new()
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.0.4.zip",
                 FileId = 6066,
                 GameId = 1303,
                 Version = "1.0.4",
+                ModId = 1348,
             },
-            new()
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.0.5.zip",
                 FileId = 6077,
                 GameId = 1303,
                 Version = "1.0.5",
+                ModId = 1348,
             },
-            new()
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.1.0.zip",
                 FileId = 7516,
                 GameId = 1303,
                 Version = "1.1.0",
+                ModId = 1348,
             },
-            new()
+            new TestModData
             {
                 Name = "1.2-SpaceCore-1.1.1.zip",
                 FileId = 7718,
                 GameId = 1303,
                 Version = "1.1.1",
+                ModId = 1348,
             },
-        },
+        ],
     };
 }
