e15c9a9611c123294da8ec2ffa8690df8c87f83a
AL <26797547+Al12rs@users.noreply.github.com>
2025-03-11 16:39:32 +0100

    Remove startup sort since it only sort the items that are present in the source at the time of the call, not any items that might still be getting added

--- src/NexusMods.App.UI/Controls/TreeDataGrid/TreeDataGridAdapter.cs
+++ src/NexusMods.App.UI/Controls/TreeDataGrid/TreeDataGridAdapter.cs
@@ -125,16 +125,6 @@ public abstract class TreeDataGridAdapter<TModel, TKey> : ReactiveR3Object
                 .Subscribe()
                 .AddTo(disposables);
 
-            self.Source.AsObservable()
-                // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
-                .Where(source => source is not null)
-                .Subscribe(static (source) =>
-                    {
-                        var sortColumn = source.Columns.FirstOrOptional(column => column.SortDirection is not null);
-                        if (sortColumn.HasValue) source.SortBy(sortColumn.Value, sortColumn.Value.SortDirection!.Value);
-                    }
-                );
-
             Disposable.Create(self._selectionModelsSerialDisposable, static serialDisposable => serialDisposable.Disposable = null).AddTo(disposables);
         });
     }
