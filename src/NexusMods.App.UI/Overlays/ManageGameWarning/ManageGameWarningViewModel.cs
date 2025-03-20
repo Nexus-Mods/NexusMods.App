00a555db7fb1bd3a8a4f710020a16fac5b32dec3
Sewer56 <sewer56lol@googlemail.com>
2025-03-20 15:02:50 +0000

    Removed: Invalid XML Comment

--- src/NexusMods.App.UI/Overlays/ManageGameWarning/ManageGameWarningViewModel.cs
+++ src/NexusMods.App.UI/Overlays/ManageGameWarning/ManageGameWarningViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using JetBrains.Annotations;
 namespace NexusMods.App.UI.Overlays.ManageGameWarning;
 
-/// <summary>
-/// Primary view model for the MetricsOptIn overlay.
-/// </summary>
 [UsedImplicitly]
 public class ManageGameWarningViewModel : AOverlayViewModel<IManageGameWarningViewModel, bool>, IManageGameWarningViewModel { }
