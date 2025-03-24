90b7602da91f7f1c5a83f91bacb08dc69ab07625
AL <26797547+Al12rs@users.noreply.github.com>
2025-03-24 12:57:16 +0100

    Update LoadOrderDataProvider to provide MoveUp and MoveDown canExecute observables

--- src/NexusMods.App.UI/Pages/Sorting/LoadOrder/TreeDataGrid/LoadOrderDataProvider.cs
+++ src/NexusMods.App.UI/Pages/Sorting/LoadOrder/TreeDataGrid/LoadOrderDataProvider.cs
@@ -1,26 +1,56 @@
+using System.ComponentModel;
 using System.Reactive.Linq;
 using DynamicData;
 using DynamicData.Binding;
 using Humanizer;
 using NexusMods.Abstractions.Games;
 using NexusMods.App.UI.Controls;
+using R3;
 using ReactiveUI;
 using static NexusMods.App.UI.Pages.Sorting.LoadOrderComponents;
+// ReSharper disable InvokeAsExtensionMethod
 
 namespace NexusMods.App.UI.Pages.Sorting;
 
 public class LoadOrderDataProvider : ILoadOrderDataProvider
 {
     
-    public IObservable<IChangeSet<CompositeItemModel<Guid>, Guid>> ObserveLoadOrder(ILoadoutSortableItemProvider sortableItemProvider)
+    public IObservable<IChangeSet<CompositeItemModel<Guid>, Guid>> ObserveLoadOrder(
+        ILoadoutSortableItemProvider sortableItemProvider,
+        Observable<ListSortDirection> sortDirectionObservable)
     {
+        var lastIndexObservable = sortableItemProvider.SortableItems
+            .ToObservableChangeSet(item => item.ItemId)
+            .QueryWhenChanged(query => query.Count == 0 ? 0 : query.Items.Max(item => item.SortIndex))
+            .ToObservable()
+            .Prepend(sortableItemProvider.SortableItems.Count == 0 ? 0 : sortableItemProvider.SortableItems.Max(item => item.SortIndex));
+
+        var topMostIndexObservable = R3.Observable.CombineLatest(
+                sortDirectionObservable,
+                lastIndexObservable,
+                (sortDirection, lastIndex) => sortDirection == ListSortDirection.Ascending ? 0 : lastIndex
+            )
+            .Publish()
+            .RefCount();
+
+        var bottomMostIndexObservable = R3.Observable.CombineLatest(
+                sortDirectionObservable,
+                lastIndexObservable,
+                (sortDirection, lastIndex) => sortDirection == ListSortDirection.Ascending ? lastIndex : 0
+            )
+            .Publish()
+            .RefCount();
+        
         return sortableItemProvider.SortableItems
             .ToObservableChangeSet(item => item.ItemId)
-            .Transform( item => ToLoadOrderItemModel(item));
+            .Transform( item => ToLoadOrderItemModel(item, topMostIndexObservable, bottomMostIndexObservable));
     }
 
 
-    private CompositeItemModel<Guid> ToLoadOrderItemModel(ISortableItem sortableItem)
+    private static CompositeItemModel<Guid> ToLoadOrderItemModel(
+        ISortableItem sortableItem,
+        R3.Observable<int> topMostIndexObservable,
+        R3.Observable<int> bottomMostIndexObservable)
     {
         var compositeModel = new CompositeItemModel<Guid>(sortableItem.ItemId);
 
@@ -37,17 +67,31 @@ public class LoadOrderDataProvider : ILoadOrderDataProvider
             new ValueComponent<bool>(sortableItem.IsActive, sortableItem.WhenAnyValue(item => item.IsActive)));
 
         // SortIndex
+        var sortIndexObservable = sortableItem.WhenAnyValue(item => item.SortIndex).ToObservable();
+        
+        var canExecuteMoveUp = R3.Observable.CombineLatest(
+            sortIndexObservable,
+            topMostIndexObservable,
+            (sortIndex, topMostIndex) => sortIndex != topMostIndex
+        );
+        var canExecuteMoveDown = R3.Observable.CombineLatest(
+            sortIndexObservable,
+            bottomMostIndexObservable,
+            (sortIndex, bottomMost) => sortIndex != bottomMost
+        );
+        
         var displayIndexObservable = sortableItem.WhenAnyValue(item => item.SortIndex).Select(index => index.Ordinalize());
+        
         compositeModel.Add(LoadOrderColumns.IndexColumn.IndexComponentKey,
             new IndexComponent(
-                new ValueComponent<int>(sortableItem.SortIndex, sortableItem.WhenAnyValue(item => item.SortIndex)),
+                new ValueComponent<int>(sortableItem.SortIndex, sortIndexObservable),
                 new ValueComponent<string>(sortableItem.SortIndex.Ordinalize(), displayIndexObservable),
-                // TODO: Implement canExecuteMoveUp and canExecuteMoveDown
-                R3.Observable.Return(true),
-                R3.Observable.Return(true)
+                canExecuteMoveUp,
+                canExecuteMoveDown
             )
         );
 
+        
         return compositeModel;
     }
 }
