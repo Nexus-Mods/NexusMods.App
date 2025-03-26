51f41bbe7830af17d705177af8d2a0d94cb6de5f
Simon Davies <simon.davies@nexusmods.com>
2025-03-26 11:04:07 +0000

    Scrollbars back in line

--- src/NexusMods.App.UI/Controls/StandardButton.cs
+++ src/NexusMods.App.UI/Controls/StandardButton.cs
@@ -1,4 +1,5 @@
 using Avalonia;
+using Avalonia.Automation;
 using Avalonia.Controls;
 using Avalonia.Controls.Metadata;
 using Avalonia.Controls.Presenters;
--- src/NexusMods.App.UI/Pages/DebugControls/DebugControlsPageView.axaml.cs
+++ src/NexusMods.App.UI/Pages/DebugControls/DebugControlsPageView.axaml.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using System.Reactive.Disposables;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
