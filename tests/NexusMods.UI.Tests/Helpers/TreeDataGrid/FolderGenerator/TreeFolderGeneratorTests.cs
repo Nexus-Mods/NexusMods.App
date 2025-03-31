722434742f99d49006bd71c73c1cecd62faa68d2
Sewer56 <sewer56lol@googlemail.com>
2025-03-31 14:50:22 +0100

    Added: Tests for observability of ObservableRoots in TreeFolderGeneratorTests

--- tests/NexusMods.UI.Tests/Helpers/TreeDataGrid/FolderGenerator/TreeFolderGeneratorTests.cs
+++ tests/NexusMods.UI.Tests/Helpers/TreeDataGrid/FolderGenerator/TreeFolderGeneratorTests.cs
@@ -132,16 +132,189 @@ public class TreeFolderGeneratorTests
     }
 
     [Fact]
-    public void ObservableRoots_ReturnsEmptySnapshot_WhenNoFilesAdded()
+    public void ObservableRoots_EmitsChange_WhenFirstItemAdded()
     {
         // Arrange
         var generator = CreateGenerator();
-
-        // Act - verify no trees exist initially
-        var idToTree = generator.LocationIdToTree;
-
+        var fileId = EntityId.From(0UL);
+        var locationId = LocationId.Game;
+        var filePath = new GamePath(locationId, new RelativePath("file.txt"));
+        var fileModel = CreateFileModel(fileId);
+        var testItem = CreateTestTreeItem(fileId, filePath);
+        
+        var addedItems = new List<CompositeItemModel<EntityId>>();
+        
+        // Subscribe to the observable
+        using var subscription = generator.ObservableRoots()
+            .Subscribe(changes => 
+            {
+                foreach (var change in changes)
+                {
+                    if (change.Reason == DynamicData.ChangeReason.Add)
+                    {
+                        addedItems.Add(change.Current);
+                    }
+                }
+            });
+            
+        // Act
+        generator.OnReceiveFile(testItem, fileModel);
+        
         // Assert
-        idToTree.Should().BeEmpty();
+        addedItems.Should().HaveCount(1, "one root item should be added");
+    }
+    
+    [Fact]
+    public void ObservableRoots_DoesNotEmitChange_WhenItemAddedToExistingRoot()
+    {
+        // Arrange
+        var generator = CreateGenerator();
+        var locationId = LocationId.Game;
+        
+        // Add first file to create the root
+        var fileId1 = EntityId.From(0UL);
+        var filePath1 = new GamePath(locationId, new RelativePath("file1.txt"));
+        var fileModel1 = CreateFileModel(fileId1);
+        var testItem1 = CreateTestTreeItem(fileId1, filePath1);
+        generator.OnReceiveFile(testItem1, fileModel1);
+        
+        // Prepare second file
+        var fileId2 = EntityId.From(1UL);
+        var filePath2 = new GamePath(locationId, new RelativePath("file2.txt"));
+        var fileModel2 = CreateFileModel(fileId2);
+        var testItem2 = CreateTestTreeItem(fileId2, filePath2);
+        
+        var rootChangesDetected = false;
+        
+        // Subscribe to the observable after first file added
+        using var subscription = generator.ObservableRoots()
+            .Subscribe(changes => 
+            {
+                rootChangesDetected = true;
+            });
+            
+        // Act - add second file to existing root
+        rootChangesDetected = false;
+        generator.OnReceiveFile(testItem2, fileModel2);
+        
+        // Assert
+        rootChangesDetected.Should().BeFalse("no root-level changes should occur when adding to existing root");
+    }
+    
+    [Fact]
+    public void ObservableRoots_EmitsChange_WhenNewRootAdded()
+    {
+        // Arrange
+        var generator = CreateGenerator();
+        
+        // Add first file to create the first root
+        var fileId1 = EntityId.From(0UL);
+        var locationId1 = LocationId.Game;
+        var filePath1 = new GamePath(locationId1, new RelativePath("file1.txt"));
+        var fileModel1 = CreateFileModel(fileId1);
+        var testItem1 = CreateTestTreeItem(fileId1, filePath1);
+        generator.OnReceiveFile(testItem1, fileModel1);
+        
+        // Prepare file for second root
+        var fileId2 = EntityId.From(1UL);
+        var locationId2 = LocationId.Saves;
+        var filePath2 = new GamePath(locationId2, new RelativePath("file2.txt"));
+        var fileModel2 = CreateFileModel(fileId2);
+        var testItem2 = CreateTestTreeItem(fileId2, filePath2);
+        
+        var addedItems = new List<CompositeItemModel<EntityId>>();
+        
+        // Subscribe to the observable after first file added
+        using var subscription = generator.ObservableRoots()
+            .Subscribe(changes => 
+            {
+                foreach (var change in changes)
+                {
+                    if (change.Reason == DynamicData.ChangeReason.Add)
+                    {
+                        addedItems.Add(change.Current);
+                    }
+                }
+            });
+            
+        // Act - add file to new root
+        generator.OnReceiveFile(testItem2, fileModel2);
+        
+        // Assert
+        addedItems.Should().HaveCount(1, "one root item should be added");
+    }
+    
+    [Fact]
+    public void ObservableRoots_EmitsChange_WhenRootRemoved()
+    {
+        // Arrange
+        var generator = CreateGenerator();
+        var fileId = EntityId.From(0UL);
+        var locationId = LocationId.Game;
+        var filePath = new GamePath(locationId, new RelativePath("file.txt"));
+        var fileModel = CreateFileModel(fileId);
+        var testItem = CreateTestTreeItem(fileId, filePath);
+        
+        // Add a file to create the root
+        generator.OnReceiveFile(testItem, fileModel);
+        var removedItems = new List<CompositeItemModel<EntityId>>();
+        
+        // Subscribe to the observable after file is added
+        using var subscription = generator.ObservableRoots()
+            .Subscribe(changes => 
+            {
+                foreach (var change in changes)
+                {
+                    if (change.Reason == DynamicData.ChangeReason.Remove)
+                    {
+                        removedItems.Add(change.Current);
+                    }
+                }
+            });
+            
+        // Act - remove the file, which should remove the root
+        generator.OnDeleteFile(testItem, fileModel);
+        
+        // Assert
+        removedItems.Should().HaveCount(1, "one root item should be removed");
+    }
+    
+    [Fact]
+    public void ObservableRoots_DoesNotEmitChange_WhenNonLastItemRemoved()
+    {
+        // Arrange
+        var generator = CreateGenerator();
+        var locationId = LocationId.Game;
+        
+        // Add two files to the same root
+        var fileId1 = EntityId.From(0UL);
+        var filePath1 = new GamePath(locationId, new RelativePath("file1.txt"));
+        var fileModel1 = CreateFileModel(fileId1);
+        var testItem1 = CreateTestTreeItem(fileId1, filePath1);
+        
+        var fileId2 = EntityId.From(1UL);
+        var filePath2 = new GamePath(locationId, new RelativePath("file2.txt"));
+        var fileModel2 = CreateFileModel(fileId2);
+        var testItem2 = CreateTestTreeItem(fileId2, filePath2);
+        
+        generator.OnReceiveFile(testItem1, fileModel1);
+        generator.OnReceiveFile(testItem2, fileModel2);
+        
+        var rootChangesDetected = false;
+        
+        // Subscribe to the observable after both files are added
+        using var subscription = generator.ObservableRoots()
+            .Subscribe(changes => 
+            {
+                rootChangesDetected = true;
+            });
+            
+        // Act - remove one file but not the last one
+        rootChangesDetected = false;
+        generator.OnDeleteFile(testItem1, fileModel1);
+        
+        // Assert
+        rootChangesDetected.Should().BeFalse("no root-level changes should occur when removing a non-last item");
     }
 
     [Fact]
