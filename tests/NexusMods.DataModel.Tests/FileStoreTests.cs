27cb0a104dfe616abda65801a4c9510e4c72c148
AL <26797547+Al12rs@users.noreply.github.com>
2025-04-01 10:10:37 +0200

    Simplify test again and remove timing inconsistencies due to running the GC in a fire and forget task

--- tests/NexusMods.DataModel.Tests/FileStoreTests.cs
+++ tests/NexusMods.DataModel.Tests/FileStoreTests.cs
@@ -18,17 +18,20 @@ public class FileStoreTests : ACyberpunkIsolatedGameTest<FileStoreTests>
     private readonly ILibraryService _libraryService;
     private readonly IFileStore _fileStore;
     private readonly IConnection _connection;
+    private readonly IGarbageCollectorRunner _gcRunner;
 
     public FileStoreTests(ITestOutputHelper helper) : base(helper)
     {
         _libraryService = ServiceProvider.GetRequiredService<ILibraryService>();
         _fileStore = ServiceProvider.GetRequiredService<IFileStore>();
         _connection = ServiceProvider.GetRequiredService<IConnection>();
+        _gcRunner = ServiceProvider.GetRequiredService<IGarbageCollectorRunner>();
     }
-
+    
+    
     [Fact]
     [GithubIssue(2944)]
-    public async Task DeletingModWithSharedFilesDoesNotCorruptRemainingFiles()
+    public async Task DeletingModWithSharedFileDoesNotCorrupt()
     {
         // Arrange
         var pathA = FileSystem.GetKnownPath(KnownPath.CurrentDirectory).Combine("Resources").Combine("Lookup Anything 1.48.1-541-1-48-1-1739333325.zip");
@@ -45,27 +48,26 @@ public class FileStoreTests : ACyberpunkIsolatedGameTest<FileStoreTests>
         
         var dataFileBHashBefore = dataFileBBefore.Value.Hash;
         
-        await using var dataFileBStreamBefore = await _fileStore.GetFileStream(dataFileBHashBefore);
-        var dataFileBCheckedHashBefore = await dataFileBStreamBefore.xxHash3Async();
-        dataFileBCheckedHashBefore.Should().Be(dataFileBHashBefore);
-        
         // Act
-        // Remove item A, and re-add it
-        await _libraryService.RemoveItems([libraryItemA.AsLibraryFile().AsLibraryItem()]);
-        var libraryItemA2 = await _libraryService.AddLocalFile(pathA);
+        await _libraryService.RemoveItems([libraryItemA.AsLibraryFile().AsLibraryItem()], GarbageCollectorRunMode.DoNotRun);
+        _gcRunner.Run();
         
         // Assert
-        // Removing item A and re-adding it should not affect item B
+        // Changing A should not affect B
 
         var hashesBAfter = LibraryArchiveFileEntry.FindByParent(_connection.Db, libraryItemB)
             .Select(x => x.AsLibraryFile());
         
         var dataFileBAfter = hashesBAfter.FirstOrOptional(x => x.FileName == "data.json");
         dataFileBAfter.HasValue.Should().BeTrue();
-
-        await using var dataFileBStreamAfter = await _fileStore.GetFileStream(dataFileBAfter.Value.Hash);
-        var dataFileBCheckedHashAfter = await dataFileBStreamAfter.xxHash3Async();
         
-        dataFileBCheckedHashAfter.Should().Be(dataFileBHashBefore);
+        dataFileBAfter.Value.Hash.Should().Be(dataFileBHashBefore, "Hash should be the same after changing A");
+
+        {
+            await using var dataFileBStreamAfter = await _fileStore.GetFileStream(dataFileBAfter.Value.Hash);
+            var dataFileBCheckedHashAfter = await dataFileBStreamAfter.xxHash3Async();
+            dataFileBCheckedHashAfter.Should().Be(dataFileBHashBefore, "Hash should be the same after changing A");
+        }
+        
     }
 }
