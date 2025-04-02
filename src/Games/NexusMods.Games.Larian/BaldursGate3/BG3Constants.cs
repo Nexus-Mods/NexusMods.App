f577c047d1873e6673ff0cdf4219eeeca7cb7fd6
erri120 <erri120@fossmailer.de>
2025-04-02 11:35:09 +0200

    Merge pull request #2973 from erri120/fix/2887-1
    Replace `IsIgnoredPath` in Synchronizer

--- src/Games/NexusMods.Games.Larian/BaldursGate3/BG3Constants.cs
+++ src/Games/NexusMods.Games.Larian/BaldursGate3/BG3Constants.cs
@@ -8,6 +8,10 @@ public static class Bg3Constants
     public static readonly Extension PakFileExtension = new(".pak");
     
     public static readonly LocationId ModsLocationId = LocationId.From("Mods");
-    
+
+    public static readonly LocationId PlayerProfilesLocationId = LocationId.From("PlayerProfiles");
+
+    public static readonly LocationId ScriptExtenderConfigLocationId = LocationId.From("ScriptExtenderConfig");
+
     public static readonly GamePath BG3SEGamePath = new(LocationId.Game, "bin/DWrite.dll");
 }
