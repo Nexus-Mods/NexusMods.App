e44a2e15baa63e4b6afc0bca7000e2cf103f6689
Sewer56 <sewer56lol@googlemail.com>
2025-04-14 19:43:22 +0100

    Added: Missing XUnit.DependencyInjection component on game files

--- tests/NexusMods.App.GarbageCollection.DataModel.Tests/FindUsedFiles/MarkGameFilesTest.cs
+++ tests/NexusMods.App.GarbageCollection.DataModel.Tests/FindUsedFiles/MarkGameFilesTest.cs
@@ -1,3 +1,4 @@
+using Microsoft.Extensions.DependencyInjection;
 using NexusMods.Abstractions.Library;
 using NexusMods.Games.TestFramework;
 using NexusMods.StandardGameLocators.TestHelpers.StubbedGames;
@@ -23,5 +24,12 @@ public class MarkGameFilesTest(IServiceProvider serviceProvider, ILibraryService
         
         // Assert: No game files should be deleted from FileStore, they are roots.
         
+    }
+    
+    public class Startup
+    {
+        // https://github.com/pengweiqhca/Xunit.DependencyInjection?tab=readme-ov-file#3-closest-startup
+        // A trick for parallelizing tests with Xunit.DependencyInjection
+        public void ConfigureServices(IServiceCollection services) => DIHelpers.ConfigureServices(services);
     }
 }
