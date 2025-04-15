088ed22b4262192c13331ea3ce9ce8009b5cabe2
erri120 <erri120@fossmailer.de>
2025-04-15 13:16:38 +0200

    Fix directory names

--- src/ArchiveManagement/NexusMods.FileExtractor/Extractors/ManagedZipExtractor.cs
+++ src/ArchiveManagement/NexusMods.FileExtractor/Extractors/ManagedZipExtractor.cs
@@ -13,26 +13,40 @@ internal class ManagedZipExtractor : IExtractor
     public async Task ExtractAllAsync(IStreamFactory source, AbsolutePath destination, CancellationToken cancellationToken = default)
     {
         await using var stream = await source.GetStreamAsync().ConfigureAwait(false);
-
         using var archive = new ZipArchive(stream, mode: ZipArchiveMode.Read, leaveOpen: false, entryNameEncoding: null);
-        foreach (var entry in archive.Entries)
+
+        var directoryMapping = new Dictionary<string, RelativePath>(StringComparer.Ordinal);
+        foreach (var directoryEntry in archive.Entries.Where(IsDirectoryEntry))
         {
-            if (entry.FullName.EndsWith('/') || entry.FullName.EndsWith('\\')) continue;
+            var relativeDirectoryPath = RelativePath.FromUnsanitizedInput(PathsHelper.FixDirectoryName(directoryEntry.FullName));
+            directoryMapping.Add(directoryEntry.FullName, relativeDirectoryPath);
 
-            var fileName = RelativePath.FromUnsanitizedInput(PathsHelper.FixFileName(entry.FullName));
-            var outputPath = destination.Combine(fileName);
+            var directoryPath = destination.Combine(relativeDirectoryPath);
+            if (!directoryPath.DirectoryExists()) directoryPath.CreateDirectory();
+        }
 
-            var parent = outputPath.Parent;
-            if (!parent.DirectoryExists()) parent.CreateDirectory();
+        foreach (var fileEntry in archive.Entries.Where(IsFileEntry))
+        {
+            var fileName = RelativePath.FromUnsanitizedInput(PathsHelper.FixFileName(fileEntry.Name));
+            var directory = directoryMapping
+                .Where(kv => fileEntry.FullName.StartsWith(kv.Key, StringComparison.Ordinal))
+                .OrderByDescending(kv => kv.Key, StringComparer.Ordinal)
+                .First();
+
+            var relativePath = directory.Value.Join(fileName);
+            var outputPath = destination.Combine(relativePath);
 
             await using var outputStream = outputPath.Open(mode: FileMode.Create, access: FileAccess.ReadWrite, share: FileShare.Read);
-            outputStream.SetLength(entry.Length);
+            outputStream.SetLength(fileEntry.Length);
 
-            await using var inputStream = entry.Open();
+            await using var inputStream = fileEntry.Open();
 
-            await inputStream.CopyToAsync(outputStream, cancellationToken: cancellationToken);
+            await inputStream.CopyToAsync(outputStream, cancellationToken: cancellationToken).ConfigureAwait(false);
         }
     }
+
+    private static bool IsDirectoryEntry(ZipArchiveEntry entry) => entry.Name.Length == 0;
+    private static bool IsFileEntry(ZipArchiveEntry entry) => entry.Name.Length != 0;
 
     public Priority DeterminePriority(IEnumerable<FileType> signatures)
     {
