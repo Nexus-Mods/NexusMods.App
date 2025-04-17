c7b8bb4b77e0b7a420b85e902ac730fa916b0624
IDCs <adrianraer@gmail.com>
2025-04-17 12:17:25 +0100

    fixed observable not generating SortableEntry instances

--- src/Abstractions/NexusMods.Abstractions.Games/ASortableItemProvider.cs
+++ src/Abstractions/NexusMods.Abstractions.Games/ASortableItemProvider.cs
@@ -1,19 +1,19 @@
 using System.Collections.ObjectModel;
-using System.Reactive.Linq;
 using DynamicData;
 using DynamicData.Binding;
 using DynamicData.Kernel;
-using FomodInstaller.Utils.Collections;
+using NexusMods.Abstractions.Games.Interfaces;
 using NexusMods.Abstractions.Loadouts;
 using NexusMods.MnemonicDB.Abstractions;
 using NexusMods.MnemonicDB.Abstractions.TxFunctions;
 using R3;
+using Observable = R3.Observable;
 
 namespace NexusMods.Abstractions.Games;
 
 /// <inheritdoc />
 public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvider
-    where TObject : notnull
+    where TObject : ISortableDbEntryConstraints
 {
     private readonly IConnection _connection;
     private readonly SourceCache<ISortableItem, Guid> _orderCache = new(item => item.ItemId);
@@ -21,90 +21,55 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
     private readonly SemaphoreSlim _semaphore = new(1, 1);
     private readonly CompositeDisposable _disposables = new();
 
-    protected abstract SortOrderId SortOrderId { get; }
+    private Guid _sortOrderTypeId;
+    public Guid SortOrderTypeId { get; }
+
+    private SortOrderId _sortOrderId;
+    public SortOrderId SortOrderId { get; }
+    
+    /// <inheritdoc />
+    public LoadoutId LoadoutId { get; }
 
     /// <inheritdoc />
     public ReadOnlyObservableCollection<ISortableItem> SortableItems => _readOnlyOrderList;
 
     /// <inheritdoc />
     public IObservable<IChangeSet<ISortableItem, Guid>> SortableItemsChangeSet { get; }
-    
+
     /// <summary>
-    /// Other observable changes we want to listen to.
+    /// We don't actually care about the changesets as they're unreliable.
+    ///  we just want to kick off the update when something changes.
     /// </summary>
     /// <returns></returns>
-    protected abstract IObservable<IChangeSet<TObject, EntityId>> GetObservableChanges();
-    
-    /// <inheritdoc />
-    public LoadoutId LoadoutId { get; }
+    protected abstract IObservable<bool> GetObservableChanges();
 
     /// <inheritdoc />
     public ISortableItemProviderFactory ParentFactory { get; }
 
-    /// <inheritdoc />
-    public abstract ISortableItem CreateSortableItem(IConnection connection, LoadoutId loadoutId, Guid id, int idx);
-
-    /// <inheritdoc />
-    protected abstract List<ISortableItem> GetSortableEntries(IDb? db = null);
+    /// <summary>
+    ///  Create a sortableItem from the given persistent item. 
+    /// </summary>
+    protected abstract ISortableItem CreateSortableItem(IConnection connection, LoadoutId loadoutId, TObject item, int idx);
 
     /// <summary>
-    /// 
+    /// The persistent entries we have in the database. This can be any type
+    ///  of data we want as long as we can identify the sortable item the goes
+    ///  along with it + the EntityId as stored in the db.
     /// </summary>
     /// <param name="db"></param>
     /// <returns></returns>
-    protected abstract List<SortableEntry.ReadOnly> GetPersistentEntries(IDb? db = null);
-    
-    protected virtual async Task PersistSortableItems(List<ISortableItem> orderList, CancellationToken token)
+    protected abstract List<TObject> GetPersistentEntries(IDb? db = null);
+
+    /// <summary>
+    /// Allows the implementation to pull the current entries from the cache
+    /// </summary>
+    /// <param name="db"></param>
+    /// <returns></returns>
+    protected List<ISortableItem> GetSortableEntries(IDb? db = null)
     {
-        var availableMods = GetSortableEntries(_connection.Db);
-        var persistentSortableItems = GetPersistentEntries(_connection.Db);
-        
-        if (token.IsCancellationRequested) return;
-        
-        using var tx = _connection.BeginTransaction();
-
-        // Remove outdated persistent items
-        foreach (var dbItem in persistentSortableItems)
-        {
-            var liveItem = availableMods.FirstOrOptional(
-                i => i.ItemId == dbItem.ItemId
-            );
-            
-            if (!liveItem.HasValue)
-            {
-                tx.Delete(dbItem, recursive: false);
-                continue;
-            }
-            
-            var liveIdx = orderList.IndexOf(liveItem.Value);
-
-            if (dbItem.SortIndex != liveIdx)
-            {
-                tx.Add(dbItem, SortableEntry.SortIndex, liveIdx);
-            }
-        }
-
-        // Add new items
-        for (var i = 0; i < orderList.Count; i++)
-        {
-            var liveItem = orderList[i];
-            if (persistentSortableItems.Any(si => si.ItemId == liveItem.ItemId))
-                continue;
-
-            _ = new SortableEntry.New(tx)
-            {
-                ItemId = liveItem.ItemId,
-                ParentSortOrderId = SortOrderId,
-                SortIndex = i,
-            };
-        }
-
-        if (token.IsCancellationRequested) return;
-
-        await tx.Commit();
+        return _orderCache.Items.OrderBy(x => x.SortIndex).ToList();
     }
 
-    
     /// <summary>
     /// 
     /// </summary>
@@ -121,10 +86,6 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
         LoadoutId = loadoutId;
         ParentFactory = parentFactory;
 
-        // load the previously saved order
-        var order = GetSortableEntries(_connection.Db);
-        _orderCache.AddOrUpdate(order);
-        
         // populate read only list
         _orderCache.Connect()
             .Transform(item => item)
@@ -132,22 +93,61 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             .Bind(out _readOnlyOrderList)
             .Subscribe()
             .AddTo(_disposables);
-        
+
         SortableItemsChangeSet = SortableItems.ToObservableChangeSet(item => item.ItemId).RefCount();
     }
 
     /// <summary>
-    /// 
+    /// Takes the current persistent entries and generates a new list of sortable items
     /// </summary>
-    protected void RegisterObservables()
+    /// <returns>A collection of generated ISortableItems</returns>
+    protected virtual IEnumerable<ISortableItem> GenerateSortableItems()
     {
-        GetObservableChanges()
-            .ChangeKey(x => GenerateGuid(x))
-            .ToObservable()
-            .SubscribeAwait(
-                async (changes, token) => { await UpdateOrderCache(changes, token); },
-                awaitOperation: AwaitOperation.Sequential
+        // Get the persistent entries from the db
+        var persistentEntries = GetPersistentEntries(_connection.Db)
+            .OrderBy(x => x.SortIndex)
+            .ToArray();
+
+        // Create a new list of sortable items
+        var sortableItems = new List<ISortableItem>();
+
+        // Create the sortable items
+        for (var i = 0; i < persistentEntries.Length; i++)
+        {
+            var entry = persistentEntries[i];
+            var sortableItem = CreateSortableItem(_connection, LoadoutId, entry,
+                i
+            );
+            sortableItems.Add(sortableItem);
+        }
+
+        return sortableItems;
+    }
+
+    /// <summary>
+    /// In order to trigger the cache subscription, this method MUST be called within
+    ///  the implementation's constructor.
+    /// </summary>
+    protected void Initialize()
+    {
+        // Get the sort order model - this should ideally be overriden by the implementation.
+        var sortOrder = GetOrAddSortOrderModel().GetAwaiter().GetResult();
+        _sortOrderTypeId = sortOrder.SortOrderTypeId;
+        _sortOrderId = sortOrder.SortOrderId;
+
+        // load the previously saved order
+        var order = GenerateSortableItems();
+        _orderCache.AddOrUpdate(order);
+
+        Observable.Create<bool>(observer =>
+                {
+                    var subscription = GetObservableChanges()
+                        .Subscribe(_ => observer.OnNext(true))
+                        .AddTo(_disposables);
+                    return subscription;
+                }
             )
+            .SubscribeAwait(async (_, token) => { await UpdateOrderCache(token); })
             .AddTo(_disposables);
     }
 
@@ -181,15 +181,15 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             // Move the item in the list
             stagingList.RemoveAt(currentIndex);
             stagingList.Insert(newIndex, sortableItem);
-            
+
             // Update the sort index of all items
             for (var i = 0; i < stagingList.Count; i++)
             {
                 stagingList[i].SortIndex = i;
             }
-            
+
             if (token.IsCancellationRequested) return;
-            
+
             await PersistSortableItems(stagingList, token);
 
             _orderCache.Edit(innerCache =>
@@ -204,7 +204,7 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             _semaphore.Release();
         }
     }
-    
+
     /// <inheritdoc/>
     public async Task MoveItemsTo(ISortableItem[] sourceItems, ISortableItem targetItem, TargetRelativePosition relativePosition, CancellationToken token)
     {
@@ -215,18 +215,18 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             var sortedSourceItems = sourceItems
                 .OrderBy(item => item.SortIndex)
                 .ToArray();
-            
+
             // Get current ordering from cache
             var stagingList = _orderCache.Items
                 .OrderBy(item => item.SortIndex)
                 .ToList();
-            
+
             // Determine target insertion position
             var targetItemIndex = stagingList.IndexOf(targetItem);
             var targetIndex = relativePosition == TargetRelativePosition.BeforeTarget ? targetItemIndex : targetItemIndex + 1;
 
             var insertPositionIndex = targetIndex;
-            
+
             // Adjust the insert position index to account for any items before the target index that are also being moved
             foreach (var item in sortedSourceItems)
             {
@@ -234,22 +234,22 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
                     break;
                 insertPositionIndex--;
             }
-            
+
             // Remove items from the staging list and insert them at the new adjusted position
             stagingList.Remove(sortedSourceItems);
             stagingList.InsertRange(insertPositionIndex, sortedSourceItems);
-            
+
             // Update the sort index of all items
             for (var i = 0; i < stagingList.Count; i++)
             {
                 stagingList[i].SortIndex = i;
             }
-            
+
             if (token.IsCancellationRequested) return;
-            
+
             // Update the database
             await PersistSortableItems(stagingList, token);
-            
+
             // Update the public cache
             _orderCache.Edit(innerCache =>
                 {
@@ -263,30 +263,30 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             _semaphore.Release();
         }
     }
-    
+
     /// <summary>
-    /// 
+    /// Changesets are unreliable for our purposes, so we need to update the order cache
+    ///  from the persistent entries.
     /// </summary>
-    /// <param name="changes"></param>
     /// <param name="token"></param>
-    private async Task UpdateOrderCache(IChangeSet<TObject, Guid> changes, CancellationToken token)
+    private async Task UpdateOrderCache(CancellationToken token)
     {
         await _semaphore.WaitAsync(token);
         try
         {
             var availableMods = GetPersistentEntries(_connection.Db);
             var currentOrder = _orderCache.Items.OrderBy(item => item.SortIndex);
-            
+
             if (token.IsCancellationRequested) return;
-            
+
             // Update the order
-            var stagingList = SynchronizeSortingToItems(currentOrder.ToList(), availableMods, this);
-            
+            var stagingList = SynchronizeSortingToItems(currentOrder.ToList(), availableMods);
+
             if (token.IsCancellationRequested) return;
 
             // Update the database
             await PersistSortableItems(stagingList, token);
-            
+
             if (token.IsCancellationRequested) return;
 
             // Update the cache
@@ -304,26 +304,87 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
     }
 
     /// <summary>
+    /// Persist the sortable items to the database and remove the cached sortable
+    ///  items that are no longer available.
+    ///  This should ideally be overriden by the implementation.
+    /// </summary>
+    /// <param name="orderList">The list of sortable items we want to persist</param>
+    /// <param name="token"></param>
+    protected virtual async Task PersistSortableItems(List<ISortableItem> orderList, CancellationToken token)
+    {
+        // See what we actually have in our cache.
+        var cachedSortableItems = _orderCache.Items
+            .OrderBy(item => item.SortIndex)
+            .ToList();
+
+        // Get all the persistent entries from the implementation.
+        var persistentSortableItems = GetPersistentEntries(_connection.Db);
+
+        if (token.IsCancellationRequested) return;
+
+        using var tx = _connection.BeginTransaction();
+
+        // Remove outdated persistent items
+        foreach (var dbItem in persistentSortableItems)
+        {
+            var liveItem = cachedSortableItems.FirstOrOptional(
+                i => i.ItemId.Equals(dbItem.ItemId)
+            );
+
+            if (!liveItem.HasValue)
+            {
+                tx.Delete(dbItem.EntityId, recursive: false);
+                continue;
+            }
+
+            var liveIdx = orderList.IndexOf(liveItem.Value);
+
+            if (dbItem.SortIndex != liveIdx)
+            {
+                tx.Add(dbItem.EntityId, SortableEntry.SortIndex, liveIdx);
+            }
+        }
+
+        // Add new items
+        for (var i = 0; i < orderList.Count; i++)
+        {
+            var liveItem = orderList[i];
+            if (persistentSortableItems.Any(si => si.ItemId == liveItem.ItemId))
+                continue;
+
+            _ = new SortableEntry.New(tx)
+            {
+                ItemId = liveItem.ItemId,
+                ParentSortOrderId = SortOrderId,
+                SortIndex = i,
+            };
+        }
+
+        if (token.IsCancellationRequested) return;
+
+        await tx.Commit();
+    }
+
+    /// <summary>
     /// This method generates a new order list from currentOrder, after removing items that are no longer available and
     /// adding new items that have become available.
     /// New items are added at the beginning of the list, to make them win over existing items.
     /// </summary>
-    /// <param name="availableMods">Collection of mods to synchronize against</param>
     /// <param name="currentOrder">The starting order</param>
+    /// <param name="availableMods">Collection of mods to synchronize against</param>
     /// <returns>The new sorting</returns>
     protected List<ISortableItem> SynchronizeSortingToItems(
         List<ISortableItem> currentOrder,
-        List<SortableEntry.ReadOnly> availableMods,
-        ASortableItemProvider<TObject> provider)
+        List<TObject> availableMods)
     {
-        var modsToAdd = new List<Guid>();
+        var modsToAdd = new List<TObject>();
         var sortableItemsToRemove = new List<ISortableItem>();
-        
+
         // Find items to remove
         foreach (var si in currentOrder)
         {
             // TODO: determine the winning mod in case of multiple mods with the same name
-            var modMatch = availableMods.FirstOrOptional(g => GenerateGuid(g).Equals(si.ItemId));
+            var modMatch = availableMods.FirstOrOptional(g => g.ItemId.Equals(si.ItemId));
             if (!modMatch.HasValue)
             {
                 sortableItemsToRemove.Add(si);
@@ -334,11 +395,12 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
         foreach (var mod in availableMods)
         {
             var sortableItem = currentOrder.FirstOrOptional(item =>
-                item.ItemId.Equals(GenerateGuid(mod)));
+                item.ItemId.Equals(mod.ItemId)
+            );
 
             if (!sortableItem.HasValue)
             {
-                modsToAdd.Add(GenerateGuid(mod));
+                modsToAdd.Add(mod);
             }
         }
 
@@ -348,28 +410,37 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
             .ToList();
 
         stagingList.Remove(sortableItemsToRemove);
-        
+
         // Insert based on the sort index behavior (i.e. smaller index wins or greater index wins)
         //  we want the items to be added as "winners" out of the box.
-        var insertIdx = ParentFactory.IndexOverrideBehavior.Equals(IndexOverrideBehavior.GreaterIndexWins) ? modsToAdd.Capacity : 0;
+        var insertIdx = ParentFactory.IndexOverrideBehavior.Equals(IndexOverrideBehavior.GreaterIndexWins) ? stagingList.Capacity : 0;
         stagingList.InsertRange(insertIdx,
-            modsToAdd.Select((id, idx) =>
-                provider.CreateSortableItem(_connection, LoadoutId, id, idx)));
+            modsToAdd.Select((obj, idx) => CreateSortableItem(_connection, LoadoutId, obj,
+                    insertIdx + idx
+                )
+            )
+        );
 
         for (var i = 0; i < stagingList.Count; i++)
         {
             var item = stagingList[i];
             item.SortIndex = i;
         }
-        
+
         return stagingList;
     }
 
+    /// <summary>
+    /// Obtains the SortOrder model for this provider - default is cute, but the implementation
+    ///  should override this to get the correct SortOrder model.
+    /// </summary>
+    /// <returns></returns>
     protected virtual async ValueTask<SortOrder.ReadOnly> GetOrAddSortOrderModel()
     {
         var sortOrder = SortOrder.All(_connection.Db)
             .FirstOrOptional(lo => lo.LoadoutId == LoadoutId
-                                   && lo.SortOrderTypeId == ParentFactory.SortOrderTypeId);
+                                   && lo.SortOrderTypeId == ParentFactory.SortOrderTypeId
+            );
 
         if (sortOrder.HasValue)
             return sortOrder.Value;
@@ -382,26 +453,9 @@ public abstract class ASortableItemProvider<TObject> : ILoadoutSortableItemProvi
         };
 
         await ts.Commit();
-        
+
         return sortOrder.Value;
     }
-    
-    private static Dictionary<string, Guid> _guidCache = new();
-    protected static Guid GenerateGuid(dynamic input)
-    {
-        var guidEntry = input.ToString();
-        if (_guidCache.ContainsKey(guidEntry))
-            return _guidCache[guidEntry];
-        
-        using var hashAlgorithm = System.Security.Cryptography.SHA256.Create();
-        var hash = hashAlgorithm.ComputeHash(System.Text.Encoding.UTF8.GetBytes(guidEntry));
-        var guidBytes = new byte[16];
-        Array.Copy(hash, guidBytes, 16);
-        var guid = new Guid(guidBytes);
-        _guidCache[guidEntry] = guid;
-        return new Guid(guidBytes);
-    }
-
 
     /// <inheritdoc />
     public void Dispose()
