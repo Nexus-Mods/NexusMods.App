069f48f029afec68ff7507ad900308abad1d9252
MjKey <mj.dot.key@gmail.com>
2025-04-24 17:48:14 +0300

    Add Russian language.

--- src/NexusMods.App.UI/Settings/LanguageSettings.cs
+++ src/NexusMods.App.UI/Settings/LanguageSettings.cs
@@ -22,6 +22,7 @@ public record LanguageSettings : ISettings
             new CultureInfo("pl"),
             new CultureInfo("de"),
             new CultureInfo("it"),
+            new CultureInfo("ru"),
         ];
 
         Array.Sort(supportedLanguages, (a, b) => string.Compare(a.NativeName, b.NativeName, StringComparison.InvariantCulture));
