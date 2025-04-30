9e4c2a3e0aab9da886bb104a8adda36f27e8107f
Timothy Baldridge <tbaldridge@gmail.com>
2025-04-30 17:36:10 -0600

    Wip, hitting some performance issues

---
+++ src/NexusMods.DataModel/Undo/UndoService.cs
@@ -0,0 +1,142 @@
+using System.Collections.Frozen;
+using NexusMods.Abstractions.Loadouts;
+using NexusMods.Cascade;
+using NexusMods.Cascade.Patterns;
+using NexusMods.MnemonicDB.Abstractions;
+using NexusMods.MnemonicDB.Abstractions.Cascade;
+using NexusMods.MnemonicDB.Abstractions.ElementComparers;
+using NexusMods.MnemonicDB.Abstractions.IndexSegments;
+using NexusMods.MnemonicDB.Abstractions.Query;
+using NexusMods.MnemonicDB.Abstractions.ValueSerializers;
+
+namespace NexusMods.DataModel.Undo;
+
+public class UndoService
+{
+    private readonly IConnection _conn;
+
+    public UndoService(IConnection connection)
+    {
+        _conn = connection;
+    }
+
+    public async Task<OutletNode<LoadoutRevisionWithStats>> RevisionsFor(EntityId loadout)
+    {
+        return await _conn.Topology.OutletAsync(Queries.LoadoutRevisionsWithMetadata
+            .Where(row => row.RowId == loadout)
+            .Select(row => LoadoutStats(_conn, row)));
+    }
+    
+    private static readonly Inlet<EntityId> LoadoutId = new();
+    
+    private static readonly Flow<(EntityId LoadoutId, int ModCount)> ModCount =
+        Pattern.Create()
+            .Each(LoadoutId, out var loadoutId)
+            .Db(out var itemId, LoadoutItem.LoadoutId, loadoutId)
+            .Db(itemId, LibraryLinkedLoadoutItem.LibraryItemId, out _)
+            .Return(loadoutId, itemId.Count());
+    
+
+    private static LoadoutRevisionWithStats LoadoutStats(IConnection conn, LoadoutRevision revision)
+    {
+        var newDb = conn.AsOf(TxId.From(revision.TxEntity.Value));
+
+        var loadoutInlet = newDb.Topology.Intern(LoadoutId);
+        loadoutInlet.Values = [revision.RowId];
+        
+        var modCount = newDb.Topology.Outlet(ModCount).FirstOrDefault().ModCount;
+        return new LoadoutRevisionWithStats(revision, modCount); 
+    }
+    
+
+    private static readonly IAttribute[] IgnoreAttributes =
+    [
+        GameInstallMetadata.LastSyncedLoadoutId,
+        Loadout.LastAppliedDateTime,
+        LoadoutItem.ParentId,
+    ];
+
+    /// <summary>
+    /// Reverts the given loadout to the given revision.
+    /// </summary>
+    public async Task RevertTo(LoadoutRevision revisionRevision)
+    {
+        var currentDb = _conn.Db;
+        var revertDb = _conn.AsOf(TxId.From(revisionRevision.TxEntity.Value));
+        var ignoreAttrs = IgnoreAttributes.Select(a => currentDb.AttributeCache.GetAttributeId(a.Id)).ToFrozenSet();
+        var toProcess = new HashSet<EntityId>();
+        toProcess.Add(revisionRevision.RowId);
+
+        var tx = _conn.BeginTransaction();
+        var processed = new HashSet<EntityId>();
+        
+        while (toProcess.Count > 0 && processed.Count < 20000)
+        {
+            var current = toProcess.First(); 
+            toProcess.Remove(current);
+            if (!processed.Add(current))
+                continue;
+
+            var desiredState = revertDb.Datoms(current);
+            var currentState = currentDb.Datoms(current);
+
+
+            CompareEntities(currentState, processed, toProcess, desiredState, tx);
+            ExtractBackReferences(currentDb, current, ignoreAttrs, processed, toProcess, revertDb);
+        }
+
+        // Mark the transaction as a snapshot
+        tx.Add(EntityId.From(TxId.Tmp.Value), LoadoutSnapshot.Snapshot, revisionRevision.EntityId);
+
+        //await tx.Commit();
+    }
+
+    private static void ExtractBackReferences(IDb currentDb, EntityId current, FrozenSet<AttributeId> ignoreAttrs, HashSet<EntityId> processed, HashSet<EntityId> toProcess, IDb revertDb)
+    {
+        var backReferenceDatoms = currentDb.Datoms(SliceDescriptor.CreateReferenceTo(current));
+                
+        foreach (var datom in backReferenceDatoms)
+        {
+            if (ignoreAttrs.Contains(datom.A) || processed.Contains(datom.E))
+                continue;
+            toProcess.Add(datom.E);
+        }
+                
+        var backReferenceDesiredState = revertDb.Datoms(SliceDescriptor.CreateReferenceTo(current));
+        foreach (var datom in backReferenceDesiredState)
+        {
+            if (ignoreAttrs.Contains(datom.A) || processed.Contains(datom.E))
+                continue;
+            toProcess.Add(datom.E);
+        }
+    }
+
+    private static void CompareEntities(EntitySegment currentState, HashSet<EntityId> processed, HashSet<EntityId> toProcess, EntitySegment desiredState, ITransaction tx)
+    {
+        foreach (var datom in currentState)
+        {
+            if (datom.Prefix.ValueTag == ValueTag.Reference)
+            {
+                var value = EntityIdSerializer.Read(datom.ValueSpan);
+                if (!processed.Contains(value)) 
+                    toProcess.Add(value);
+            }
+                
+            if (!desiredState.Contains(datom)) 
+                tx.Add(datom.Retract());
+        }
+
+        foreach (var datom in desiredState)
+        {
+            if (datom.Prefix.ValueTag == ValueTag.Reference)
+            {
+                var value = EntityIdSerializer.Read(datom.ValueSpan);
+                if (!processed.Contains(value)) 
+                    toProcess.Add(value);
+            }
+                
+            if (!currentState.Contains(datom)) 
+                tx.Add(datom);
+        }
+    }
+}
---
+++ tests/NexusMods.DataModel.Tests/Undo/RestorePointTests.cs
@@ -0,0 +1,50 @@
+using System.Diagnostics;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using NexusMods.Abstractions.Loadouts;
+using NexusMods.DataModel.Undo;
+using NexusMods.MnemonicDB.Abstractions;
+using Xunit.Abstractions;
+
+namespace NexusMods.DataModel.Tests.Undo;
+
+public class RestorePointTests(ITestOutputHelper helper) : AArchivedDatabaseTest(helper)
+{
+    [Fact]
+    public async Task CanGetRestorePoints()
+    {
+        await using var tmpConn = await ConnectionFor("two_sdv_collections_added_removed.zip");
+
+        var undoService = tmpConn.Host.Services.GetRequiredService<UndoService>();
+
+        var loadout = Loadout.All(tmpConn.Connection.Db).First();
+
+        var restorePoints = (await undoService.RevisionsFor(loadout))
+            .OrderBy(row => row.Revision.Timestamp);
+
+        var beforePoints = restorePoints.Select(row => new
+            {
+                TxId = row.Revision.TxEntity.ToString(),
+                Timestamp = row.Revision.Timestamp.ToString(),
+                ModCount = row.ModCount
+            }
+        );
+        
+        await undoService.RevertTo(restorePoints.Skip(2).First().Revision);
+        
+        restorePoints = (await undoService.RevisionsFor(loadout))
+            .OrderBy(row => row.Revision.Timestamp);
+        
+        var afterPoints = restorePoints.Select(row => new
+            {
+                TxId = row.Revision.TxEntity.ToString(),
+                Timestamp = row.Revision.Timestamp.ToString(),
+                ModCount = row.ModCount
+            }
+        );
+
+        await Verify(new { Before = beforePoints, After = afterPoints });
+    }
+
+    
+}
