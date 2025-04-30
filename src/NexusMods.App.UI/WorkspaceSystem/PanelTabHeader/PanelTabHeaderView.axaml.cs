cc2287b0949a38c47b659f01b782f430be066f98
Jonathan Feenstra <26406078+JonathanFeenstra@users.noreply.github.com>
2025-04-30 10:40:36 +0200

    Replace PointerMoved handler with extra check in PointerReleased handler
    Co-authored-by: Al <26797547+Al12rs@users.noreply.github.com>

--- src/NexusMods.App.UI/WorkspaceSystem/PanelTabHeader/PanelTabHeaderView.axaml.cs
+++ src/NexusMods.App.UI/WorkspaceSystem/PanelTabHeader/PanelTabHeaderView.axaml.cs
@@ -61,21 +61,15 @@ public partial class PanelTabHeaderView : ReactiveUserControl<IPanelTabHeaderVie
                 .BindToView(this, view => view.ViewModel!.IsSelected)
                 .DisposeWith(disposables);
             
-            Observable.FromEventPattern<PointerEventArgs>(
-                addHandler => Container.PointerMoved += addHandler,
-                removeHandler => Container.PointerMoved -= removeHandler)
-                .Do(eventPattern =>
-                {
-                    var position = eventPattern.EventArgs.GetPosition(this);
-                    _pointerCaptured = Bounds.Contains(position);
-                })
-                .Subscribe()
-                .DisposeWith(disposables);
-                
             Observable.FromEventPattern<PointerReleasedEventArgs>(
                 addHandler => Container.PointerReleased += addHandler,
                 removeHandler => Container.PointerReleased -= removeHandler
-                ).Where(eventPattern => _pointerCaptured && eventPattern.EventArgs.InitialPressMouseButton == MouseButton.Middle)
+                ).Where(eventPattern => eventPattern.EventArgs.InitialPressMouseButton == MouseButton.Middle && 
+                                        Bounds.Contains(eventPattern.EventArgs.GetPosition(this)) && 
+                                        eventPattern.EventArgs.GetIntermediatePoints(this).FirstOrOptional(_ => true)
+                                            .Convert(point => Bounds.Contains(point.Position)) 
+                                            .ValueOr(false)
+                )
                 .Select(_ => Unit.Default)
                 .InvokeReactiveCommand(this, view => view.ViewModel!.CloseTabCommand)
                 .DisposeWith(disposables);
