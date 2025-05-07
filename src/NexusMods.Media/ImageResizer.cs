0153ac00c949debb5e2731823ed387a1e76d6280
Simon Davies <simon.davies@nexusmods.com>
2025-05-07 14:27:11 +0100

    Updated resize function to maintain aspect ratio by default

--- src/NexusMods.Media/ImageResizer.cs
+++ src/NexusMods.Media/ImageResizer.cs
@@ -7,12 +7,15 @@ public sealed class ImageResizer<TResourceIdentifier> : ANestedResourceLoader<TR
     where TResourceIdentifier : notnull
 {
     private readonly SKSizeI _newSize;
+    private readonly bool _maintainAspectRatio;
 
     public ImageResizer(
         SKSizeI newSize,
-        IResourceLoader<TResourceIdentifier, SKBitmap> innerLoader) : base(innerLoader)
+        IResourceLoader<TResourceIdentifier, SKBitmap> innerLoader,
+        bool maintainAspectRatio = true) : base(innerLoader)
     {
         _newSize = newSize;
+        _maintainAspectRatio = maintainAspectRatio;
     }
 
     protected override ValueTask<Resource<SKBitmap>> ProcessResourceAsync(
@@ -20,10 +23,35 @@ public sealed class ImageResizer<TResourceIdentifier> : ANestedResourceLoader<TR
         TResourceIdentifier resourceIdentifier,
         CancellationToken cancellationToken)
     {
-        return ValueTask.FromResult(resource with
+        var original = resource.Data;
+
+        var targetSize = _newSize;
+
+        /*
+         * If Width is larger, the Height is calculated. If Height is larger, the Width is calculated. This ensures the resizing
+         * is based on the dominant dimension while maintaining the aspect ratio
+        */
+        if (_maintainAspectRatio)
         {
-            Data = resource.Data.Resize(_newSize, quality: SKFilterQuality.Low),
-        });
+            var aspectRatio = (float)original.Height / original.Width;
+
+            if (_newSize.Width > _newSize.Height)
+            {
+                var newHeight = (int)(_newSize.Width * aspectRatio);
+                targetSize = new SKSizeI(_newSize.Width, newHeight);
+            }
+            else
+            {
+                var newWidth = (int)(_newSize.Height / aspectRatio);
+                targetSize = new SKSizeI(newWidth, _newSize.Height);
+            }
+        }
+
+        return ValueTask.FromResult(resource with
+            {
+                Data = original.Resize(targetSize, quality: SKFilterQuality.Low),
+            }
+        );
     }
 }
 
@@ -31,14 +59,16 @@ public static partial class Extensions
 {
     public static IResourceLoader<TResourceIdentifier, SKBitmap> Resize<TResourceIdentifier>(
         this IResourceLoader<TResourceIdentifier, SKBitmap> inner,
-        SKSizeI newSize)
+        SKSizeI newSize,
+        bool maintainAspectRatio = true)
         where TResourceIdentifier : notnull
     {
         return inner.Then(
-            state: newSize,
-            factory: static (newSize, inner) => new ImageResizer<TResourceIdentifier>(
-                newSize: newSize,
-                innerLoader: inner
+            state: (newSize, maintainAspectRatio),
+            factory: static (state, inner) => new ImageResizer<TResourceIdentifier>(
+                newSize: state.newSize,
+                innerLoader: inner,
+                maintainAspectRatio: state.maintainAspectRatio
             )
         );
     }
