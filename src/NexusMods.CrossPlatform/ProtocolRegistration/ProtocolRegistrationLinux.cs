08840531bfb49f551b3e013ed67efc6ab41dd48f
sewer56 <seweratwork@gmail.com>
2025-05-20 12:34:36 +0100

    Added: Spec-correct handling of Exec and TryExec field

--- src/NexusMods.CrossPlatform/ProtocolRegistration/ProtocolRegistrationLinux.cs
+++ src/NexusMods.CrossPlatform/ProtocolRegistration/ProtocolRegistrationLinux.cs
@@ -16,7 +16,8 @@ internal class ProtocolRegistrationLinux : IProtocolRegistration
     private const string ApplicationId = "com.nexusmods.app";
     private const string DesktopFile = $"{ApplicationId}.desktop";
     private const string DesktopFileResourceName = $"NexusMods.CrossPlatform.{DesktopFile}";
-    private const string ExecutablePathPlaceholder = "${INSTALL_EXEC}";
+    private const string ExecuteParameterPlaceholder = "${INSTALL_EXEC}";
+    private const string TryExecuteParameterPlaceholder = "${INSTALL_TRYEXEC}";
 
     private readonly ILogger _logger;
     private readonly IProcessFactory _processFactory;
@@ -116,7 +117,8 @@ internal class ProtocolRegistrationLinux : IProtocolRegistration
         using var sr = new StreamReader(stream, encoding: Encoding.UTF8);
 
         var text = await sr.ReadToEndAsync(cancellationToken);
-        text = text.Replace(ExecutablePathPlaceholder, EscapeWhitespaceForCli(_osInterop.GetOwnExe()));
+        text = text.Replace(ExecuteParameterPlaceholder, EscapeDesktopExecFilePath(_osInterop.GetOwnExe()));
+        text = text.Replace(TryExecuteParameterPlaceholder, EscapeDesktopFilePath(_osInterop.GetOwnExe()));
 
         await filePath.WriteAllTextAsync(text, cancellationToken);
     }
@@ -125,15 +127,60 @@ internal class ProtocolRegistrationLinux : IProtocolRegistration
     {
         _logger.LogInformation("Updating MIME cache database");
 
-        var command = _updateDesktopDatabaseDependency.BuildUpdateCommand(EscapeWhitespaceForCli(applicationsDirectory));
+        var command = _updateDesktopDatabaseDependency.BuildUpdateCommand($"\"{applicationsDirectory}\"");
         await _processFactory.ExecuteAsync(command, cancellationToken: cancellationToken);
     }
 
     private async Task SetAsDefaultHandler(string uriScheme, CancellationToken cancellationToken = default)
     {
+        // Note(sewer): This adds `MimeType=x-scheme-handler/nxm;` to the protocol handler entry
+        //              so if we supply this in the existing `.desktop` file running this would make
+        //              a duplicate.
         var command = _xdgSettingsDependency.CreateSetDefaultUrlSchemeHandlerCommand(uriScheme, DesktopFile);
         await _processFactory.ExecuteAsync(command, cancellationToken: cancellationToken);
     }
 
-    private string EscapeWhitespaceForCli(AbsolutePath path) => path.ToNativeSeparators(_fileSystem.OS).Replace(" ", @"\ ");
+    // For the exec field. Quoted relevant parts below.
+    // https://specifications.freedesktop.org/desktop-entry-spec/latest/exec-variables.html
+    //
+    // Arguments may be quoted in whole. If an argument contains a reserved character the argument must be quoted.
+    // The rules for quoting of arguments is also applicable to the executable name or path of the executable
+    // program as provided.
+    //
+    // Quoting must be done by enclosing the argument between double quotes and escaping the double quote
+    // character, backtick character ("`"), dollar sign ("$") and backslash character ("\") by preceding
+    // it with an additional backslash character.
+    private string EscapeDesktopExecFilePath(AbsolutePath path)
+    {
+        var nativePath = path.ToNativeSeparators(_fileSystem.OS);
+        
+        // 'by preceding it with an additional backslash character.'
+        var escapedPath = nativePath
+            .Replace("\"", @"\""") // 'and escaping the double quote character'
+            .Replace("`", @"\`") // backtick character ("`")
+            .Replace("$", @"\$") // dollar sign ("$")
+            .Replace(@"\", @"\\");  // and backslash character ("\")
+            
+            
+        // Enclose the entire path in double quotes
+        return $"\"{escapedPath}\"";
+    }
+
+    // For other fields 'string' and 'localestring':
+    //
+    // https://specifications.freedesktop.org/desktop-entry-spec/1.0/value-types.html
+    // The escape sequences \s, \n, \t, \r, and \\ are supported for values of type string and localestring,
+    // meaning ASCII space, newline, tab, carriage return, and backslash, respectively. 
+    //
+    // Note that 'Exec' is a string https://specifications.freedesktop.org/desktop-entry-spec/1.0/recognized-keys.html
+    private string EscapeDesktopFilePath(AbsolutePath path)
+    {
+        var nativePath = path.ToNativeSeparators(_fileSystem.OS);
+        return nativePath
+            .Replace(@"\", @"\\") // Escape backslashes first to avoid double escaping
+            .Replace(" ", @"\s")
+            .Replace("\n", @"\n")
+            .Replace("\t", @"\t")
+            .Replace("\r", @"\r");
+    }
 }
