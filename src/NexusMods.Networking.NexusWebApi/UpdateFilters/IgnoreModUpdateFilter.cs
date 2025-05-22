000e03d9eedb914d36d32376ecf730d99710cb1a
Sewer <seweratwork@gmail.com>
2025-05-22 12:46:00 +0100

    Added: Generic type for IgnoreModUpdateFilter

--- src/NexusMods.Networking.NexusWebApi/UpdateFilters/IgnoreModUpdateFilter.cs
+++ src/NexusMods.Networking.NexusWebApi/UpdateFilters/IgnoreModUpdateFilter.cs
@@ -7,18 +7,19 @@ namespace NexusMods.Networking.NexusWebApi.UpdateFilters;
 /// <summary>
 /// A class whose purpose is to hide updates for <see cref="ModUpdateService"/>.
 /// </summary>
-public class IgnoreModUpdateFilter
+public class IgnoreModUpdateFilter<TShouldIgnoreFile> where TShouldIgnoreFile : IShouldIgnoreFile
 {
-    private readonly IConnection _connection;
-    
-    /// <summary/>
-    public IgnoreModUpdateFilter(IConnection connection) => _connection = connection;
+    private readonly TShouldIgnoreFile _fileFilter;
+
+    /// <summary>Creates a new instance of the file update filter.</summary>
+    /// <param name="fileFilter">Implementation of the method to check if a file should be filtered.</param>
+    public IgnoreModUpdateFilter(TShouldIgnoreFile fileFilter) => _fileFilter = fileFilter;
 
     /// <summary>Returns true if a file should be excluded from update results.</summary>
     internal bool ShouldIgnoreFile(NexusModsFileMetadata.ReadOnly file) => ShouldIgnoreFile(file.Uid);
 
     /// <summary>Returns true if a file should be excluded from update results.</summary>
-    internal bool ShouldIgnoreFile(UidForFile file) => IgnoreFileUpdateModel.FindByUid(_connection.Db, file).Count > 0;
+    internal bool ShouldIgnoreFile(UidForFile file) => _fileFilter.ShouldIgnoreFile(file);
 
     /// <summary>
     /// Plugs into <see cref="ModUpdateService.GetNewestFileVersionObservable"/>
@@ -48,5 +49,45 @@ public class IgnoreModUpdateFilter
     {
         return null;
     }
+}
+
+/// <summary>
+/// The default implementation of <see cref="IgnoreModUpdateFilter{TFilter}"/> using <see cref="DefaultFileUpdateFilter"/>
+/// (DataStore) as the backend.
+/// </summary>
+public class IgnoreModUpdateFilter : IgnoreModUpdateFilter<DefaultFileUpdateFilter>
+{
+    /// <summary>
+    /// Creates a new instance of the update filter using the default file filter implementation.
+    /// </summary>
+    public IgnoreModUpdateFilter(IConnection connection) 
+        : base(new DefaultFileUpdateFilter(connection))
+    {
+    }
+}
+
+/// <summary>
+/// A filter interface for determining if a file should be ignored in update checks.
+/// The 'ignored' in this context means that the file will not be suggested as a new version.
+/// </summary>
+public interface IShouldIgnoreFile
+{
+    /// <summary>Returns true if a file should be excluded from update results.</summary>
+    bool ShouldIgnoreFile(UidForFile file);
+}
+
+/// <summary>
+/// The default implementation of the file update filter, ignoring files based on <see cref="IgnoreFileUpdateModel"/>(s)
+/// stored in the DataStore.
+/// </summary>
+public class DefaultFileUpdateFilter : IShouldIgnoreFile
+{
+    private readonly IConnection _connection;
     
+    /// <summary>Creates a new instance of the file update filter.</summary>
+    /// <param name="connection">The database connection.</param>
+    public DefaultFileUpdateFilter(IConnection connection) => _connection = connection;
+
+    /// <inheritdoc />
+    public bool ShouldIgnoreFile(UidForFile file) => IgnoreFileUpdateModel.FindByUid(_connection.Db, file).Count > 0;
 }
