fb73ace6d9a0121c9db0276e62868c63e9c72b8c
Simon Davies <simon.davies@nexusmods.com>
2025-05-27 13:52:04 +0100

    Fixed icon
    More layout and styles
    FlexPanel for better size and wrapping

--- src/NexusMods.App.UI/Pages/RestoreLoadout/RestoreLoadoutDesignViewModel.cs
+++ src/NexusMods.App.UI/Pages/RestoreLoadout/RestoreLoadoutDesignViewModel.cs
@@ -15,6 +15,8 @@ namespace NexusMods.App.UI.Pages.RestoreLoadout;
             Revisions = new ReadOnlyObservableCollection<IRevisionViewModel>(new ObservableCollection<IRevisionViewModel>
             {
                 new RevisionDesignViewModel(new LoadoutRevisionWithStats(EntityId.From(123), EntityId.From(456), 1,2,3,4,  DateTimeOffset.Now )),
+                new RevisionDesignViewModel(new LoadoutRevisionWithStats(EntityId.From(789), EntityId.From(456), 1,2,3,4,  DateTimeOffset.Now.AddDays(-1))),
+                new RevisionDesignViewModel(new LoadoutRevisionWithStats(EntityId.From(101112), EntityId.From(456), 1,2,3,4,  DateTimeOffset.Now.AddDays(-2))),
             });
         }
         
