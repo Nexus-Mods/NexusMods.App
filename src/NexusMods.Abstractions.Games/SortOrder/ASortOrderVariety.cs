9fec436a38c3cfd9ce66f65f3799a723a1e6683a
AL <26797547+Al12rs@users.noreply.github.com>
2025-06-04 14:39:54 +0200

    Add MoveItems implementation

--- src/NexusMods.Abstractions.Games/SortOrder/ASortOrderVariety.cs
+++ src/NexusMods.Abstractions.Games/SortOrder/ASortOrderVariety.cs
@@ -1,5 +1,9 @@
 using System.ComponentModel;
+using System.Diagnostics.Contracts;
 using DynamicData;
+using DynamicData.Kernel;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using NexusMods.Abstractions.Loadouts;
 using NexusMods.MnemonicDB.Abstractions;
 using OneOf;
@@ -13,7 +17,17 @@ public abstract class ASortOrderVariety<TItem, TKey> : ISortOrderVariety<TItem, 
     where TItem : ISortableItem<TItem, TKey>
     where TKey : IEquatable<TKey>, ISortItemKey
 {
-    
+    private readonly IConnection _connection;
+    private readonly ILogger _logger;
+    private readonly ISortOrderManager _manager;
+
+    protected ASortOrderVariety(IServiceProvider serviceProvider, ISortOrderManager manager)
+    {
+        _connection = serviceProvider.GetRequiredService<IConnection>();
+        _logger = serviceProvider.GetRequiredService<ILogger>();
+        _manager = manager;
+    }
+        
     #region public members
     
     /// <inheritdoc />
@@ -45,7 +59,7 @@ public abstract class ASortOrderVariety<TItem, TKey> : ISortOrderVariety<TItem, 
     public abstract ValueTask SetSortOrder(SortOrderId sortOrderId, IReadOnlyList<TKey> items, IDb? db = null, CancellationToken token = default);
 
     /// <inheritdoc />
-    public ValueTask MoveItems(
+    public async ValueTask MoveItems(
         SortOrderId sortOrderId,
         TKey[] itemsToMove,
         TKey dropTargetItem,
@@ -53,9 +67,61 @@ public abstract class ASortOrderVariety<TItem, TKey> : ISortOrderVariety<TItem, 
         IDb? db = null,
         CancellationToken token = default)
     {
-        throw new NotImplementedException();
-        GetSortableItems(sortOrderId, db);
+        // acquire the lock
+        using var _ = await _manager.Lock(token);
         
+        // retrieve the sorting from the db
+        var startingOrder = RetrieveSortOrder(sortOrderId, db);
+        
+        // prepare the data for moving
+        var sortedSourceItems = itemsToMove
+            .Select(key => startingOrder.FirstOrOptional(item => item.Key.Equals(key)))
+            .Where(item => {
+                if (!item.HasValue)
+                    _logger.LogWarning("Unable to move Item: {ItemKey} not found in sort order {SortOrderId}", item.Value.Key, sortOrderId);
+                return item.HasValue;
+            })
+            .Select(item => item.Value)
+            .OrderBy(item => item.SortIndex)
+            .ToArray();
+        
+        var stagingList = startingOrder.ToList();
+        
+        // Determine the index of the drop target item
+        var dropTargetIndex = stagingList.FindIndex(item => item.Key.Equals(dropTargetItem));
+        if (dropTargetIndex == -1)
+        {
+            _logger.LogWarning("Drop target item {DropTargetItem} not found in sort order {SortOrderId}", dropTargetItem, sortOrderId);
+            return;
+        }
+        
+        var targetIndex = relativePosition == TargetRelativePosition.BeforeTarget ? dropTargetIndex : dropTargetIndex + 1;
+        
+        var insertPositionIndex = targetIndex;
+        
+        // Adjust the insert position index to account for any items before the target index that are also being moved
+        foreach (var item in sortedSourceItems)
+        {
+            if (!(item.SortIndex < targetIndex))
+                break;
+            insertPositionIndex--;
+        }
+        
+        // Remove items from the staging list and insert them at the new adjusted position
+        stagingList.Remove(sortedSourceItems);
+        stagingList.InsertRange(insertPositionIndex, sortedSourceItems);
+        
+        // Update the sort index of all items
+        for (var i = 0; i < stagingList.Count; i++)
+        {
+            var item = stagingList[i];
+            item.SortIndex = i;
+        }
+        
+        if (token.IsCancellationRequested) return;
+        
+        // persist the new sorting
+        await PersistSortOrder(stagingList, sortOrderId, token);
     }
     
     /// <inheritdoc />
@@ -68,7 +134,19 @@ public abstract class ASortOrderVariety<TItem, TKey> : ISortOrderVariety<TItem, 
     
     #region protected members
     
+    /// <summary>
+    /// Retrieves the sortable entries for the sortOrderId, and returns them as a list of sortable items.
+    /// </summary>
+    /// <remarks>
+    /// The items in the returned list can have temporary values for properties such as `ModName` and `IsActive`.
+    /// Those will need to be updated after the sortableItems are matched to items in the loadout. 
+    /// </remarks>
+    protected abstract IReadOnlyList<TItem> RetrieveSortOrder(SortOrderId sortOrderEntityId, IDb? db = null);
     
+    /// <summary>
+    /// Persists the sort order for the provided list of sortable items
+    /// </summary>
+    protected abstract Task PersistSortOrder(IReadOnlyList<TItem> items, SortOrderId sortOrderEntityId, CancellationToken token);
     
     #endregion protected members
 }
