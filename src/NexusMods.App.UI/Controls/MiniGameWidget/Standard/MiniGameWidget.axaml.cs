877bc35118402a512bdd6aae66bd8e88ebdb36ff
Simon Davies <simon.davies@nexusmods.com>
2025-07-07 12:09:51 +0100

    new gamepad icon, one liner to show tooltip if truncated

--- src/NexusMods.App.UI/Controls/MiniGameWidget/Standard/MiniGameWidget.axaml.cs
+++ src/NexusMods.App.UI/Controls/MiniGameWidget/Standard/MiniGameWidget.axaml.cs
@@ -1,5 +1,7 @@
 using System.Reactive;
 using System.Reactive.Disposables;
+using Avalonia.Controls;
+using Avalonia.Media;
 using Avalonia.ReactiveUI;
 using ReactiveUI;
 
@@ -16,18 +18,27 @@ public partial class MiniGameWidget : ReactiveUserControl<IMiniGameWidgetViewMod
                 this.OneWayBind(ViewModel, vm => vm.Image, v => v.GameImage.Source)
                     .DisposeWith(d);
 
-                this.OneWayBind<IMiniGameWidgetViewModel, MiniGameWidget, string, string?>(ViewModel, vm => vm.Name, v => v.NameTextBlock.Text)
-                    .DisposeWith(d);
-
-                this.BindCommand<MiniGameWidget, IMiniGameWidgetViewModel, ReactiveCommand<Unit, Unit>, StandardButton>(ViewModel, vm => vm.GiveFeedbackCommand, view => view.ButtonGameNotFound)
+                this.WhenAnyValue(view => view.ViewModel!.Name)
+                    .Subscribe(name =>
+                        {
+                            NameTextBlock.Text = name;
+                            
+                            // set tip if the name is too long
+                            var isTrimmed = NameTextBlock.TextTrimming != TextTrimming.None && NameTextBlock.TextLayout.TextLines.Any(x => x.HasCollapsed);
+                            ToolTip.SetTip(NameTextBlock, isTrimmed ? name : null);
+                        }
+                    )
                     .DisposeWith(d);
                 
+                this.BindCommand<MiniGameWidget, IMiniGameWidgetViewModel, ReactiveCommand<Unit, Unit>, StandardButton>(ViewModel, vm => vm.GiveFeedbackCommand, view => view.ButtonGameNotFound)
+                    .DisposeWith(d);
+
                 this.WhenAnyValue(view => view.ViewModel!.GameInstallations)
                     .Subscribe(installations =>
                         {
-                            if(installations is null)
+                            if (installations is null)
                                 return;
-                            
+
                             var tooltip = string.Join(", ", installations.Select(installation => installation.Store.ToString()));
                         }
                     )
