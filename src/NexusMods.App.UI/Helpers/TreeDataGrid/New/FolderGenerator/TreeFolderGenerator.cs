31a8fdb2b6198712afd449e84d0d806836c4aea9
Timothy Baldridge <tbaldridge@gmail.com>
2025-08-12 14:38:19 -0600

    Fix typo in exception message in `SimplifiedObservableRoots` method

--- src/NexusMods.App.UI/Helpers/TreeDataGrid/New/FolderGenerator/TreeFolderGenerator.cs
+++ src/NexusMods.App.UI/Helpers/TreeDataGrid/New/FolderGenerator/TreeFolderGenerator.cs
@@ -36,7 +36,7 @@ public class TreeFolderGenerator<TTreeItemWithPath, TFolderModelInitializer> : I
     /// </summary>
     public IObservable<IChangeSet<CompositeItemModel<GamePath>, GamePath>> SimplifiedObservableRoots()
     {
-        throw new NotSupportedException("Not usable yet, calling this causes a double dispoe error");
+        throw new NotSupportedException("Not usable yet, calling this causes a double dispose error");
         return _observableRoots
             .Select(_ => LocationIdToTree.Count) // tied 1:1 with root count
             .Select(GetAdaptedChangeSet) // get either changeset with 1 root, or with all roots.
