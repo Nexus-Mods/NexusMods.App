b479d60c64248a9179387a212b9952d0a977fbae
Sewer56 <sewer56lol@googlemail.com>
2025-09-04 13:48:50 +0100

    Fixed: Bug in DownloadsService, on cancellation previous job is nulled out.

--- src/NexusMods.Library/DownloadsService.cs
+++ src/NexusMods.Library/DownloadsService.cs
@@ -77,7 +77,7 @@ public sealed class DownloadsService : IDownloadsService, IDisposable
                                 // Note(sewer): JobMonitor removes all jobs after completion, but we want to keep the completed jobs
                                 //              to show in the 'Completed' tab.
                                 //              Cancelled jobs can also yield a ChangeReason.Remove, so we need to make a distinction here. 
-                                if (change.Previous.Value.Status == JobStatus.Completed)
+                                if (change.Previous != null && change.Previous.Value.Status == JobStatus.Completed)
                                 {
                                     // Keep completed downloads but mark them as completed
                                     var existingItem = updater.Lookup(change.Key);
