b91183fc102570e8fba0db07c9edc7018a824b66
Timothy Baldridge <tbaldridge@gmail.com>
2025-09-16 19:57:09 -0600

    Enforce 2GB maximum backup size in `ALoadoutSynchronizer` and add validation for total archived file size.

--- src/NexusMods.Abstractions.Loadouts.Synchronizers/ALoadoutSynchronizer.cs
+++ src/NexusMods.Abstractions.Loadouts.Synchronizers/ALoadoutSynchronizer.cs
@@ -31,6 +31,7 @@ using NexusMods.Paths;
 using NexusMods.Sdk;
 using NexusMods.Sdk.FileStore;
 using NexusMods.Sdk.IO;
+using ReactiveUI;
 using Reloaded.Memory.Extensions;
 
 namespace NexusMods.Abstractions.Loadouts.Synchronizers;
@@ -44,6 +45,12 @@ using DiskState = Entities<DiskStateEntry.ReadOnly>;
 [PublicAPI]
 public class ALoadoutSynchronizer : ILoadoutSynchronizer
 {
+    /// <summary>
+    /// We'll limit backups to 2GB, for now we should never see much more than this
+    /// of modified game files. s
+    /// </summary>
+    private static Size MaximumBackupSize => Size.GB * 2;
+    
     private readonly ScopedAsyncLock _lock = new();
     private readonly IFileStore _fileStore;
 
@@ -1303,6 +1310,10 @@ public class ALoadoutSynchronizer : ILoadoutSynchronizer
             }
         );
 
+        var totalSize = archivedFiles.Sum(static x => x.Size);
+        if (totalSize > MaximumBackupSize)
+            throw new Exception($"Cannot backup files, total size is {totalSize}, which is larger than the maximum of {MaximumBackupSize}");
+        
         // PERFORMANCE: We deduplicate above with the HaveFile call.
         await _fileStore.BackupFiles(archivedFiles, deduplicate: false);
 
