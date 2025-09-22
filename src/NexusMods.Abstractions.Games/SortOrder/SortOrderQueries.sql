d78e6762956fb6a1fdb25c1469127a9f2ddcca80
AL <26797547+Al12rs@users.noreply.github.com>
2025-09-22 17:54:33 +0200

    Improve performance of SortOrder updates query

--- src/NexusMods.Abstractions.Games/SortOrder/SortOrderQueries.sql
+++ src/NexusMods.Abstractions.Games/SortOrder/SortOrderQueries.sql
@@ -6,12 +6,21 @@ CREATE SCHEMA IF NOT EXISTS sortorder;
 -- TODO: listen to changes to Game files too
 -- TODO: this only checks for changes to items in collections, external changes is not covered
 CREATE MACRO sortorder.TrackCollectionAndLoadoutChanges(db, gameId) AS TABLE
-SELECT itemGroup.Parent, itemGroup.Loadout, MAX(d.T) as tx
+SELECT 
+    itemGroup.Parent, 
+    itemGroup.Loadout,
+    GREATEST(
+        COALESCE(MAX(itemDatoms.T), 0),
+        COALESCE(MAX(groupDatoms.T), 0),
+        COALESCE(MAX(collectionDatoms.T), 0)
+    ) as tx
 FROM mdb_LoadoutItemGroup(Db=>db) itemGroup
 JOIN mdb_LoadoutItem(Db=>db) item  on item.Parent = itemGroup.Id
 JOIN mdb_Loadout(Db=>db) loadout on itemGroup.Loadout = loadout.Id
 JOIN mdb_GameInstallMetadata(Db=>db) as install on loadout.Installation = install.Id
 JOIN mdb_CollectionGroup(Db=>db) collection on itemGroup.Parent = collection.Id
-JOIN mdb_Datoms() d ON d.E = item.Id OR d.E = itemGroup.Id OR d.E = itemGroup.Parent
+LEFT JOIN mdb_Datoms() itemDatoms ON itemDatoms.E = item.Id 
+LEFT JOIN mdb_Datoms() groupDatoms ON groupDatoms.E = itemGroup.Id 
+LEFT JOIN mdb_Datoms() collectionDatoms on collectionDatoms.E = itemGroup.Parent
 WHERE install.GameId = gameId
 GROUP BY itemGroup.Parent, itemGroup.Loadout;
