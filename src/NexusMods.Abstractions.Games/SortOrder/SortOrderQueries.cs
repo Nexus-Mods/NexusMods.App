62527992dc505711c2d34137d70e95c318420889
AL <26797547+Al12rs@users.noreply.github.com>
2025-09-23 14:59:46 +0200

    Remove unused query

--- src/NexusMods.Abstractions.Games/SortOrder/SortOrderQueries.cs
+++ src/NexusMods.Abstractions.Games/SortOrder/SortOrderQueries.cs
@@ -6,16 +6,11 @@ namespace NexusMods.Abstractions.Games;
 
 public static class SortOrderQueries
 {
-    public static IObservable<IChangeSet<(EntityId CollectionId, EntityId LoadoutId, ulong TxId),EntityId>> TrackCollectionAndLoadoutChanges(IConnection connection, GameId gameId)
-    {
-        return connection.Query<(EntityId ChangedCollection, EntityId LoaodutId, ulong TxId)>($"""
-                                                 SELECT * FROM sortorder.TrackCollectionAndLoadoutChanges({connection}, {gameId.Value})
-                                                 """
-            )
-            .Observe(x => x.ChangedCollection);
-    }
-    
-    public static IObservable<IChangeSet<(EntityId ItemId, EntityId GroupId, EntityId CollectionId, EntityId LoadoutId), EntityId>> TrackLoadoutItemChanges(IConnection connection, GameId gameId)
+    /// <summary>
+    /// Returns an observable of changes to loadoutItemsWithTargetPath in the specified game.
+    /// </summary>
+    public static IObservable<IChangeSet<(EntityId ItemId, EntityId GroupId, EntityId CollectionId, EntityId LoadoutId), EntityId>> 
+        TrackLoadoutItemChanges(IConnection connection, GameId gameId)
     {
         return connection.Query<(EntityId ItemId, EntityId GroupId, EntityId CollectionId, EntityId LoadoutId)>($"""
                                                  SELECT * FROM sortorder.TrackLoadoutItemChanges({connection}, {gameId.Value})
