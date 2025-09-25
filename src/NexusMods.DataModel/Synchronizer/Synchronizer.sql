a9c780780110c7813e6811753aad9007d6809a75
Timothy Baldridge <tbaldridge@gmail.com>
2025-09-25 14:47:22 -0600

    Add synchronization SQL macros and enhance file hash queries
    - Introduced `synchronizer` schema with macros for enabled, override, and winning files.
    - Added `file_hashes.Stores` ENUM and macros for loadout files across Steam and GOG.

---
+++ src/NexusMods.DataModel/Synchronizer/Synchronizer.sql
@@ -0,0 +1,49 @@
+-- namespace: NexusMods.Games.FileHashes
+CREATE SCHEMA IF NOT EXISTS synchronizer;
+
+-- The sources of items in the loadout.
+CREATE TYPE synchronizer.ItemType AS ENUM ('Loadout', 'Game', 'Deleted', 'Intrinsic');
+
+-- All the enabled files in a loadout
+CREATE MACRO synchronizer.EnabledFiles(db) AS TABLE
+SELECT item.Loadout, item.Id,
+    {Location: item.TargetPath.Item2, Path: item.TargetPath.Item3} Path,
+    file.Hash,
+    file.Size,
+    (CASE WHEN deletedFile.Id is NOT NULL THEN 'Deleted' ELSE 'Loadout' END)::synchronizer.ItemType ItemType
+FROM MDB_LOADOUTITEMWITHTARGETPATH(Db=>db) item
+    LEFT JOIN MDB_LOADOUTITEMGROUP(Db=>db) itemGroup ON item.Parent = itemGroup.Id
+    LEFT JOIN MDB_COLLECTIONGROUP(Db=>db) coll ON itemGroup.Parent = coll.Id
+    LEFT JOIN MDB_DELETEDFILE(Db=>db) deletedFile ON item.Id = deletedFile.Id
+    LEFT JOIN MDB_LOADOUTFILE(Db=>db) file on item.Id = file.Id
+WHERE not item.Disabled and not itemGroup.Disabled and not coll.Disabled
+
+-- All the files in the overrides group
+CREATE MACRO synchronizer.OverrideFiles(db) AS TABLE
+SELECT item.Loadout, item.Id,
+    {Location: item.TargetPath.Item2, Path: item.TargetPath.Item3} Path,
+    file.Hash,
+    file.Size,
+    (CASE WHEN deletedFile.Id is NOT NULL THEN 'Deleted' ELSE 'Loadout' END)::synchronizer.ItemType ItemType
+FROM MDB_LOADOUTITEMWITHTARGETPATH(Db=>db) item
+    LEFT JOIN MDB_LOADOUTOVERRIDESGROUP(Db=>db) itemGroup ON item.Parent = itemGroup.Id
+    LEFT JOIN MDB_DELETEDFILE(Db=>db) deletedFile ON item.Id = deletedFile.Id
+    LEFT JOIN MDB_LOADOUTFILE(Db=>db) file on item.Id = file.Id
+WHERE not item.Disabled
+
+-- All winning files in loadouts
+CREATE MACRO synchronizer.WinningFiles(db) as TABLE
+WITH 
+   allFiles AS (-- Game files on Layer 0
+                SELECT Loadout, NULL, {Location: 57681, Path: Path}, Hash, Size, 'Game'::synchronizer.ItemType, 0 Layer 
+                FROM file_hashes.loadout_files(db)
+                UNION
+                -- Loadout files on Layer 1
+                SELECT Loadout, Id, Path, Hash, Size, ItemType, 1 Layer FROM synchronizer.EnabledFiles(db)
+                UNION
+                -- Override files on Layer 2
+                SELECT Loadout, Id, Path, Hash, Size, ItemType, 2 Layer FROM synchronizer.OverrideFiles(db))
+-- Group by loadout, path and take the winning file
+SELECT Loadout, Path, arg_max(Layer, Hash) Hash, arg_max(Layer, Size) Size, arg_max(Layer, Id) Id
+FROM allFiles
+GROUP BY Loadout, Path       
--- src/NexusMods.Games.FileHashes/FileHashesQueries.sql
+++ src/NexusMods.Games.FileHashes/FileHashesQueries.sql
@@ -33,3 +33,56 @@ SELECT arg_max(ManifestId, matching_count) DepotId
 FROM file_hashes.resolve_steam_manifests(GameMetadataId) manifests
 GROUP BY manifests.AppId
 Having DepotId is not null;
+
+-- ENUM of all the store names
+CREATE TYPE file_hashes.Stores AS ENUM ('GOG', 'Steam');
+
+-- gets all the loadouts, locatorids, and stores
+CREATE MACRO file_hashes.loadout_locatorids(db) AS TABLE
+SELECT install.Store::Stores, loadout.id Loadout, unnest(locatorIds) AS LocatorId
+FROM MDB_LOADOUT(Db=>db) loadout
+         LEFT JOIN MDB_GAMEINSTALLMETADATA(Db=>db) install on loadout.Installation = install.id  
+    
+-- gets all the paths and hashes of game files for steam loadouts
+CREATE OR REPLACE MACRO file_hashes.steam_loadout_files(db) AS TABLE
+SELECT files.Loadout, PathRel.Path, hashRel.xxHash3 Hash, hashRel.Size FROM
+    (SELECT Loadout, ManifestId, unnest(Files) FileId
+     FROM file_hashes.loadout_locatorids(db) locators
+              LEFT JOIN HASHES_STEAMMANIFEST(Db=>db) manifest ON manifest.ManifestId = locators.LocatorID::UBIGINT
+     WHERE locators.Store = 'Steam') files
+        LEFT JOIN HASHES_PATHHASHRELATION() pathrel on files.FileId = pathrel.Id
+        LEFT JOIN HASHES_HASHRELATION() hashrel on pathrel.Hash = hashrel.Id
+
+
+-- gets all the paths and hashes of game files for gog loadouts
+CREATE OR REPLACE MACRO file_hashes.gog_loadout_files(db) AS TABLE
+WITH 
+  locatorIds AS (SELECT Loadout, LocatorId::UBIGINT LocatorId
+                 FROM file_hashes.loadout_locatorids(db) locators
+                 WHERE locators.Store = 'GOG'), 
+  builds AS (SELECT Loadout, BuildId, ProductId BaseProductId, unnest(build.Depots) DepotId
+             FROM HASHES_GOGBUILD() build
+             INNER JOIN locatorIds ON build.BuildId = locatorIds.LocatorId),
+  validDepots AS (SELECT Id, ProductId, Manifest 
+                  FROM HASHES_GOGDEPOT() 
+                  WHERE Languages == [] OR 'en-US' in Languages),
+  manifests AS (SELECT builds.Loadout, validDepots.Manifest 
+                FROM validDepots 
+                INNER JOIN builds ON validDepots.ProductId = builds.BaseProductId
+                UNION
+                SELECT locatorIds.Loadout, validDepots.Manifest 
+                FROM validDepots 
+                INNER JOIN locatorIds ON validDepots.ProductId = LocatorIds.LocatorId),
+  files AS (SELECT Loadout, unnest(manifest.Files) File
+            FROM manifests
+            LEFT JOIN HASHES_GOGMANIFEST() manifest ON manifests.Manifest = manifest.Id)
+SELECT files.Loadout, pathrel.Path, hashrel.XxHash3 Hash, hashrel.Size 
+    FROM files
+    LEFT JOIN HASHES_PATHHASHRELATION() pathrel ON pathrel.Id = files.File
+    LEFT JOIN HASHES_HASHRELATION() hashrel ON hashrel.Id = pathrel.Hash
+
+-- gets all the paths and hashes for game files in loadouts
+CREATE MACRO file_hashes.loadout_files(db) AS TABLE
+SELECT Loadout, Path, Hash, Size FROM file_hashes.gog_loadout_files(db)
+UNION
+SELECT Loadout, Path, Hash, Size FROM file_hashes.steam_loadout_files(db)
