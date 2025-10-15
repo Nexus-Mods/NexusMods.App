e50978a156206b14f47026e92d940c5cabd94d6b
AL <26797547+Al12rs@users.noreply.github.com>
2025-10-15 14:12:44 +0200

    Add FileConflictsColumns.cs and IndexColumn

--- src/NexusMods.App.UI/Pages/Sorting/FileConflicts/FileConflictsViewModel.cs
+++ src/NexusMods.App.UI/Pages/Sorting/FileConflicts/FileConflictsViewModel.cs
@@ -13,15 +13,12 @@ using NexusMods.App.UI.Controls;
 using NexusMods.App.UI.Controls.MarkdownRenderer;
 using NexusMods.App.UI.Dialog;
 using NexusMods.App.UI.Dialog.Enums;
-using NexusMods.App.UI.Extensions;
 using NexusMods.App.UI.Windows;
 using NexusMods.MnemonicDB.Abstractions;
 using NexusMods.Sdk.Resources;
 using NexusMods.UI.Sdk;
-using NexusMods.UI.Sdk.Dialog;
 using R3;
 using ReactiveUI;
-using ReactiveCommand = R3.ReactiveCommand;
 
 namespace NexusMods.App.UI.Pages.Sorting;
 
@@ -141,67 +138,5 @@ public class FileConflictsTreeDataGridAdapter : TreeDataGridAdapter<CompositeIte
             viewHierarchical ? ITreeDataGridItemModel<CompositeItemModel<EntityId>, EntityId>.CreateExpanderColumn(nameColumn) : nameColumn,
             ColumnCreator.Create<EntityId, FileConflictsColumns.Actions>(),
         ];
-    }
-}
-
-public static class FileConflictsColumns
-{
-    private const string Prefix = "FileConflicts_";
-
-    public class Actions : ICompositeColumnDefinition<Actions>
-    {
-        public static int Compare<TKey>(CompositeItemModel<TKey> a, CompositeItemModel<TKey> b) where TKey : notnull
-        {
-            return a.GetOptional<FileConflictsComponents.ViewAction>(ViewComponentKey).Compare(b.GetOptional<FileConflictsComponents.ViewAction>(ViewComponentKey));
-        }
-
-        public const string ColumnTemplateResourceKey = Prefix + "Action";
-        public static readonly ComponentKey ViewComponentKey = ComponentKey.From(ColumnTemplateResourceKey + "_" + nameof(Actions) + "_" + "View");
-
-        public static string GetColumnHeader() => "Action";
-        public static string GetColumnTemplateResourceKey() => ColumnTemplateResourceKey;
-    }
-}
-
-public static class FileConflictsComponents
-{
-    public class ViewAction : IItemModelComponent<ViewAction>, IComparable<ViewAction>
-    {
-        public LoadoutItemGroup.ReadOnly Group { get; }
-        private readonly LoadoutFile.ReadOnly[] _loadoutFiles;
-        private readonly FrozenDictionary<GamePath, FileConflictGroup> _conflictsByPath;
-        public ReactiveCommand<Unit> CommandViewConflicts { get; } = new ReactiveCommand();
-
-        public ViewAction(LoadoutItemGroup.ReadOnly group, LoadoutFile.ReadOnly[] loadoutFiles, FrozenDictionary<GamePath, FileConflictGroup> conflictsByPath)
-        {
-            Group = group;
-            _loadoutFiles = loadoutFiles;
-            _conflictsByPath = conflictsByPath;
-        }
-
-        public int CompareTo(ViewAction? other) => 0;
-
-        public string CreateMarkdown()
-        {
-            var markdown = _loadoutFiles
-                .Select(GamePath (x) => x.AsLoadoutItemWithTargetPath().TargetPath)
-                .Select(gamePath =>
-                {
-                    var conflicts = _conflictsByPath[gamePath].Items;
-                    var conflictingGroups = conflicts.Where(x => x.File.IsT0).Select(x => x.File.AsT0.AsLoadoutItemWithTargetPath().AsLoadoutItem().Parent).ToArray();
-                    return (gamePath, conflictingGroups);
-                })
-                .Select(tuple =>
-                {
-                    var (gamePath, conflictingGroups) = tuple;
-                    var heading = $"## {gamePath}\n";
-                    var body = conflictingGroups.Select(x => $"- {x.AsLoadoutItem().Name}").Aggregate((a, b) => $"{a}\n{b}");
-
-                    return $"{heading}\n{body}";
-                })
-                .Aggregate((a,b) => $"{a}\n{b}");
-
-            return markdown;
-        }
     }
 }
