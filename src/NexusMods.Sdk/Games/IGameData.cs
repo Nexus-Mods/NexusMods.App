0a2168a4ccb77a679131e657e73f6c365c609486
erri120 <erri120@fossmailer.de>
2025-11-03 12:45:01 +0100

    Create `IGameData`

---
+++ src/NexusMods.Sdk/Games/IGameData.cs
@@ -0,0 +1,51 @@
+using DynamicData.Kernel;
+using JetBrains.Annotations;
+using NexusMods.Sdk.IO;
+
+namespace NexusMods.Sdk.Games;
+
+[PublicAPI]
+public interface IGameData
+{
+    /// <summary>
+    /// Gets the unique identifier for the game.
+    /// </summary>
+    GameId Id { get; }
+
+    /// <summary>
+    /// Gets the display name of the game.
+    /// </summary>
+    string DisplayName { get; }
+
+    /// <summary>
+    /// Gets the ID of the game on Nexus Mods.
+    /// </summary>
+    Optional<Sdk.NexusModsApi.GameId> NexusModsId { get; }
+
+    /// <summary>
+    /// Gets the stream factory for the square icon image.
+    /// </summary>
+    IStreamFactory IconImage { get; }
+
+    /// <summary>
+    /// Gets the stream factory for the horizontal tile image.
+    /// </summary>
+    IStreamFactory TileImage { get; }
+}
+
+[PublicAPI]
+public interface IGameData<TSelf> : IGameData
+    where TSelf : IGameData, IGameData<TSelf>
+{
+    /// <inheritdoc cref="IGameData.Id"/>
+    new static abstract GameId Id { get; }
+    GameId IGameData.Id => TSelf.Id;
+
+    /// <inheritdoc cref="IGameData.DisplayName"/>
+    new static abstract string DisplayName { get; }
+    string IGameData.DisplayName => TSelf.DisplayName;
+
+    /// <inheritdoc cref="IGameData.NexusModsId"/>
+    new static abstract Optional<Sdk.NexusModsApi.GameId> NexusModsId { get; }
+    Optional<Sdk.NexusModsApi.GameId> IGameData.NexusModsId => TSelf.NexusModsId;
+}
