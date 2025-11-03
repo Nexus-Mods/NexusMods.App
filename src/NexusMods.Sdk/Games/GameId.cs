4557e3a04fb85b25b2a933b210bed3fddd6239e9
erri120 <erri120@fossmailer.de>
2025-11-03 14:51:22 +0100

    Add `GameIdAttribute`

--- src/NexusMods.Sdk/Games/GameId.cs
+++ src/NexusMods.Sdk/Games/GameId.cs
@@ -1,4 +1,7 @@
 using JetBrains.Annotations;
+using NexusMods.MnemonicDB.Abstractions;
+using NexusMods.MnemonicDB.Abstractions.Attributes;
+using NexusMods.MnemonicDB.Abstractions.ValueSerializers;
 using NexusMods.Sdk.Hashes;
 using TransparentValueObjects;
 
@@ -17,4 +20,17 @@ public readonly partial struct GameId
 
     /// <inheritdoc/>
     public override string ToString() => HashPool[Value];
+}
+
+/// <summary>
+/// Attribute for <see cref="GameId"/>.
+/// </summary>
+[PublicAPI]
+public class GameIdAttribute(string ns, string name) : ScalarAttribute<GameId, ulong, UInt64Serializer>(ns, name)
+{
+    /// <inheritdoc />
+    protected override ulong ToLowLevel(GameId value) => value.Value;
+
+    /// <inheritdoc />
+    protected override GameId FromLowLevel(ulong value, AttributeResolver resolver) => GameId.From(value);
 }
