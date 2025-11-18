90804ca8c56ae5857231bba0d3037d7f1f4cd568
erri120 <erri120@fossmailer.de>
2025-11-18 11:34:36 +0100

    Fix tests

--- tests/Networking/NexusMods.Networking.NexusWebApi.Tests/Startup.cs
+++ tests/Networking/NexusMods.Networking.NexusWebApi.Tests/Startup.cs
@@ -23,6 +23,7 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         services
+            .AddGameServices()
             .AddSerializationAbstractions()
             .AddFileSystem()
             .AddSettingsManager()
